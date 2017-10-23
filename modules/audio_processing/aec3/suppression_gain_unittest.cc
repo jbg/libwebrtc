652c13339da70a1a9e4a45d5fbd8d3b6efc9c1f4
Per Åhgren <peah@webrtc.org>
2017-10-23 20:16:47 +0200

    Memory test
    Memory test
    
    
    Memory test
    
    
    Memory test
    
    
    Memory test
    
    
    Balancing the transparency in AEC3 between saturating and low echo paths
    
    This CL balances the NLP tradeoff in AEC3 to properly handle the cases
    when the echo path is so strong that it saturates the echo and when it
    is so weak that the echo is very low compared to nearend.
    
    
    Bug:webrtc:8411, webrtc:8412, chromium:775653
    Change-Id: I5aff74dfadd51cac1ce71b1cb935d68a5be6918d

--- modules/audio_processing/aec3/suppression_gain_unittest.cc
+++ modules/audio_processing/aec3/suppression_gain_unittest.cc
@@ -30,7 +30,7 @@ TEST(SuppressionGain, NullOutputGains) {
   N2.fill(0.f);
   float high_bands_gain;
   EXPECT_DEATH(SuppressionGain(EchoCanceller3Config{}, DetectOptimization())
-                   .GetGain(E2, R2, N2, RenderSignalAnalyzer(), false,
+                   .GetGain(E2, R2, N2, RenderSignalAnalyzer(), false, false,
                             std::vector<std::vector<float>>(
                                 3, std::vector<float>(kBlockSize, 0.f)),
                             false, true, &high_bands_gain, nullptr),
@@ -56,7 +56,7 @@ TEST(SuppressionGain, BasicGainComputation) {
   R2.fill(0.1f);
   N2.fill(100.f);
   for (int k = 0; k < 10; ++k) {
-    suppression_gain.GetGain(E2, R2, N2, analyzer, false, x, false, true,
+    suppression_gain.GetGain(E2, R2, N2, analyzer, false, false, x, false, true,
                              &high_bands_gain, &g);
   }
   std::for_each(g.begin(), g.end(),
@@ -67,7 +67,7 @@ TEST(SuppressionGain, BasicGainComputation) {
   R2.fill(0.1f);
   N2.fill(0.f);
   for (int k = 0; k < 10; ++k) {
-    suppression_gain.GetGain(E2, R2, N2, analyzer, false, x, false, true,
+    suppression_gain.GetGain(E2, R2, N2, analyzer, false, false, x, false, true,
                              &high_bands_gain, &g);
   }
   std::for_each(g.begin(), g.end(),
@@ -78,14 +78,14 @@ TEST(SuppressionGain, BasicGainComputation) {
   R2.fill(10000000000000.f);
   N2.fill(0.f);
   for (int k = 0; k < 10; ++k) {
-    suppression_gain.GetGain(E2, R2, N2, analyzer, false, x, false, true,
+    suppression_gain.GetGain(E2, R2, N2, analyzer, false, false, x, false, true,
                              &high_bands_gain, &g);
   }
   std::for_each(g.begin(), g.end(),
                 [](float a) { EXPECT_NEAR(0.f, a, 0.001); });
 
   // Verify the functionality for forcing a zero gain.
-  suppression_gain.GetGain(E2, R2, N2, analyzer, false, x, true, true,
+  suppression_gain.GetGain(E2, R2, N2, analyzer, false, false, x, true, true,
                            &high_bands_gain, &g);
   std::for_each(g.begin(), g.end(), [](float a) { EXPECT_FLOAT_EQ(0.f, a); });
   EXPECT_FLOAT_EQ(0.f, high_bands_gain);
