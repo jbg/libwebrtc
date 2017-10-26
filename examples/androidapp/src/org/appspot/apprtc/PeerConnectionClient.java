8442b3e3cc342c8f2c35626dcbb49fe19853e63e
Sami Kalliomäki <sakal@webrtc.org>
2017-10-26 13:51:32 +0200

    Fix/suppress all javac warnings.
    This is done in preparation to make all javac warnings into errors for
    WebRTC targets.
    
    Bug: webrtc:6597
    Change-Id: I402043157bd75943adf0de52111e5a1bb179c6d1

--- examples/androidapp/src/org/appspot/apprtc/PeerConnectionClient.java
+++ examples/androidapp/src/org/appspot/apprtc/PeerConnectionClient.java
@@ -720,6 +720,7 @@ public class PeerConnectionClient {
     return rootEglBase.getEglBaseContext();
   }
 
+  @SuppressWarnings("deprecation") // TODO(sakal): getStats is deprecated.
   private void getStats() {
     if (peerConnection == null || isError) {
       return;
