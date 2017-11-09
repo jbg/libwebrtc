393176b5edf3c66ba8e729814f2763a5e7ff9f6c
Zhi Huang <zhihuang@webrtc.org>
2017-11-09 15:54:29 -0800

    Replaced the SignalSelectedCandidatePairChanged with a new signal.
    |transport overhead| field is added to rtc::NetworkRoute structure.
    
    In PackTransportInternal:
    1. network_route() is added which returns the current network route.
    2. debug_name() is removed.
    3. transport_name() is moved from DtlsTransportInternal and
       IceTransportInternal to PacketTransportInternal.
    
    When the selected candidate pair is changed, the P2PTransportChannel
    will fire the SignalNetworkRouteChanged instead of
    SignalSelectedCandidatePairChanged to upper layers.
    
    The Rtp/SrtpTransport takes the responsibility of calculating the
    transport overhead from the BaseChannel so that the BaseChannel
    doesn't need to depend on P2P layer transports.
    
    Bug:webrtc:7013
    Change-Id: I60d30d785666a50a95052d00bf08f829d8f57e9c

--- p2p/base/p2phelper.h
+++ p2p/base/p2phelper.h
@@ -7,13 +7,21 @@
  *  in the file PATENTS.  All contributing project authors may
  *  be found in the AUTHORS file in the root of the source tree.
  */
+#ifndef P2P_BASE_P2PHELPER_H_
+#define P2P_BASE_P2PHELPER_H_
 
-#include "p2p/base/dtlstransportinternal.h"
+#include <cstdlib>
+#include <string>
 
+// This header contains helper functions used in different types of transports.
 namespace cricket {
 
-DtlsTransportInternal::DtlsTransportInternal() = default;
+// Get the network layer overhead per packet based on the IP address family.
+int GetIpOverhead(int addr_family);
 
-DtlsTransportInternal::~DtlsTransportInternal() = default;
+// Get the transport layer overhead per packet based on the protocol.
+int GetProtocolOverhead(const std::string& protocol);
 
 }  // namespace cricket
+
+#endif  // P2P_BASE_P2PHELPER_H_
