b36c190613b226a49811adc852ed29515829cd7c
Edward Lemur <ehmaldonado@webrtc.org>
2017-12-06 16:27:59 +0100

    test ios.
    Bug:
    Change-Id: Ib00992cce0007e0b5c9274340df1a892f810b0c5

--- test/ios/test_support.mm
+++ test/ios/test_support.mm
@@ -10,9 +10,12 @@
 
 #import <UIKit/UIKit.h>
 
+#include "rtc_base/file.h"
 #include "test/ios/test_support.h"
+#include "test/testsupport/perf_test.h"
 
 #import "sdk/objc/Framework/Classes/Common/RTCUIApplicationStatusObserver.h"
+#include "sdk/objc/Framework/Classes/Common/helpers.h"
 
 // Springboard will kill any iOS app that fails to check in after launch within
 // a given time. Starting a UIApplication before invoking TestSuite::Run
@@ -100,8 +103,22 @@ void InitTestSuite(int (*test_suite)(void), int argc, char *argv[]) {
 }
 
 void RunTestsFromIOSApp() {
+  using webrtc::ios::StdStringFromNSString;
+
   @autoreleasepool {
-    exit(UIApplicationMain(g_argc, g_argv, nil, @"WebRtcUnitTestDelegate"));
+    // Stores data into a json file under the app's document directory.
+    NSString* fileName = @"perf_result.json";
+    NSArray<NSString*>* outputDirectories = NSSearchPathForDirectoriesInDomains(
+        NSDocumentDirectory, NSUserDomainMask, YES);
+    int exitCode = (
+        UIApplicationMain(g_argc, g_argv, nil, @"WebRtcUnitTestDelegate"));
+    if ([outputDirectories count] != 0) {
+      NSString* outputPath =
+          [outputDirectories[0] stringByAppendingPathComponent:fileName];
+
+      webrtc::test::WritePerfResults(StdStringFromNSString(outputPath));
+    }
+    exit(exitCode);
   }
 }
 }  // namespace test
