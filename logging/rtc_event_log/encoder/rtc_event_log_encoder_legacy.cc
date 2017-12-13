1b269b11f523c6970db64b72c363d8c7f3fbdde9
Bjorn Terelius <terelius@webrtc.org>
2017-12-13 15:12:18 +0100

    Add method to encode a batch of RtcEvents.
    Bug:webrtc:8111
    Change-Id: Ieb3ff6b817a6bc068b358d49e6d6df07e3ec8d8b

--- logging/rtc_event_log/encoder/rtc_event_log_encoder_legacy.cc
+++ logging/rtc_event_log/encoder/rtc_event_log_encoder_legacy.cc
@@ -107,6 +107,16 @@ rtclog::VideoReceiveConfig_RtcpMode ConvertRtcpMode(RtcpMode rtcp_mode) {
 }
 }  // namespace
 
+std::string RtcEventLogEncoderLegacy::EncodeBatch(
+    std::deque<std::unique_ptr<RtcEvent>>::const_iterator begin,
+    std::deque<std::unique_ptr<RtcEvent>>::const_iterator end) {
+  std::string encoded_output;
+  for (auto it = begin; it != end; ++it) {
+    encoded_output += Encode(**it);
+  }
+  return encoded_output;
+}
+
 std::string RtcEventLogEncoderLegacy::Encode(const RtcEvent& event) {
   switch (event.GetType()) {
     case RtcEvent::Type::AudioNetworkAdaptation: {
