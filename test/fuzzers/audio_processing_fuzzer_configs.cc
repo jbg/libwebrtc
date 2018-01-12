6ef5d29cd8c9ef7778ca4a19da45fc7b6a4bc50f
Alex Loiko <aleloi@webrtc.org>
2018-01-12 13:59:29 +0100

    AEC-m and AEC-2 fuzzing.
    Going through the coverage of audio_processing_fuzzer, it was noticed
    that it didn't cover AEC-m and AEC-2 code. Therefore this CL adds 2
    fuzzer targets that only fuzz the previous generation echo cancellers.
    
    To avoid code duplication, the APM running code was broken out in a
    new GN target. We have also changed all fuzzing code to use the
    FuzzDataHelper class to avoid manual pointer arithmetic.
    
    Bug: webrtc:7820
    Change-Id: Ifea3266e396b487952a736945577fccea15d0e01

--- test/fuzzers/audio_processing_fuzzer_configs.cc
+++ test/fuzzers/audio_processing_fuzzer_configs.cc
@@ -10,43 +10,35 @@
 
 #include "modules/audio_processing/include/audio_processing.h"
 #include "test/fuzzers/audio_processing_fuzzer.h"
-
-#include "api/optional.h"
+#include "test/fuzzers/fuzz_data_helper.h"
 
 namespace webrtc {
 
-std::unique_ptr<AudioProcessing> CreateAPM(const uint8_t** data,
-                                           size_t* remaining_size) {
+std::unique_ptr<AudioProcessing> CreateAPM(test::FuzzDataHelper* fuzz_data) {
   // Parse boolean values for optionally enabling different
   // configurable public components of APM.
-  auto exp_agc = ParseBool(data, remaining_size);
-  auto exp_ns = ParseBool(data, remaining_size);
-  auto bf = ParseBool(data, remaining_size);
-  auto ef = ParseBool(data, remaining_size);
-  auto raf = ParseBool(data, remaining_size);
-  auto da = ParseBool(data, remaining_size);
-  auto ie = ParseBool(data, remaining_size);
-  auto red = ParseBool(data, remaining_size);
-  auto lc = ParseBool(data, remaining_size);
-  auto hpf = ParseBool(data, remaining_size);
-  auto aec3 = ParseBool(data, remaining_size);
+  bool exp_agc = fuzz_data->ReadOrDefaultValue(true);
+  bool exp_ns = fuzz_data->ReadOrDefaultValue(true);
+  bool bf = fuzz_data->ReadOrDefaultValue(true);
+  bool ef = fuzz_data->ReadOrDefaultValue(true);
+  bool raf = fuzz_data->ReadOrDefaultValue(true);
+  bool da = fuzz_data->ReadOrDefaultValue(true);
+  bool ie = fuzz_data->ReadOrDefaultValue(true);
+  bool red = fuzz_data->ReadOrDefaultValue(true);
+  bool lc = fuzz_data->ReadOrDefaultValue(true);
+  bool hpf = fuzz_data->ReadOrDefaultValue(true);
+  bool aec3 = fuzz_data->ReadOrDefaultValue(true);
 
-  auto use_aec = ParseBool(data, remaining_size);
-  auto use_aecm = ParseBool(data, remaining_size);
-  auto use_agc = ParseBool(data, remaining_size);
-  auto use_ns = ParseBool(data, remaining_size);
-  auto use_le = ParseBool(data, remaining_size);
-  auto use_vad = ParseBool(data, remaining_size);
-  auto use_agc_limiter = ParseBool(data, remaining_size);
-
-  if (!(exp_agc && exp_ns && bf && ef && raf && da && ie && red && lc && hpf &&
-        aec3 && use_aec && use_aecm && use_agc && use_ns && use_le && use_vad &&
-        use_agc_limiter)) {
-    return nullptr;
-  }
+  bool use_aec = fuzz_data->ReadOrDefaultValue(true);
+  bool use_aecm = fuzz_data->ReadOrDefaultValue(true);
+  bool use_agc = fuzz_data->ReadOrDefaultValue(true);
+  bool use_ns = fuzz_data->ReadOrDefaultValue(true);
+  bool use_le = fuzz_data->ReadOrDefaultValue(true);
+  bool use_vad = fuzz_data->ReadOrDefaultValue(true);
+  bool use_agc_limiter = fuzz_data->ReadOrDefaultValue(true);
 
   // Filter out incompatible settings that lead to CHECK failures.
-  if (*use_aecm && *use_aec) {
+  if (use_aecm && use_aec) {
     return nullptr;
   }
 
@@ -55,19 +47,19 @@ std::unique_ptr<AudioProcessing> CreateAPM(const uint8_t** data,
   Config config;
 
   std::unique_ptr<EchoControlFactory> echo_control_factory;
-  if (*aec3) {
+  if (aec3) {
     echo_control_factory.reset(new EchoCanceller3Factory());
   }
 
-  config.Set<ExperimentalAgc>(new ExperimentalAgc(*exp_agc));
-  config.Set<ExperimentalNs>(new ExperimentalNs(*exp_ns));
-  if (*bf) {
+  config.Set<ExperimentalAgc>(new ExperimentalAgc(exp_agc));
+  config.Set<ExperimentalNs>(new ExperimentalNs(exp_ns));
+  if (bf) {
     config.Set<Beamforming>(new Beamforming());
   }
-  config.Set<ExtendedFilter>(new ExtendedFilter(*ef));
-  config.Set<RefinedAdaptiveFilter>(new RefinedAdaptiveFilter(*raf));
-  config.Set<DelayAgnostic>(new DelayAgnostic(*da));
-  config.Set<Intelligibility>(new Intelligibility(*ie));
+  config.Set<ExtendedFilter>(new ExtendedFilter(ef));
+  config.Set<RefinedAdaptiveFilter>(new RefinedAdaptiveFilter(raf));
+  config.Set<DelayAgnostic>(new DelayAgnostic(da));
+  config.Set<Intelligibility>(new Intelligibility(ie));
 
   std::unique_ptr<AudioProcessing> apm(
       AudioProcessingBuilder()
@@ -75,27 +67,29 @@ std::unique_ptr<AudioProcessing> CreateAPM(const uint8_t** data,
           .Create(config));
 
   webrtc::AudioProcessing::Config apm_config;
-  apm_config.residual_echo_detector.enabled = *red;
-  apm_config.level_controller.enabled = *lc;
-  apm_config.high_pass_filter.enabled = *hpf;
+  apm_config.residual_echo_detector.enabled = red;
+  apm_config.level_controller.enabled = lc;
+  apm_config.high_pass_filter.enabled = hpf;
 
   apm->ApplyConfig(apm_config);
 
-  apm->echo_cancellation()->Enable(*use_aec);
-  apm->echo_control_mobile()->Enable(*use_aecm);
-  apm->gain_control()->Enable(*use_agc);
-  apm->noise_suppression()->Enable(*use_ns);
-  apm->level_estimator()->Enable(*use_le);
-  apm->voice_detection()->Enable(*use_vad);
-  apm->gain_control()->enable_limiter(*use_agc_limiter);
+  apm->echo_cancellation()->Enable(use_aec);
+  apm->echo_control_mobile()->Enable(use_aecm);
+  apm->gain_control()->Enable(use_agc);
+  apm->noise_suppression()->Enable(use_ns);
+  apm->level_estimator()->Enable(use_le);
+  apm->voice_detection()->Enable(use_vad);
+  apm->gain_control()->enable_limiter(use_agc_limiter);
 
   return apm;
 }
 
 void FuzzOneInput(const uint8_t* data, size_t size) {
-  auto apm = CreateAPM(&data, &size);
+  test::FuzzDataHelper fuzz_data(rtc::ArrayView<const uint8_t>(data, size));
+  auto apm = CreateAPM(&fuzz_data);
+
   if (apm) {
-    FuzzAudioProcessing(data, size, std::move(apm));
+    FuzzAudioProcessing(&fuzz_data, std::move(apm));
   }
 }
 }  // namespace webrtc
