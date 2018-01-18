94fa68761eef1c209c3226c2d258020c44306176
Sami Kalliomäki <sakal@webrtc.org>
2018-01-18 15:50:11 +0100

    Approach NDK
    Change-Id: I0ef0f6b297b5002322915660d26cca33e91ff05b

--- sdk/android/native_api/base/init.h
+++ sdk/android/native_api/base/init.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2017 The WebRTC project authors. All Rights Reserved.
+ *  Copyright 2018 The WebRTC project authors. All Rights Reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
@@ -8,20 +8,18 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#ifndef SDK_ANDROID_SRC_JNI_VIDEOCODECSTATUS_H_
-#define SDK_ANDROID_SRC_JNI_VIDEOCODECSTATUS_H_
+#ifndef SDK_ANDROID_NATIVE_API_BASE_INIT_H_
+#define SDK_ANDROID_NATIVE_API_BASE_INIT_H_
 
 #include <jni.h>
-#include <stdint.h>
 
 #include "sdk/android/native_api/jni/scoped_java_ref.h"
 
 namespace webrtc {
-namespace jni {
-int32_t JavaToNativeVideoCodecStatus(
-    JNIEnv* env,
-    const JavaRef<jobject>& j_video_codec_status);
-}  // namespace jni
+
+// Initializes global state needed by WebRTC Android NDK.
+void InitAndroid(JavaVM* jvm);
+
 }  // namespace webrtc
 
-#endif  // SDK_ANDROID_SRC_JNI_VIDEOCODECSTATUS_H_
+#endif  // SDK_ANDROID_NATIVE_API_BASE_INIT_H_
