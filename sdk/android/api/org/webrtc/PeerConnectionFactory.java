83f48ebdf1eb452ae73d426a5d794f24e4b1384b
Joachim Bauch <jbauch@webrtc.org>
2018-02-04 18:05:00 +0100

    Use SRTP_AES128_CM_SHA1_80 by default instead of SRTP_AES128_CM_SHA1_32.
    A field has been added to "CryptoOptions" to enable SRTP_AES128_CM_SHA1_32
    from native apps if really necessary.
    
    Bug: webrtc:7670
    Change-Id: I36b6ab3e302fbf3cda2611ff196757e43a56e704
    R=deadbeef@webrtc.org

--- sdk/android/api/org/webrtc/PeerConnectionFactory.java
+++ sdk/android/api/org/webrtc/PeerConnectionFactory.java
@@ -105,6 +105,7 @@ public class PeerConnectionFactory {
     public int networkIgnoreMask;
     public boolean disableEncryption;
     public boolean disableNetworkMonitor;
+    public boolean enableAes128Sha1_32CryptoCipher;
 
     @CalledByNative("Options")
     int getNetworkIgnoreMask() {
@@ -119,6 +120,11 @@ public class PeerConnectionFactory {
     @CalledByNative("Options")
     boolean getDisableNetworkMonitor() {
       return disableNetworkMonitor;
+    }
+
+    @CalledByNative("Options")
+    boolean getEnableAes128Sha1_32CryptoCipher() {
+      return enableAes128Sha1_32CryptoCipher;
     }
   }
 
