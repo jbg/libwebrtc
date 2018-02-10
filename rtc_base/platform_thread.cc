b58d573966d16a94b4155145fa4eb95ebf4251cd
Tommi <tommi@webrtc.org>
2018-02-10 10:44:25 +0100

    Allow PlatformThread to use SequencedTaskChecker
    Change-Id: I85e7943750f54a03ae6569431556b3684f1aa0c6

--- rtc_base/platform_thread.cc
+++ rtc_base/platform_thread.cc
@@ -124,7 +124,7 @@ PlatformThread::PlatformThread(ThreadRunFunction func,
 }
 
 PlatformThread::~PlatformThread() {
-  RTC_DCHECK(thread_checker_.CalledOnValidThread());
+  RTC_DCHECK(sequence_checker_.CalledSequentially());
 #if defined(WEBRTC_WIN)
   RTC_DCHECK(!thread_);
   RTC_DCHECK(!thread_id_);
@@ -149,7 +149,7 @@ void* PlatformThread::StartThread(void* param) {
 #endif  // defined(WEBRTC_WIN)
 
 void PlatformThread::Start() {
-  RTC_DCHECK(thread_checker_.CalledOnValidThread());
+  RTC_DCHECK(sequence_checker_.CalledSequentially());
   RTC_DCHECK(!thread_) << "Thread already started?";
 #if defined(WEBRTC_WIN)
   stop_ = false;
@@ -170,7 +170,7 @@ void PlatformThread::Start() {
 }
 
 bool PlatformThread::IsRunning() const {
-  RTC_DCHECK(thread_checker_.CalledOnValidThread());
+  RTC_DCHECK(sequence_checker_.CalledSequentially());
 #if defined(WEBRTC_WIN)
   return thread_ != nullptr;
 #else
@@ -187,7 +187,7 @@ PlatformThreadRef PlatformThread::GetThreadRef() const {
 }
 
 void PlatformThread::Stop() {
-  RTC_DCHECK(thread_checker_.CalledOnValidThread());
+  RTC_DCHECK(sequence_checker_.CalledSequentially());
   if (!IsRunning())
     return;
 
@@ -285,12 +285,12 @@ bool PlatformThread::SetPriority(ThreadPriority priority) {
   if (run_function_) {
     // The non-deprecated way of how this function gets called, is that it must
     // be called on the worker thread itself.
-    RTC_DCHECK(!thread_checker_.CalledOnValidThread());
+    RTC_DCHECK(!sequence_checker_.CalledSequentially());
     RTC_DCHECK(spawned_thread_checker_.CalledOnValidThread());
   } else {
     // In the case of deprecated use of this method, it must be called on the
     // same thread as the PlatformThread object is constructed on.
-    RTC_DCHECK(thread_checker_.CalledOnValidThread());
+    RTC_DCHECK(sequence_checker_.CalledSequentially());
     RTC_DCHECK(IsRunning());
   }
 #endif
@@ -348,7 +348,7 @@ bool PlatformThread::SetPriority(ThreadPriority priority) {
 
 #if defined(WEBRTC_WIN)
 bool PlatformThread::QueueAPC(PAPCFUNC function, ULONG_PTR data) {
-  RTC_DCHECK(thread_checker_.CalledOnValidThread());
+  RTC_DCHECK(sequence_checker_.CalledSequentially());
   RTC_DCHECK(IsRunning());
 
   return QueueUserAPC(function, thread_, data) != FALSE;
