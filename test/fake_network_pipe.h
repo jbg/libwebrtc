6723665f48de89187224a384e562a7b1ccdeb505
Erik Språng <sprang@webrtc.org>
2018-02-16 15:48:06 +0100

    Add ability to emulate degraded network in Call via field trial
    This is especially useful in Chrome, allowing use to emulate network
    conditions in incoming or outgoing media without the need for platform
    specific tools or hacks. It also doesn't interfere with the rest of the
    network traffic.
    
    Also includes some refactorings and a fix for crash on exit in loopback
    tests.
    
    Bug: None
    Change-Id: I2656a2d4218acbe7f8ffd669de19a02275735438

--- test/fake_network_pipe.h
+++ test/fake_network_pipe.h
@@ -11,13 +11,16 @@
 #ifndef TEST_FAKE_NETWORK_PIPE_H_
 #define TEST_FAKE_NETWORK_PIPE_H_
 
-#include <string.h>
+#include <deque>
 #include <map>
 #include <memory>
 #include <queue>
-#include <set>
+#include <string>
 
+#include "api/call/transport.h"
+#include "call/call.h"
 #include "common_types.h"  // NOLINT(build/include)
+#include "modules/include/module.h"
 #include "rtc_base/constructormagic.h"
 #include "rtc_base/criticalsection.h"
 #include "rtc_base/random.h"
@@ -31,34 +34,50 @@ enum class MediaType;
 
 class NetworkPacket {
  public:
-  NetworkPacket(const uint8_t* data,
-                size_t length,
+  NetworkPacket(rtc::CopyOnWriteBuffer packet,
                 int64_t send_time,
-                int64_t arrival_time)
-      : data_(new uint8_t[length]),
-        data_length_(length),
-        send_time_(send_time),
-        arrival_time_(arrival_time) {
-    memcpy(data_.get(), data, length);
-  }
+                int64_t arrival_time,
+                rtc::Optional<PacketOptions> packet_options,
+                bool is_rtcp,
+                MediaType media_type_,
+                rtc::Optional<PacketTime> packet_time_);
+  // Disallow copy constructor (no deep copies of |data_|).
+  NetworkPacket(const NetworkPacket&) = delete;
+  // Allow move constructor/assignment, so that we can use in stl containers.
+  NetworkPacket(NetworkPacket&&);
+  NetworkPacket& operator=(NetworkPacket&&);
 
-  uint8_t* data() const { return data_.get(); }
-  size_t data_length() const { return data_length_; }
+  const uint8_t* data() const { return packet_.data(); }
+  size_t data_length() const { return packet_.size(); }
+  rtc::CopyOnWriteBuffer* raw_packet() { return &packet_; }
   int64_t send_time() const { return send_time_; }
   int64_t arrival_time() const { return arrival_time_; }
   void IncrementArrivalTime(int64_t extra_delay) {
     arrival_time_ += extra_delay;
   }
+  PacketOptions packet_options() const {
+    return packet_options_.value_or(PacketOptions());
+  }
+  bool is_rtcp() const { return is_rtcp_; }
+  MediaType media_type() const { return media_type_; }
+  PacketTime packet_time() const { return packet_time_.value_or(PacketTime()); }
 
  private:
-  // The packet data.
-  std::unique_ptr<uint8_t[]> data_;
-  // Length of data_.
-  size_t data_length_;
+  rtc::CopyOnWriteBuffer packet_;
   // The time the packet was sent out on the network.
-  const int64_t send_time_;
+  int64_t send_time_;
   // The time the packet should arrive at the receiver.
   int64_t arrival_time_;
+  // If using a Transport for outgoing degradation, populate with either
+  // PacketOptions (transport-wide sequence number) for RTP.
+  rtc::Optional<PacketOptions> packet_options_;
+  bool is_rtcp_;
+  // If using a PacketReceiver for incoming degradation, populate with
+  // appropriate MediaType and PacketTime. This type/timing will be kept and
+  // forwarded. The PacketTime might be altered to reflect time spent in fake
+  // network pipe.
+  MediaType media_type_;
+  rtc::Optional<PacketTime> packet_time_;
 };
 
 class Demuxer {
@@ -78,6 +97,7 @@ class DemuxerImpl final : public Demuxer {
                      const PacketTime& packet_time) override;
 
  private:
+  rtc::CriticalSection lock_;
   PacketReceiver* packet_receiver_;
   const std::map<uint8_t, MediaType> payload_type_map_;
   RTC_DISALLOW_COPY_AND_ASSIGN(DemuxerImpl);
@@ -87,7 +107,7 @@ class DemuxerImpl final : public Demuxer {
 // capacity and adding an extra transport delay in addition to the capacity
 // introduced delay.
 
-class FakeNetworkPipe {
+class FakeNetworkPipe : public Transport, public PacketReceiver, public Module {
  public:
   struct Config {
     Config() {}
@@ -107,6 +127,10 @@ class FakeNetworkPipe {
     int avg_burst_loss_length = -1;
   };
 
+  // Use this constructor if you plan to insert packets using DeliverPacket().
+  FakeNetworkPipe(Clock* clock, const FakeNetworkPipe::Config& config);
+
+  // Use these constructors if you plan to insert packets using SendPacket().
   FakeNetworkPipe(Clock* clock,
                   const FakeNetworkPipe::Config& config,
                   std::unique_ptr<Demuxer> demuxer);
@@ -114,21 +138,48 @@ class FakeNetworkPipe {
                   const FakeNetworkPipe::Config& config,
                   std::unique_ptr<Demuxer> demuxer,
                   uint64_t seed);
+
+  // Use this constructor if you plan to insert packets using SendRt[c?]p().
+  FakeNetworkPipe(Clock* clock,
+                  const FakeNetworkPipe::Config& config,
+                  Transport* transport);
+
   virtual ~FakeNetworkPipe();
 
   // Sets a new configuration. This won't affect packets already in the pipe.
   void SetConfig(const FakeNetworkPipe::Config& config);
 
-  // Sends a new packet to the link.
+  // Sends a new packet to the link. When/if packets are delivered, they will
+  // be passed to the receiver instance given in SetReceiver(). This method
+  // should only be used if a Demuxer was provided in the constructor.
   virtual void SendPacket(const uint8_t* packet, size_t packet_length);
 
   // Must not be called in parallel with SendPacket or Process.
   void SetReceiver(PacketReceiver* receiver);
 
+  // Implements Transport interface. When/if packets are delivered, they will
+  // be passed to the transport instance given in SetReceiverTransport(). These
+  // methods should only be called if a Transport instance we provided in the
+  // constructor.
+  bool SendRtp(const uint8_t* packet,
+               size_t length,
+               const PacketOptions& options) override;
+  bool SendRtcp(const uint8_t* packet, size_t length) override;
+
+  // Implements the PacketReceiver interface. When/if packets are delivered,
+  // they will be passed directly to the receiver instance given in
+  // SetReceiver(), without passing through a Demuxer. The receive time in
+  // PacketTime will be increased by the amount of time the packet spent in the
+  // fake network pipe.
+  PacketReceiver::DeliveryStatus DeliverPacket(
+      MediaType media_type,
+      rtc::CopyOnWriteBuffer packet,
+      const PacketTime& packet_time) override;
+
   // Processes the network queues and trigger PacketReceiver::IncomingPacket for
   // packets ready to be delivered.
-  virtual void Process();
-  int64_t TimeUntilNextProcess() const;
+  void Process() override;
+  int64_t TimeUntilNextProcess() override;
 
   // Get statistics.
   float PercentageLoss();
@@ -137,21 +188,23 @@ class FakeNetworkPipe {
   size_t sent_packets() { return sent_packets_; }
 
  protected:
+  // Returns true if enqueued, or false if packet was dropped.
+  bool EnqueuePacket(rtc::CopyOnWriteBuffer packet,
+                     rtc::Optional<PacketOptions> options,
+                     bool is_rtcp,
+                     MediaType media_type,
+                     rtc::Optional<PacketTime> packet_time);
+  void DeliverPacket(NetworkPacket* packet);
+
   Clock* const clock_;
   rtc::CriticalSection lock_;
   const std::unique_ptr<Demuxer> demuxer_;
-  std::queue<NetworkPacket*> capacity_link_;
+  PacketReceiver* receiver_;
+  Transport* const transport_;
+  std::queue<NetworkPacket> capacity_link_;
   Random random_;
 
-  // Since we need to access both the packet with the earliest and latest
-  // arrival time we need to use a multiset to keep all packets sorted,
-  // hence, we cannot use a priority queue.
-  struct PacketArrivalTimeComparator {
-    bool operator()(const NetworkPacket* p1, const NetworkPacket* p2) {
-      return p1->arrival_time() < p2->arrival_time();
-    }
-  };
-  std::multiset<NetworkPacket*, PacketArrivalTimeComparator> delay_link_;
+  std::deque<NetworkPacket> delay_link_;
 
   // Link configuration.
   Config config_;
