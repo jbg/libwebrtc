88f69165fc148966bd91722c602174cbac6446b8
Tommi <tommi@webrtc.org>
2018-02-19 21:51:30 +0100

    [ Reland of https://codereview.webrtc.org/2764573002/ ]
    Deliver video frames on Android, on the decode thread.
    
    VideoCoding
    * Adding a method for polling for frames on Android only until the capture implementation takes care of this (longer term plan).
    
    CodecDatabase
    * Add an accessor for the current decoder
    * Use std::unique_ptr<> for ownership.
    * Remove "Release()" and "ReleaseDecoder()". Instead just delete.
    * Remove |friend| relationship between CodecDatabase and VCMGenericDecoder.
    
    VCMDecodedFrameCallback
    * DCHECKs for thread correctness.
    * Remove |lock_| now that a threading model has been established and verified.
    
    VCMGenericDecoder
    * All methods now have thread checks.
    * Variable access associated with thread checkers.
    
    VideoReceiver
    * Added two notification methods, DecoderThreadStarting() and DecoderThreadStopped()
      * Allows us to establish a period when the decoder thread is not running and it is safe to modify variables such as callbacks, that are only read when the decoder thread is running.
      * Allows us to DCHECK thread guarantees.
      * Allows synchronizing callbacks from the module process thread and have them only active while the decoder thread is running.
      * The above, allows us to establish two modes for the thread, single-threaded-mutable and multi-threaded-const.
      * Using that knowledge, we can remove |receive_crit_| as well as locking for a number of member variables.
    
    MediaCodecVideoDecoder
    * Removed frame polling code from this class, since this is now done from the root thread function in VideoReceiveStream.
    
    VideoReceiveStream
    * On Android: Polls for decoded frames every 10ms (same interval as previously in MediaCodecVideoDecoder)
    * [Un]Registers the |video_receiver_| with the module thread only around the time the decoder thread is started/stopped.
    * Notifies the receiver of start/stop events of the decoder thread.
    * Changed the decoder thread to use the new PlatformThread callback type.
    
    BUG=webrtc:7361, 695438
    
    patch from issue 2811963004 at patchset 490001 (http://crrev.com/2811963004#ps490001)
    
    Change-Id: I7c3017b51ee53acf080c87cb99e01b42bb1c85e5

--- video/video_receive_stream.cc
+++ video/video_receive_stream.cc
@@ -35,6 +35,7 @@
 #include "rtc_base/location.h"
 #include "rtc_base/logging.h"
 #include "rtc_base/ptr_util.h"
+#include "rtc_base/timeutils.h"
 #include "rtc_base/trace_event.h"
 #include "system_wrappers/include/clock.h"
 #include "system_wrappers/include/field_trial.h"
@@ -224,6 +225,7 @@ void VideoReceiveStream::Start() {
   call_stats_->RegisterStatsObserver(video_stream_decoder_.get());
 
   process_thread_->RegisterModule(&video_receiver_, RTC_FROM_HERE);
+  video_receiver_.DecoderThreadStarting();
 
   // Start the decode thread
   decode_thread_.Start();
@@ -246,6 +248,7 @@ void VideoReceiveStream::Stop() {
     video_receiver_.TriggerDecoderShutdown();
 
     decode_thread_.Stop();
+    video_receiver_.DecoderThreadStopped();
     // Deregister external decoders so they are no longer running during
     // destruction. This effectively stops the VCM since the decoder thread is
     // stopped, the VCM is deregistered and no asynchronous decoder threads are
@@ -416,13 +419,28 @@ bool VideoReceiveStream::Decode() {
   std::unique_ptr<video_coding::FrameObject> frame;
   // TODO(philipel): Call NextFrame with |keyframe_required| argument when
   //                 downstream project has been fixed.
-  video_coding::FrameBuffer::ReturnReason res =
-      frame_buffer_->NextFrame(wait_ms, &frame);
+  video_coding::FrameBuffer::ReturnReason res;
 
-  if (res == video_coding::FrameBuffer::ReturnReason::kStopped) {
-    video_receiver_.DecodingStopped();
+#if defined(WEBRTC_ANDROID)
+  // This is a temporary workaround for video capture on Android in order to
+  // deliver asynchronously delivered frames, on the decoder thread.
+  // More details here:
+  // https://bugs.chromium.org/p/webrtc/issues/detail?id=7361
+  static const int kPollIntervalMs = 10;
+  int time_remaining = kMaxWaitForFrameMs;
+  do {
+    res = frame_buffer_->NextFrame(kPollIntervalMs, &frame);
+    if (res != video_coding::FrameBuffer::ReturnReason::kTimeout)
+      break;
+    time_remaining -= kPollIntervalMs;
+    video_receiver_.PollDecodedFrames();
+  } while (time_remaining > 0);
+#else
+  res = frame_buffer_->NextFrame(kMaxWaitForFrameMs, &frame);
+#endif
+
+  if (res == video_coding::FrameBuffer::ReturnReason::kStopped)
     return false;
-  }
 
   if (frame) {
     int64_t now_ms = clock_->TimeInMilliseconds();
@@ -470,6 +488,7 @@ bool VideoReceiveStream::Decode() {
       RequestKeyFrame();
     }
   }
+
   return true;
 }
 }  // namespace internal
