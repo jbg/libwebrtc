3abd5765b171235652398d63514cecbf2deff9c4
Dino Radaković <dinor@webrtc.org>
2018-02-20 17:34:09 +0100

    Add NOLINT(build/include) for #include "common_types.h".
    Clang-format rtp_sender.cc
    
    
    Add application_data field to RtpPacketToSend, and make sure it gets passed through to Transport::SendRtp.
    
    
    Add application_data{,_size} fields to PacketOptions.
    
    Change-Id: I70be3ed8039ba7fcc2298c3dc3bbc61497024505

--- modules/rtp_rtcp/source/rtp_packet_to_send.h
+++ modules/rtp_rtcp/source/rtp_packet_to_send.h
@@ -10,6 +10,9 @@
 #ifndef MODULES_RTP_RTCP_SOURCE_RTP_PACKET_TO_SEND_H_
 #define MODULES_RTP_RTCP_SOURCE_RTP_PACKET_TO_SEND_H_
 
+#include <vector>
+
+#include "common_types.h"  // NOLINT(build/include)
 #include "modules/rtp_rtcp/source/rtp_header_extensions.h"
 #include "modules/rtp_rtcp/source/rtp_packet.h"
 
@@ -29,6 +32,16 @@ class RtpPacketToSend : public RtpPacket {
   int64_t capture_time_ms() const { return capture_time_ms_; }
 
   void set_capture_time_ms(int64_t time) { capture_time_ms_ = time; }
+
+  // Additional data bound to the RTP packet for use in application code,
+  // outside of WebRTC.
+  rtc::ArrayView<const uint8_t> application_data() const {
+    return application_data_;
+  }
+
+  void set_application_data(rtc::ArrayView<const uint8_t> data) {
+    application_data_.assign(data.begin(), data.end());
+  }
 
   void set_packetization_finish_time_ms(int64_t time) {
     SetExtension<VideoTimingExtension>(
@@ -56,6 +69,7 @@ class RtpPacketToSend : public RtpPacket {
 
  private:
   int64_t capture_time_ms_ = 0;
+  std::vector<uint8_t> application_data_;
 };
 
 }  // namespace webrtc
