1ce9498ba7ec95f12896f45f8e035e7b0f7f26be
Seth Hampson <shampson@webrtc.org>
2018-03-01 18:56:51 -0800

    Changes name of RtpTransceiverInit's stream_labels to stream_ids.
    The naming convention according to the spec is stream id, not stream
    labels.Changing things now to be spec compliant, before it is widely
    used. This also includes changes to objective C wrapper code to be in
    sync with the change.
    
    Bug: webrtc:7932
    Change-Id: I5705e6d8a647aaeed860316466a7320132f24b00

--- api/rtptransceiverinterface.h
+++ api/rtptransceiverinterface.h
@@ -41,7 +41,7 @@ struct RtpTransceiverInit final {
 
   // The added RtpTransceiver will be added to these streams.
   // TODO(bugs.webrtc.org/7600): Not implemented.
-  std::vector<std::string> stream_labels;
+  std::vector<std::string> stream_ids;
 
   // TODO(bugs.webrtc.org/7600): Not implemented.
   std::vector<RtpEncodingParameters> send_encodings;
