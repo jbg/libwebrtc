6075486547c05cbcc64650f765629cd6815a626b
Per Åhgren <peah@webrtc.org>
2018-03-06 09:28:58 +0100

    Separating the AEC3 suppressor gain rampup behavior for call startup and in-call resets
    This CL introduces a different rampup behavir for the call startup and after resets
    that may occur due to delay changes, clock-drift and audio path glitches.
    
    Bug: chromium:819111, webrtc:8979
    Change-Id: Ied1d7896be7f0c69aa6deb61475117021ca6ab09

--- modules/audio_processing/aec3/aec_state.h
+++ modules/audio_processing/aec3/aec_state.h
@@ -26,6 +26,7 @@
 #include "modules/audio_processing/aec3/erl_estimator.h"
 #include "modules/audio_processing/aec3/erle_estimator.h"
 #include "modules/audio_processing/aec3/render_buffer.h"
+#include "modules/audio_processing/aec3/suppression_gain_limiter.h"
 #include "rtc_base/constructormagic.h"
 
 namespace webrtc {
@@ -91,7 +92,9 @@ class AecState {
   float ReverbDecay() const { return reverb_decay_; }
 
   // Returns the upper limit for the echo suppression gain.
-  float SuppressionGainLimit() const { return suppressor_gain_limit_; }
+  float SuppressionGainLimit() const {
+    return suppression_gain_limiter_.Limit();
+  }
 
   // Returns whether the echo in the capture signal is audible.
   bool InaudibleEcho() const { return echo_audibility_.InaudibleEcho(); }
@@ -179,6 +182,7 @@ class AecState {
   bool filter_has_had_time_to_converge_ = false;
   bool initial_state_ = true;
   const float gain_rampup_increase_;
+  SuppressionGainUpperLimiter suppression_gain_limiter_;
 
   RTC_DISALLOW_COPY_AND_ASSIGN(AecState);
 };
