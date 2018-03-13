fe77a72c0caba21e634c7a0f951a1108ee9ee622
Sebastian Jansson <srte@webrtc.org>
2018-03-13 17:29:14 +0100

    Communicate availability of transport feedback.
    This change add indication of per packet feedback availability to
    SendSideCongestionController. This allows SSCC to provide different
    functionality depending on whether per packet feedback is available.
    This prepares for an upcoming CL where BBR is introduced. This is
    needed since BBR depends on packet feedback being available.
    
    Bug: webrtc:8415
    Change-Id: Ic31fc043e69dd3cc4eadc365bd1154e04a138390

--- video/video_send_stream.cc
+++ video/video_send_stream.cc
@@ -717,6 +717,7 @@ VideoSendStreamImpl::VideoSendStreamImpl(
   // If send-side BWE is enabled, check if we should apply updated probing and
   // pacing settings.
   if (TransportSeqNumExtensionConfigured(*config_)) {
+    transport->SetPerPacketFeedbackAvailable(true);
     rtc::Optional<AlrExperimentSettings> alr_settings;
     if (content_type == VideoEncoderConfig::ContentType::kScreen) {
       alr_settings = AlrExperimentSettings::CreateFromFieldTrial(
