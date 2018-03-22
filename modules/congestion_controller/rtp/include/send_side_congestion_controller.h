77835047c62aef4b842eb14f815d4e7eef7134c4
Sebastian Jansson <srte@webrtc.org>
2018-03-22 15:33:42 +0100

    Adding interfaces for network controller subsets.
    Adding interfaces representing different kinds of network controllers.
    This prepares for a later CL introducing the BBR network controller
    which will only implement the feedback based interface.
    
    Bug: webrtc:8415
    Change-Id: If8f8b02e589ac6b05bc9ff4f5d0490da6803a9e4

--- modules/congestion_controller/rtp/include/send_side_congestion_controller.h
+++ modules/congestion_controller/rtp/include/send_side_congestion_controller.h
@@ -160,7 +160,8 @@ class SendSideCongestionController
   // TODO(srte): Move all access to feedback adapter to task queue.
   TransportFeedbackAdapter transport_feedback_adapter_;
 
-  const std::unique_ptr<NetworkControllerFactoryInterface> controller_factory_;
+  const std::unique_ptr<CombinedNetworkControllerFactoryInterface>
+      combined_controller_factory_ RTC_GUARDED_BY(task_queue_ptr_);
 
   const std::unique_ptr<PacerController> pacer_controller_
       RTC_GUARDED_BY(task_queue_ptr_);
@@ -168,7 +169,7 @@ class SendSideCongestionController
   std::unique_ptr<send_side_cc_internal::ControlHandler> control_handler_
       RTC_GUARDED_BY(task_queue_ptr_);
 
-  std::unique_ptr<NetworkControllerInterface> controller_
+  std::unique_ptr<CombinedNetworkControllerInterface> controller_
       RTC_GUARDED_BY(task_queue_ptr_);
 
   TimeDelta process_interval_ RTC_GUARDED_BY(task_queue_ptr_);
