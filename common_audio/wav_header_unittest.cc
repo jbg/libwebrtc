a3f2ea2fc361e9c6acc97b3a3f7e7d1e3f8f73d7
Alessio Bazzica <alessiob@webrtc.org>
2018-04-05 13:01:34 +0200

    Tolerate optional chunks in WAV files
    Wave files may contain optional chunks, such as a metadata one.
    This CL makes WavReader tolerant to such chunks - it just ignores them.
    For more details on the Wave format, please refer to
    https://sites.google.com/site/musicgapi/technical-documents/wav-file-format.
    
    Bug: webrtc:8762
    Change-Id: Ie0e19dea75661808e7781f51faa1d0f0affeb3e1

--- common_audio/wav_header_unittest.cc
+++ common_audio/wav_header_unittest.cc
@@ -37,7 +37,7 @@ class ReadableWavBuffer : public ReadableWav {
       EXPECT_EQ(size_, pos_);
   }
 
-  virtual size_t Read(void* buf, size_t num_bytes) {
+  size_t Read(void* buf, size_t num_bytes) override {
     // Verify we don't try to read outside of a properly sized header.
     if (size_ >= kWavHeaderSize)
       EXPECT_GE(size_, pos_ + num_bytes);
@@ -54,6 +54,27 @@ class ReadableWavBuffer : public ReadableWav {
     memcpy(buf, &buf_[pos_], num_bytes);
     pos_ += num_bytes;
     return num_bytes;
+  }
+
+  bool Eof() const override { return pos_ == size_; }
+
+  bool SeekForward(uint32_t num_bytes) override {
+    // Verify we don't try to read outside of a properly sized header.
+    if (size_ >= kWavHeaderSize)
+      EXPECT_GE(size_, pos_ + num_bytes);
+    EXPECT_FALSE(buf_exhausted_);
+
+    const size_t bytes_remaining = size_ - pos_;
+    if (num_bytes > bytes_remaining) {
+      // Error: cannot seek beyond EOF.
+      return false;
+    }
+    if (num_bytes == bytes_remaining) {
+      // There should not be another read attempt after this point.
+      buf_exhausted_ = true;
+    }
+    pos_ += num_bytes;
+    return true;
   }
 
  private:
@@ -117,7 +138,7 @@ TEST(WavHeaderTest, ReadWavHeaderWithErrors) {
       'd', 'a', 't', 'a',
       0x99, 0xd0, 0x5b, 0x07,  // size of payload: 123457689
     };
-    ReadableWavBuffer r(kBadRiffID, sizeof(kBadRiffID));
+    ReadableWavBuffer r(kBadRiffID, sizeof(kBadRiffID), false);
     EXPECT_FALSE(
         ReadWavHeader(&r, &num_channels, &sample_rate, &format,
                       &bytes_per_sample, &num_samples));
@@ -288,8 +309,8 @@ TEST(WavHeaderTest, WriteAndReadWavHeader) {
 TEST(WavHeaderTest, ReadAtypicalWavHeader) {
   static const uint8_t kBuf[] = {
     'R', 'I', 'F', 'F',
-    0x3d, 0xd1, 0x5b, 0x07,  // size of whole file - 8 + an extra 128 bytes of
-                             // "metadata": 123457689 + 44 - 8 + 128. (atypical)
+    0xbf, 0xd0, 0x5b, 0x07,  // size of whole file - 8 + extra 2 bytes of zero
+                             // extension: 123457689 + 44 - 8 + 2. (atypical)
     'W', 'A', 'V', 'E',
     'f', 'm', 't', ' ',
     18, 0, 0, 0,  // size of fmt block (with an atypical extension size field)
@@ -318,6 +339,77 @@ TEST(WavHeaderTest, ReadAtypicalWavHeader) {
   EXPECT_EQ(kWavFormatALaw, format);
   EXPECT_EQ(1u, bytes_per_sample);
   EXPECT_EQ(123457689u, num_samples);
+}
+
+// Try reading a valid WAV header which contains an optional chunk and make sure
+// it's parsed OK.
+TEST(WavHeaderTest, ReadWavHeaderWithOptionalChunk) {
+  static const uint8_t kBuf[] = {
+    'R', 'I', 'F', 'F',
+    0xcd, 0xd0, 0x5b, 0x07,  // Size of whole file - 8 + an extra 16 bytes of
+                             // "metadata" (8 bytes header, 16 bytes payload):
+                             // 123457689 + 44 - 8 + 16.
+    'W', 'A', 'V', 'E',
+    'f', 'm', 't', ' ',
+    16, 0, 0, 0,  // Size of fmt block.
+    6, 0,  // Format: A-law (6).
+    17, 0,  // Channels: 17.
+    0x39, 0x30, 0, 0,  // Sample rate: 12345.
+    0xc9, 0x33, 0x03, 0,  // Byte rate: 1 * 17 * 12345.
+    17, 0,  // Block align: NumChannels * BytesPerSample.
+    8, 0,  // Bits per sample: 1 * 8.
+    'L', 'I', 'S', 'T',  // Metadata chunk ID.
+    16, 0, 0, 0,  // Metadata chunk payload size.
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // Metadata (16 bytes).
+    'd', 'a', 't', 'a',
+    0x99, 0xd0, 0x5b, 0x07,  // Size of payload: 123457689.
+  };
+
+  size_t num_channels = 0;
+  int sample_rate = 0;
+  WavFormat format = kWavFormatPcm;
+  size_t bytes_per_sample = 0;
+  size_t num_samples = 0;
+  ReadableWavBuffer r(kBuf, sizeof(kBuf));
+  EXPECT_TRUE(
+      ReadWavHeader(&r, &num_channels, &sample_rate, &format,
+                    &bytes_per_sample, &num_samples));
+  EXPECT_EQ(17u, num_channels);
+  EXPECT_EQ(12345, sample_rate);
+  EXPECT_EQ(kWavFormatALaw, format);
+  EXPECT_EQ(1u, bytes_per_sample);
+  EXPECT_EQ(123457689u, num_samples);
+}
+
+// Try reading an invalid WAV header which has the the data chunk before the
+// format one and make sure it's not parsed.
+TEST(WavHeaderTest, ReadWavHeaderWithDataBeforeFormat) {
+  static const uint8_t kBuf[] = {
+    'R', 'I', 'F', 'F',
+    52, 0, 0, 0,  // Size of whole file - 8: 16 + 44 - 8.
+    'W', 'A', 'V', 'E',
+    'd', 'a', 't', 'a',
+    16, 0, 0, 0,  // Data chunk payload size.
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // Data 16 bytes.
+    'f', 'm', 't', ' ',
+    16, 0, 0, 0,  // Size of fmt block.
+    6, 0,  // Format: A-law (6).
+    1, 0,  // Channels: 1.
+    60, 0, 0, 0,  // Sample rate: 60.
+    60, 0, 0, 0,  // Byte rate: 1 * 1 * 60.
+    1, 0,  // Block align: NumChannels * BytesPerSample.
+    8, 0,  // Bits per sample: 1 * 8.
+  };
+
+  size_t num_channels = 0;
+  int sample_rate = 0;
+  WavFormat format = kWavFormatPcm;
+  size_t bytes_per_sample = 0;
+  size_t num_samples = 0;
+  ReadableWavBuffer r(kBuf, sizeof(kBuf), false);
+  EXPECT_FALSE(
+      ReadWavHeader(&r, &num_channels, &sample_rate, &format,
+                    &bytes_per_sample, &num_samples));
 }
 
 }  // namespace webrtc
