a08e58b8a5012c726d39270c1900669cea965ccc
Sebastian Jansson <srte@webrtc.org>
2018-04-13 15:29:23 +0200

    Moves data rate related operations to data_rate.h
    This reduces the number of files.
    
    Bug: None
    Change-Id: Ia32b7b7cc3260fbecc2b9a3c75723dd4a76c6d5b

--- modules/congestion_controller/network_control/units/data_rate.cc
+++ modules/congestion_controller/network_control/units/data_rate.cc
@@ -13,6 +13,30 @@
 #include "rtc_base/strings/string_builder.h"
 
 namespace webrtc {
+DataRate operator/(const DataSize& size, const TimeDelta& duration) {
+  RTC_DCHECK(size.bytes() < std::numeric_limits<int64_t>::max() / 1000000)
+      << "size is too large, size: " << size.bytes() << " is not less than "
+      << std::numeric_limits<int64_t>::max() / 1000000;
+  int64_t bytes_per_sec = size.bytes() * 1000000 / duration.us();
+  return DataRate::bytes_per_second(bytes_per_sec);
+}
+
+TimeDelta operator/(const DataSize& size, const DataRate& rate) {
+  RTC_DCHECK(size.bytes() < std::numeric_limits<int64_t>::max() / 1000000)
+      << "size is too large, size: " << size.bytes() << " is not less than "
+      << std::numeric_limits<int64_t>::max() / 1000000;
+  int64_t microseconds = size.bytes() * 1000000 / rate.bytes_per_second();
+  return TimeDelta::us(microseconds);
+}
+
+DataSize operator*(const DataRate& rate, const TimeDelta& duration) {
+  int64_t micro_bytes = rate.bytes_per_second() * duration.us();
+  return DataSize::bytes((micro_bytes + 500000) / 1000000);
+}
+
+DataSize operator*(const TimeDelta& duration, const DataRate& rate) {
+  return rate * duration;
+}
 
 std::string ToString(const DataRate& value) {
   char buf[64];
--- modules/congestion_controller/network_control/units/data_rate_unittest.cc
+++ modules/congestion_controller/network_control/units/data_rate_unittest.cc
@@ -72,5 +72,18 @@ TEST(DataRateTest, MathOperations) {
   EXPECT_EQ((size_a * kInt32Value).bytes_per_second(), kValueA * kInt32Value);
   EXPECT_EQ((size_a * kFloatValue).bytes_per_second(), kValueA * kFloatValue);
 }
+
+TEST(UnitConversionTest, DataRateAndDataSizeAndTimeDelta) {
+  const int64_t kValueA = 5;
+  const int64_t kValueB = 450;
+  const int64_t kValueC = 45000;
+  const TimeDelta delta_a = TimeDelta::seconds(kValueA);
+  const DataRate rate_b = DataRate::bytes_per_second(kValueB);
+  const DataSize size_c = DataSize::bytes(kValueC);
+  EXPECT_EQ((delta_a * rate_b).bytes(), kValueA * kValueB);
+  EXPECT_EQ((rate_b * delta_a).bytes(), kValueA * kValueB);
+  EXPECT_EQ((size_c / delta_a).bytes_per_second(), kValueC / kValueA);
+  EXPECT_EQ((size_c / rate_b).s(), kValueC / kValueB);
+}
 }  // namespace test
 }  // namespace webrtc
