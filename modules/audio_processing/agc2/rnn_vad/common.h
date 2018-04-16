440a2c62c22d4f0f20d2e7d74a056317edaedea1
Alessio Bazzica <alessiob@webrtc.org>
2018-04-16 17:14:44 +0200

    AGC2 RNN VAD: initial build targets
    rnn_vad_tool is an executable that reads a wav file of any sample rate
    compatible with 10 ms frames that are resampled and, when the VAD is
    fully landed, will process the resampled frames to compute the VAD
    probability.
    
    Bug: webrtc:9076
    Change-Id: I810c08acfa1adf2029e3baac2adda3045ae5214a

---
+++ modules/audio_processing/agc2/rnn_vad/common.h
@@ -0,0 +1,22 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef MODULES_AUDIO_PROCESSING_AGC2_RNN_VAD_COMMON_H_
+#define MODULES_AUDIO_PROCESSING_AGC2_RNN_VAD_COMMON_H_
+
+namespace webrtc {
+namespace rnn_vad {
+
+// TODO(bugs.chromium.org/9076): Add code here.
+
+}  // namespace rnn_vad
+}  // namespace webrtc
+
+#endif  // MODULES_AUDIO_PROCESSING_AGC2_RNN_VAD_COMMON_H_
