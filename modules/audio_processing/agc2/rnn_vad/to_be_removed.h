29a495f81e8ed919d58963a79fd9d27ed18622cb
Alessio Bazzica <alessiob@webrtc.org>
2018-04-16 18:08:58 +0200

    AGC2 RNN VAD: initial build targets
    rnn_vad_tool is an executable that reads a wav file of any sample rate
    compatible with 10 ms frames that are resampled and, when the VAD is
    fully landed, will process the resampled frames to compute the VAD
    probability.
    
    To avoid mac, win and ios trybot failures, to_be_removed.h/.cc have
    been added and will be removed as soon as the :lib target includes
    code that leads to a non-empty static lib file on those platforms.
    
    Bug: webrtc:9076
    Change-Id: I810c08acfa1adf2029e3baac2adda3045ae5214a

---
+++ modules/audio_processing/agc2/rnn_vad/to_be_removed.h
@@ -0,0 +1,24 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef MODULES_AUDIO_PROCESSING_AGC2_RNN_VAD_TO_BE_REMOVED_H_
+#define MODULES_AUDIO_PROCESSING_AGC2_RNN_VAD_TO_BE_REMOVED_H_
+
+#include "api/array_view.h"
+
+namespace webrtc {
+namespace rnn_vad {
+
+void Foo(rtc::ArrayView<int> a);
+
+}  // namespace rnn_vad
+}  // namespace webrtc
+
+#endif  // MODULES_AUDIO_PROCESSING_AGC2_RNN_VAD_TO_BE_REMOVED_H_
