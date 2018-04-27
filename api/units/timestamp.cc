541d195d4b21e946592f2cd5ec4b23daddeef5f7
Sebastian Jansson <srte@webrtc.org>
2018-04-27 09:46:55 +0200

    Moves network unit types to API.
    This prepares for being able to inject network congestion controllers.
    
    Bug: webrtc:9155
    Change-Id: Ib8f9c1c97b06d791a01c3376046933d576ae46f9

--- api/units/timestamp.cc
+++ api/units/timestamp.cc
@@ -8,19 +8,26 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#include "modules/congestion_controller/network_control/units/time_delta.h"
+#include "api/units/timestamp.h"
 
 #include "rtc_base/strings/string_builder.h"
 
 namespace webrtc {
+double Timestamp::SecondsAsDouble() const {
+  if (IsInfinite()) {
+    return std::numeric_limits<double>::infinity();
+  } else if (!IsInitialized()) {
+    return std::numeric_limits<double>::signaling_NaN();
+  } else {
+    return us() * 1e-6;
+  }
+}
 
-std::string ToString(const TimeDelta& value) {
+std::string ToString(const Timestamp& value) {
   char buf[64];
   rtc::SimpleStringBuilder sb(buf);
-  if (value.IsPlusInfinity()) {
-    sb << "+inf ms";
-  } else if (value.IsMinusInfinity()) {
-    sb << "-inf ms";
+  if (value.IsInfinite()) {
+    sb << "inf ms";
   } else if (!value.IsInitialized()) {
     sb << "? ms";
   } else {
