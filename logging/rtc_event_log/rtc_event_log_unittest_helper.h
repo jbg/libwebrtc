8338b591b379c7332c69dc995ba52c1065edb2b2
Bjorn Terelius <terelius@webrtc.org>
2018-05-02 15:38:33 +0200

    Log probing bitrate and cluster id as int32 in event log.
    Bug:webrtc:8111
    Change-Id: I0eca0b443f27ece6d2473c5287faa84978eee0dd

--- logging/rtc_event_log/rtc_event_log_unittest_helper.h
+++ logging/rtc_event_log/rtc_event_log_unittest_helper.h
@@ -70,19 +70,19 @@ class RtcEventLogTestHelper {
 
   static void VerifyBweProbeCluster(const ParsedRtcEventLog& parsed_log,
                                     size_t index,
-                                    uint32_t id,
-                                    uint32_t bitrate_bps,
+                                    int32_t id,
+                                    int32_t bitrate_bps,
                                     uint32_t min_probes,
                                     uint32_t min_bytes);
 
   static void VerifyProbeResultSuccess(const ParsedRtcEventLog& parsed_log,
                                        size_t index,
-                                       uint32_t id,
-                                       uint32_t bitrate_bps);
+                                       int32_t id,
+                                       int32_t bitrate_bps);
 
   static void VerifyProbeResultFailure(const ParsedRtcEventLog& parsed_log,
                                        size_t index,
-                                       uint32_t id,
+                                       int32_t id,
                                        ProbeFailureReason failure_reason);
 };
 
