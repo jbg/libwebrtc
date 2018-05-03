f0096afc224c55f492df7a843ba0b5c9765117b3
Alex Loiko <aleloi@webrtc.org>
2018-05-03 17:16:05 +0200

    Store RuntimeSetting in Aec Dumps.
    Also read and apply settings when parsing and replaying dumps.
    
    Bug: webrtc:9138
    Change-Id: Ia2a00537c2eb19484ff442fbffd0b95f8495516f

--- modules/audio_processing/aec_dump/aec_dump_impl.h
+++ modules/audio_processing/aec_dump/aec_dump_impl.h
@@ -67,6 +67,9 @@ class AecDumpImpl : public AecDump {
 
   void WriteConfig(const InternalAPMConfig& config) override;
 
+  void WriteRuntimeSetting(
+      const InternalAecDumpRuntimeSetting& runtime_setting) override;
+
  private:
   std::unique_ptr<WriteToFileTask> CreateWriteToFileTask();
 
