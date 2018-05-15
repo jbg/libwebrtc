07fe68aecd20fd422480dd4e5649b649115a1e50
Sergey Silkin <ssilkin@webrtc.org>
2018-05-15 12:53:35 +0200

    Replace VP9 screen sharing.
    - Remove setting of reference frames from encoder wrapper. Use fixed
    temporal prediction structures.
    - Remove flexible mode from encoder wrapper.
    - Keep frame dropping enabled at screen sharing.
    - Keep inter-layer prediction enabled for all frames at screen sharing.
    - Configure layering with GetScvConfig().
    
    Bug: webrtc:9261
    Change-Id: I355baa6aab7b98ac5028b3851d1f8ccc82a308e0

--- media/engine/webrtcvideoengine_unittest.cc
+++ media/engine/webrtcvideoengine_unittest.cc
@@ -2962,8 +2962,8 @@ TEST_F(Vp9SettingsTest, VerifyVp9SpecificSettings) {
 
   ASSERT_TRUE(stream->GetVp9Settings(&vp9_settings)) << "No VP9 config set.";
   EXPECT_FALSE(vp9_settings.denoisingOn);
-  // Frame dropping always off for screen sharing.
-  EXPECT_FALSE(vp9_settings.frameDroppingOn);
+  // Frame dropping always on for screen sharing.
+  EXPECT_TRUE(vp9_settings.frameDroppingOn);
 
   stream = SetDenoisingOption(last_ssrc_, &capturer, false);
 
