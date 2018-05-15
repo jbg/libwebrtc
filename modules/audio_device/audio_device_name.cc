095609eaf7f06ac05ee3d35b859cbd2411496be4
henrika <henrika@webrtc.org>
2018-05-15 14:54:07 +0200

    Ports CoreAudioUtil from Chrome to WebRTC.
    See https://cs.chromium.org/chromium/src/media/audio/win/core_audio_util_win.h?q=coreaudio&sq=package:chromium&g=0&l=34
    for details.
    
    
    Bug: webrtc:9265
    Change-Id: I0fd26620d94a81ccced68d81021c39723a5be2cb

---
+++ modules/audio_device/audio_device_name.cc
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "modules/audio_device/audio_device_name.h"
+
+#include <utility>
+
+namespace webrtc {
+
+const char AudioDeviceName::kDefaultDeviceId[] = "default";
+const char AudioDeviceName::kDefaultCommunicationsDeviceId[] = "communications";
+
+AudioDeviceName::AudioDeviceName(const std::string& device_name,
+                                 const std::string& unique_id)
+    : device_name(std::move(device_name)), unique_id(std::move(unique_id)) {}
+
+bool AudioDeviceName::IsValid() {
+  return !device_name.empty() && !unique_id.empty();
+}
+
+void AudioDeviceName::SetName(const std::string& name) {
+  device_name = std::move(name);
+}
+
+void AudioDeviceName::SetId(const std::string& id) {
+  unique_id = std::move(id);
+}
+
+}  // namespace webrtc
