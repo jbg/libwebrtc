503914c960a452aefcf25f700c51ab2f8bddbc62
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2018-06-14 00:15:43 +0200

    Implement H264 simulcast support and generalize SimulcastEncoderAdapter use for H264 & VP8.
    * Move SimulcastEncoderAdapter out under modules/video_coding
    * Move SimulcastRateAllocator back out to modules/video_coding/utility
    * Move TemporalLayers and ScreenshareLayers to modules/video_coding/utility
    * Move any VP8 specific code - such as temporal layer bitrate budgeting -
      under codec type dependent conditionals.
    * Plumb the simulcast index for H264 in the codec specific and RTP format data structures.
    
    Bug: webrtc:5840
    Change-Id: Ieced8a00e38f273c1a6cfd0f5431a87d07b8f44e

--- modules/video_coding/media_opt_util.cc
+++ modules/video_coding/media_opt_util.cc
@@ -18,10 +18,10 @@
 #include <limits>
 
 #include "modules/include/module_common_types.h"
-#include "modules/video_coding/codecs/vp8/include/vp8_common_types.h"
-#include "modules/video_coding/include/video_coding_defines.h"
 #include "modules/video_coding/fec_rate_table.h"
+#include "modules/video_coding/include/video_coding_defines.h"
 #include "modules/video_coding/nack_fec_tables.h"
+#include "modules/video_coding/utility/simulcast_rate_allocator.h"
 
 namespace webrtc {
 // Max value of loss rates in off-line model
@@ -285,9 +285,8 @@ bool VCMFecMethod::ProtectionFactor(const VCMProtectionParameters* parameters) {
   // Average number of packets per frame (source and fec):
   const uint8_t avgTotPackets = static_cast<uint8_t>(
       std::min(static_cast<float>(std::numeric_limits<uint8_t>::max()),
-               1.5f +
-                   static_cast<float>(bitRatePerFrame) * 1000.0f /
-                       static_cast<float>(8.0 * _maxPayloadSize)));
+               1.5f + static_cast<float>(bitRatePerFrame) * 1000.0f /
+                          static_cast<float>(8.0 * _maxPayloadSize)));
 
   // FEC rate parameters: for P and I frame
   uint8_t codeRateDelta = 0;
@@ -401,8 +400,7 @@ bool VCMFecMethod::ProtectionFactor(const VCMProtectionParameters* parameters) {
 int VCMFecMethod::BitsPerFrame(const VCMProtectionParameters* parameters) {
   // When temporal layers are available FEC will only be applied on the base
   // layer.
-  const float bitRateRatio =
-      kVp8LayerRateAlloction[parameters->numLayers - 1][0];
+  const float bitRateRatio = kLayerRateAllocation[parameters->numLayers - 1][0];
   float frameRateRatio = powf(1 / 2.0, parameters->numLayers - 1);
   float bitRate = parameters->bitRate * bitRateRatio;
   float frameRate = parameters->frameRate * frameRateRatio;
