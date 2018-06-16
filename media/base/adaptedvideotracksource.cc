963ca382fe5050bf336edb0032b494d03e334b51
Emircan Uysaler <emircan@webrtc.org>
2018-06-15 17:38:30 -0700

    Add bit depth information to PlanarYuvBuffer
    For HDR codecs, we expect to receive input that has 10-bit color depth. But
    currently, WebRTC assumes only 8-bit input and output. This CL adds k010
    format that represent this input.
    
    Bug: webrtc:9376
    Change-Id: Ie7df64b0eddb0752b493e0457a49083a1e87ba51

--- media/base/adaptedvideotracksource.cc
+++ media/base/adaptedvideotracksource.cc
@@ -47,7 +47,7 @@ void AdaptedVideoTrackSource::OnFrame(const webrtc::VideoFrame& frame) {
      synchronization for us in this case, by not passing the frame on
      to sinks which don't want it. */
   if (apply_rotation() && frame.rotation() != webrtc::kVideoRotation_0 &&
-      buffer->type() == webrtc::VideoFrameBuffer::Type::kI420) {
+      (buffer->type() == webrtc::VideoFrameBuffer::Type::kI420)) {
     /* Apply pending rotation. */
     broadcaster_.OnFrame(webrtc::VideoFrame(
         webrtc::I420Buffer::Rotate(*buffer->GetI420(), frame.rotation()),
