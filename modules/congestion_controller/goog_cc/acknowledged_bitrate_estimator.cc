fb42e857d33a5bf8cb3d32da09265d1bfd5ac759
Alex Narest <alexnarest@webrtc.org>
2018-06-25 11:28:53 +0200

    Allows audio bitrate allocation in video calls without enabling TWCC (Transport Wide Congestion Control as defined at https://tools.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01.html) for audio stream.
    This will allow experimenting with audio bitrate allocation in video calls without increasing transport overhead.
    
    Bug: webrtc:8243
    Change-Id: If961780921d53bdce95b68c26641df6875509c1f

--- modules/congestion_controller/goog_cc/acknowledged_bitrate_estimator.cc
+++ modules/congestion_controller/goog_cc/acknowledged_bitrate_estimator.cc
@@ -48,12 +48,20 @@ void AcknowledgedBitrateEstimator::IncomingPacketFeedbackVector(
 }
 
 absl::optional<uint32_t> AcknowledgedBitrateEstimator::bitrate_bps() const {
-  return bitrate_estimator_->bitrate_bps();
+  auto estimated_bitrate = bitrate_estimator_->bitrate_bps();
+  return estimated_bitrate
+             ? *estimated_bitrate +
+                   (no_feedback_bitrate_bps_ ? *no_feedback_bitrate_bps_ : 0)
+             : estimated_bitrate;
 }
 
 void AcknowledgedBitrateEstimator::SetAlrEndedTimeMs(
     int64_t alr_ended_time_ms) {
   alr_ended_time_ms_.emplace(alr_ended_time_ms);
+}
+
+void AcknowledgedBitrateEstimator::SetNoFeedbackBitrate(uint32_t bitrate_bps) {
+  no_feedback_bitrate_bps_.emplace(bitrate_bps);
 }
 
 void AcknowledgedBitrateEstimator::MaybeExpectFastRateChange(
