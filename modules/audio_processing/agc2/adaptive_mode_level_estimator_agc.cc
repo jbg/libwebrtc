e0724b85bd29a7944ed98fa0d954dc5457132c2b
Alex Loiko <aleloi@webrtc.org>
2018-07-02 16:11:46 +0200

    Allow AGC2 level estimation in AgcManagerDirect.
    This CL
    
    1. Adds a new AdaptiveModeLevelEstimatorAgc implementation of the Agc
      interface. The new implementation differs from webrtc::Agc by
       1. using the AGC2 speech level estimator in
          GetRmsErrorDb. webrtc::Agc implements its own with help of
          webrtc::LoudnessHistogram.
       2. Doesn't forget its past at every GetRmsErrorDb call.
    2. Makes AgcManagerDirect use AdaptiveModeLevelEstimatorAgc instead of
       webrtc::Agc if the use_agc2_level_estimation flag is set.
    3. Adds support for Reset calls in AGC2. The AGC will be reset during
       analog gain changes.
    
    Bug: webrtc:7494
    Change-Id: I8df3f52e322d433eb5ce5297f4236af2f1877b04

---
+++ modules/audio_processing/agc2/adaptive_mode_level_estimator_agc.cc
@@ -0,0 +1,88 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "modules/audio_processing/agc2/adaptive_mode_level_estimator_agc.h"
+
+#include "modules/audio_processing/include/audio_frame_view.h"
+
+namespace webrtc {
+
+AdaptiveModeLevelEstimatorAgc::AdaptiveModeLevelEstimatorAgc(
+    ApmDataDumper* apm_data_dumper)
+    : level_estimator_(apm_data_dumper) {}
+
+// Returns the proportion of samples in the buffer which are at full-scale
+// (and presumably clipped).
+float AdaptiveModeLevelEstimatorAgc::AnalyzePreproc(const int16_t* audio,
+                                                    size_t length) {
+  RTC_DCHECK_GT(length, 0);
+  size_t num_clipped = 0;
+  for (size_t i = 0; i < length; ++i) {
+    if (audio[i] == 32767 || audio[i] == -32768)
+      ++num_clipped;
+  }
+  return 1.0f * num_clipped / length;
+}
+
+// |audio| must be mono; in a multi-channel stream, provide the first (usually
+// left) channel.
+void AdaptiveModeLevelEstimatorAgc::Process(const int16_t* audio,
+                                            size_t length,
+                                            int sample_rate_hz) {
+  std::vector<float> float_audio_frame(length);
+  for (size_t i = 0; i < length; ++i) {
+    float_audio_frame[i] = audio[i];
+  }
+  const float* first_channel = &float_audio_frame[0];
+  AudioFrameView<const float> frame_view(&first_channel, 1, length);
+  auto vad_prob = vad_.AnalyzeFrame(frame_view);
+  latest_voice_probability_ = vad_prob.speech_probability;
+  if (latest_voice_probability_ > kVadConfidenceThreshold) {
+    time_in_ms_since_last_estimate_ += kFrameDurationMs;
+  }
+  level_estimator_.UpdateEstimation(vad_prob);
+}
+
+// Retrieves the difference between the target RMS level and the current
+// signal RMS level in dB. Returns true if an update is available and false
+// otherwise, in which case |error| should be ignored and no action taken.
+bool AdaptiveModeLevelEstimatorAgc::GetRmsErrorDb(int* error) {
+  if (time_in_ms_since_last_estimate_ <= kTimeUntilConfidentMs) {
+    return false;
+  }
+  *error = std::floor(target_level_dbfs_ -
+                      level_estimator_.LatestLevelEstimate() + 0.5);
+  time_in_ms_since_last_estimate_ = 0;
+  return true;
+}
+
+void AdaptiveModeLevelEstimatorAgc::Reset() {
+  level_estimator_.Reset();
+}
+
+int AdaptiveModeLevelEstimatorAgc::set_target_level_dbfs(int level) {
+  if (level >= 0 || level <= -100) {
+    RTC_NOTREACHED();
+    return -1;
+  }
+  target_level_dbfs_ = level;
+  return 0;
+}
+
+int AdaptiveModeLevelEstimatorAgc::target_level_dbfs() const {
+  RTC_NOTREACHED();
+  return 0;
+}
+
+float AdaptiveModeLevelEstimatorAgc::voice_probability() const {
+  return latest_voice_probability_;
+}
+
+}  // namespace webrtc
