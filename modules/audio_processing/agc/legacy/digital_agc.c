1b2409e7b467328f2be2fe65ea7434b96d273052
Sam Zackrisson <saza@webrtc.org>
2018-07-02 14:21:47 +0200

    Fix fuzzer-found overflow in AGC1
    Bug: chromium:858989
    Change-Id: Ie5aac25fd0cca4e51858cba69bde06c54a5d31bf

--- modules/audio_processing/agc/legacy/digital_agc.c
+++ modules/audio_processing/agc/legacy/digital_agc.c
@@ -504,8 +504,7 @@ int32_t WebRtcAgc_ProcessDigital(DigitalAgc* stt,
   // iterate over samples
   for (n = 0; n < L; n++) {
     for (i = 0; i < num_bands; ++i) {
-      tmp32 = out[i][n] * ((gain32 + 127) >> 7);
-      out_tmp = tmp32 >> 16;
+      out_tmp = (int64_t)out[i][n] * ((gain32 + 127) >> 7) >> 16;
       if (out_tmp > 4095) {
         out[i][n] = (int16_t)32767;
       } else if (out_tmp < -4096) {
