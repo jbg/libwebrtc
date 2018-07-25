3f366ae5cb9b27fa8d332cc51f77efd0618a7b3e
Per Åhgren <peah@webrtc.org>
2018-07-25 11:57:23 +0200

    AEC3: Corrected the filter adjustment during analog gain changes
    This CL corrects the way that the echo subtractor output is
    adjusted during the adjustment of the adaptive filter when the
    analog AGC gain changes.
    
    Bug: webrtc:9561,chromium:867373
    Change-Id: I636f936128f7d9f0d82ca4140b59f148eb35d6a4

--- modules/audio_processing/aec3/subtractor.cc
+++ modules/audio_processing/aec3/subtractor.cc
@@ -171,13 +171,15 @@ void Subtractor::Process(const RenderBuffer& render_buffer,
                   &shadow_saturation);
   fft_.ZeroPaddedFft(e_shadow, Aec3Fft::Window::kHanning, &E_shadow);
 
+  bool main_filter_adjusted = false;
   if (enable_misadjustment_estimator_) {
     filter_misadjustment_estimator_.Update(e_main, y);
     if (filter_misadjustment_estimator_.IsAdjustmentNeeded()) {
       float scale = filter_misadjustment_estimator_.GetMisadjustment();
       main_filter_.ScaleFilter(scale);
-      output->ScaleOutputMainFilter(scale);
+      output->ScaleOutputMainFilter(y, scale);
       filter_misadjustment_estimator_.Reset();
+      main_filter_adjusted = true;
     }
   }
   // Compute spectra for future use.
@@ -187,8 +189,13 @@ void Subtractor::Process(const RenderBuffer& render_buffer,
   // Update the main filter.
   std::array<float, kFftLengthBy2Plus1> X2;
   render_buffer.SpectralSum(main_filter_.SizePartitions(), &X2);
-  G_main_.Compute(X2, render_signal_analyzer, *output, main_filter_,
-                  aec_state.SaturatedCapture() || main_saturation, &G);
+  if (!main_filter_adjusted) {
+    G_main_.Compute(X2, render_signal_analyzer, *output, main_filter_,
+                    aec_state.SaturatedCapture() || main_saturation, &G);
+  } else {
+    G.re.fill(0.f);
+    G.im.fill(0.f);
+  }
   main_filter_.Adapt(render_buffer, G);
   data_dumper_->DumpRaw("aec3_subtractor_G_main", G.re);
   data_dumper_->DumpRaw("aec3_subtractor_G_main", G.im);
