9c38115889e7b93f8932642ca9f9a1311bacb6dd
Per Åhgren <peah@webrtc.org>
2018-07-25 15:15:02 +0200

    AEC3: Corrected the filter adjustment during analog gain changes
    This CL corrects the way that the echo subtractor output is
    adjusted during the adjustment of the adaptive filter when the
    analog AGC gain changes.
    
    The CL also ensures that the main adaptive filter is not updated
    when this occurs.
    
    Bug: webrtc:9561,chromium:867373
    Change-Id: I636f936128f7d9f0d82ca4140b59f148eb35d6a4

--- modules/audio_processing/aec3/subtractor_output.h
+++ modules/audio_processing/aec3/subtractor_output.h
@@ -15,6 +15,7 @@
 
 #include "modules/audio_processing/aec3/aec3_common.h"
 #include "modules/audio_processing/aec3/fft_data.h"
+#include "rtc_base/checks.h"
 
 namespace webrtc {
 
@@ -37,21 +38,13 @@ struct SubtractorOutput {
     E2_shadow.fill(0.f);
   }
 
-  void ScaleOutputMainFilter(float factor) {
+  void ScaleOutputMainFilter(rtc::ArrayView<const float> y, float factor) {
+    RTC_DCHECK_EQ(s_main.size(), y.size());
     for (auto& s : s_main) {
       s *= factor;
     }
-    for (auto& e : e_main) {
-      e *= factor;
-    }
-    for (auto& E2 : E2_main) {
-      E2 *= factor * factor;
-    }
-    for (auto& re : E_main.re) {
-      re *= factor;
-    }
-    for (auto& im : E_main.im) {
-      im *= factor;
+    for (size_t k = 0; k < y.size(); ++k) {
+      e_main[k] = y[k] - s_main[k];
     }
   }
 };
