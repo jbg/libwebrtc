c39ec8330467f7cdee94db52191370ed73c19d1b
Johnny Lee <johnny@vidyo.com>
2018-08-03 14:35:28 -0400

    Frame marking RTP header extension (PART 1: implement extension)
    Bug: webrtc:7765
    Change-Id: I23896d121afd6be4bce5ff4deaf736149efebcdb

--- modules/video_coding/frame_object.cc
+++ modules/video_coding/frame_object.cc
@@ -164,5 +164,13 @@ absl::optional<RTPVideoTypeHeader> RtpFrameObject::GetCodecHeader() const {
   return packet->video_header.video_type_header;
 }
 
+absl::optional<FrameMarking> RtpFrameObject::GetFrameMarking() const {
+  rtc::CritScope lock(&packet_buffer_->crit_);
+  VCMPacket* packet = packet_buffer_->GetPacket(first_seq_num_);
+  if (!packet)
+    return absl::nullopt;
+  return packet->video_header.frame_marking;
+}
+
 }  // namespace video_coding
 }  // namespace webrtc
