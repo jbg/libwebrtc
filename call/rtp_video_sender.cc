02dde06bb93f85314cf9036ec820451921370b63
philipel <philipel@webrtc.org>
2018-08-08 16:03:19 +0200

    Add shared frame id state to RtpVideoSender.
    When using the generic descriptor we want all simulcast streams to share one
    frame id space (so that the SFU can switch stream without having to rewrite the
    frame id). The state also needs to be restored when the RtpVideoSender is
    recreated.
    
    Note that |shared_simulcast_frame_id_| is only added, but not used in this CL.
    Actually using it will be part of the next CL.
    
    Bug:webrtc:9361
    Change-Id: I7192a06d6ae4cab118ca5996ed99a56888ad1d97

--- call/rtp_video_sender.cc
+++ call/rtp_video_sender.cc
@@ -10,6 +10,7 @@
 
 #include "call/rtp_video_sender.h"
 
+#include <algorithm>
 #include <memory>
 #include <string>
 #include <utility>
@@ -206,6 +207,8 @@ RtpVideoSender::RtpVideoSender(
     auto it = states.find(ssrc);
     if (it != states.end()) {
       state = &it->second;
+      shared_simulcast_frame_id_ =
+          std::max(shared_simulcast_frame_id_, state->last_shared_frame_id);
     }
     params_.push_back(RtpPayloadParams(ssrc, state));
   }
@@ -541,6 +544,8 @@ std::map<uint32_t, RtpPayloadState> RtpVideoSender::GetRtpPayloadStates()
   std::map<uint32_t, RtpPayloadState> payload_states;
   for (const auto& param : params_) {
     payload_states[param.ssrc()] = param.state();
+    payload_states[param.ssrc()].last_shared_frame_id =
+        shared_simulcast_frame_id_;
   }
   return payload_states;
 }
