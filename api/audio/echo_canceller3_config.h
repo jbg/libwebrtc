7b65214ff9fd135fb244d5a8f9c0207bb163a678
Jesús de Vicente Peña <devicentepena@webrtc.org>
2018-08-16 12:30:08 +0200

    AEC3: adding a config option for making the initial phase shorter
    Change-Id: If0f93aa6abcb3b8e99ca40dde86b15a4b1487883

--- api/audio/echo_canceller3_config.h
+++ api/audio/echo_canceller3_config.h
@@ -53,6 +53,7 @@ struct EchoCanceller3Config {
     ShadowConfiguration shadow_initial = {12, 0.9f, 20075344.f};
 
     size_t config_change_duration_blocks = 250;
+    bool shorter_initial_phase = true;
   } filter;
 
   struct Erle {
