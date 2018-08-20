3d9e73e2e4583f25b45d9c6d762449eeeeb5820a
Sergey Silkin <ssilkin@webrtc.org>
2018-08-20 16:28:20 +0200

    Fix Vp9 flexible mode in RTP ref frame finder.
    Bug: webrtc:9643
    Change-Id: Ie545dfb982297902f7df1da90008af04c5e67d6e

--- modules/video_coding/rtp_frame_reference_finder.cc
+++ modules/video_coding/rtp_frame_reference_finder.cc
@@ -392,8 +392,7 @@ RtpFrameReferenceFinder::FrameDecision RtpFrameReferenceFinder::ManageFrameVp9(
       absl::get<RTPVideoHeaderVP9>(*rtp_codec_header);
 
   if (codec_header.picture_id == kNoPictureId ||
-      codec_header.temporal_idx == kNoTemporalIdx ||
-      codec_header.tl0_pic_idx == kNoTl0PicIdx) {
+      codec_header.temporal_idx == kNoTemporalIdx) {
     return ManageFrameGeneric(std::move(frame), codec_header.picture_id);
   }
 
@@ -417,6 +416,8 @@ RtpFrameReferenceFinder::FrameDecision RtpFrameReferenceFinder::ManageFrameVp9(
     UnwrapPictureIds(frame);
     return kHandOff;
   }
+
+  RTC_DCHECK_NE(codec_header.tl0_pic_idx, kNoTl0PicIdx);
 
   GofInfo* info;
   int64_t unwrapped_tl0 = tl0_unwrapper_.Unwrap(codec_header.tl0_pic_idx);
