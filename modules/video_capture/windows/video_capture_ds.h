9015e7ac01dfbb42c6e31949ceff23bbb2f918c9
Mirko Bonadei <mbonadei@webrtc.org>
2018-08-29 12:11:07 +0200

    Remove clang:find_bad_constructs suppression from modules/video_capture.
    This CL removes //build/config/clang:find_bad_constructs from the
    suppressed_configs list, which means that clang:find_bad_constructs
    is now enabled on these translation units.
    
    Bug: webrtc:9251, webrtc:163
    Change-Id: I71350feb45c23d5a0e17e4828c514ce1c353f6a7

--- modules/video_capture/windows/video_capture_ds.h
+++ modules/video_capture/windows/video_capture_ds.h
@@ -33,8 +33,8 @@ class VideoCaptureDS : public VideoCaptureImpl {
    *   Start/Stop
    *
    *************************************************************************/
-  virtual int32_t StartCapture(const VideoCaptureCapability& capability);
-  virtual int32_t StopCapture();
+  int32_t StartCapture(const VideoCaptureCapability& capability) override;
+  int32_t StopCapture() override;
 
   /**************************************************************************
    *
@@ -42,8 +42,8 @@ class VideoCaptureDS : public VideoCaptureImpl {
    *
    **************************************************************************/
 
-  virtual bool CaptureStarted();
-  virtual int32_t CaptureSettings(VideoCaptureCapability& settings);
+  bool CaptureStarted() override;
+  int32_t CaptureSettings(VideoCaptureCapability& settings) override;
 
  protected:
   virtual ~VideoCaptureDS();
