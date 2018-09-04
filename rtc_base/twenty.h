aa1d4e577d1f6fabfc5d0ca9b7f5b77579295243
Mirko Bonadei <mbonadei@webrtc.org>
2018-09-04 16:47:49 +0200

    Example of RTC_EXPORT.
    ** DO NOT LAND **
    
    Bug: None
    Change-Id: I8ae881230141f3b577917bad266da9668eeb99e7

---
+++ rtc_base/twenty.h
@@ -0,0 +1,19 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef RTC_BASE_TWENTY_H_
+#define RTC_BASE_TWENTY_H_
+
+#include "rtc_base/rtc_export.h"
+
+RTC_EXPORT int Twenty();
+// int Twenty();
+
+#endif  // RTC_BASE_TWENTY_H_
