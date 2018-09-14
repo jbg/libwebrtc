bdb182abea790edd3f26feab5568d2eedf63ab13
Bjorn Terelius <terelius@webrtc.org>
2018-09-14 17:05:06 +0200

    Add helper class to process RtcEventLog events in order.
    Add helper class to process RtcEventLog events in order.
    Use helper class to migrate rtc_event_log2rtp_dump.cc
    to new parser API.
    
    Bug:webrtc:8111
    Change-Id: I7cbc220dad1f50be3a985ed44de27b38e5f20476

--- logging/rtc_event_log/rtc_event_log_parser_new.h
+++ logging/rtc_event_log/rtc_event_log_parser_new.h
@@ -48,6 +48,11 @@ namespace webrtc {
 enum class BandwidthUsage;
 struct AudioEncoderRuntimeConfig;
 
+// The different event types are deliberately POD. Analysis of large logs is
+// already resource intensive. The code simplifications that would be possible
+// possible by having a base class (containing e.g. the log time) are not
+// considered to outweigh the added memory and runtime overhead incurred by
+// adding a vptr.
 struct LoggedAlrStateEvent {
   int64_t timestamp_us;
   bool in_alr;
@@ -243,7 +248,7 @@ struct LoggedRtcpPacketTransportFeedback {
 };
 
 struct LoggedStartEvent {
-  explicit LoggedStartEvent(uint64_t timestamp_us)
+  explicit LoggedStartEvent(int64_t timestamp_us)
       : timestamp_us(timestamp_us) {}
   int64_t timestamp_us;
   int64_t log_time_us() const { return timestamp_us; }
@@ -251,8 +256,7 @@ struct LoggedStartEvent {
 };
 
 struct LoggedStopEvent {
-  explicit LoggedStopEvent(uint64_t timestamp_us)
-      : timestamp_us(timestamp_us) {}
+  explicit LoggedStopEvent(int64_t timestamp_us) : timestamp_us(timestamp_us) {}
   int64_t timestamp_us;
   int64_t log_time_us() const { return timestamp_us; }
   int64_t log_time_ms() const { return timestamp_us / 1000; }
@@ -529,6 +533,8 @@ class ParsedRtcEventLogNew {
     uint32_t ssrc;
     PacketView<const LoggedRtpPacket> packet_view;
   };
+
+  static webrtc::RtpHeaderExtensionMap GetDefaultHeaderExtensionMap();
 
   explicit ParsedRtcEventLogNew(
       UnconfiguredHeaderExtensions parse_unconfigured_header_extensions =
