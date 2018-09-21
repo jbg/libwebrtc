9c4c5a76159e7f15966387b40eb2a5108613b478
Oleh Prypin <oprypin@webrtc.org>
2018-09-21 13:54:42 +0200

    Make webrtc_perf_tests output an empty result output.json
    to satisfy a stricter check introduced in
    https://chromium.googlesource.com/chromium/tools/build/+/503174a3e1e439c6ab0b18d263980fd18a3d2855
    
    The file is supposed to contain actual gtest results, so having an
    empty one is a workaround, but this just returns things to the way
    they were.
    
    Bug: chromium:885194
    Change-Id: I79870fd7e4be484c322229e1f4079784c9a9f74b

--- test/test_main.cc
+++ test/test_main.cc
@@ -38,9 +38,10 @@ DEFINE_bool(
 
 #else
 
-DEFINE_string(isolated_script_test_output,
-              "",
-              "Intentionally ignored flag intended for Chromium.");
+DEFINE_string(
+    isolated_script_test_output,
+    "",
+    "Path to output an empty JSON file which Chromium infra requires.");
 
 DEFINE_string(
     isolated_script_test_perf_output,
@@ -110,6 +111,13 @@ int main(int argc, char* argv[]) {
   std::string chartjson_result_file = FLAG_isolated_script_test_perf_output;
   if (!chartjson_result_file.empty()) {
     webrtc::test::WritePerfResults(chartjson_result_file);
+  }
+
+  std::string result_filename = FLAG_isolated_script_test_output;
+  if (!result_filename.empty()) {
+    std::ofstream result_file(result_filename);
+    result_file << "{\"version\": 3}";
+    result_file.close();
   }
 
   return exit_code;
