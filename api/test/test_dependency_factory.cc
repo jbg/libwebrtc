150e13f3d94d2d6d9d2d9f0dc1b67fdb1edd4570
Patrik Höglund <phoglund@webrtc.org>
2018-09-24 13:19:12 +0200

    Added test dependency factory.
    Bug: b/113654555
    Change-Id: I6879d0e7dcbfbb04ad7a5179c4f4fbe8d31cf3d4

---
+++ api/test/test_dependency_factory.cc
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include <memory>
+
+#include "api/test/test_dependency_factory.h"
+
+namespace webrtc {
+
+std::unique_ptr<TestDependencyFactory> TestDependencyFactory::instance_ =
+    nullptr;
+
+const TestDependencyFactory& TestDependencyFactory::GetInstance() {
+  if (instance_ == nullptr) {
+    instance_ = absl::make_unique<TestDependencyFactory>();
+  }
+  return *instance_;
+}
+
+std::unique_ptr<VideoQualityTestFixtureInterface::InjectionComponents>
+TestDependencyFactory::CreateComponents() const {
+  return nullptr;
+}
+
+}  // namespace webrtc
