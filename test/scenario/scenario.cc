1f0b5fc73174d177171441a2f3ab28cdd54269e9
Sebastian Jansson <srte@webrtc.org>
2018-09-26 14:44:11 +0200

    Adds quick scenario client.
    Adds InstantClient, a class that allows testing congestion controllers
    using the Scenario test framework without running in real time.
    
    This allows using simplified scenario tests as unit tests, narrowing
    the gap between end to end tests and unit tests.
    
    Bug: webrtc:9510
    Change-Id: I61ab388bd610f636b926675b1f14b8d85e3c1114

--- test/scenario/scenario.cc
+++ test/scenario/scenario.cc
@@ -122,6 +122,31 @@ CallClient* Scenario::CreateClient(
   return CreateClient(name, config);
 }
 
+SimulatedTimeClient* Scenario::CreateInstantController(
+    std::string name,
+    SimulatedTimeClientConfig config,
+    std::vector<PacketStreamConfig> stream_configs,
+    std::vector<NetworkNode*> send_link,
+    std::vector<NetworkNode*> return_link) {
+  uint64_t send_id = next_receiver_id_++;
+  uint64_t return_id = next_receiver_id_++;
+  SimulatedTimeClient* client = new SimulatedTimeClient(
+      GetFullPathOrEmpty(name), config, stream_configs, send_link, return_link,
+      send_id, return_id, Now());
+  if (!base_filename_.empty() && !name.empty() &&
+      config.transport.state_log_interval.IsFinite()) {
+    Every(config.transport.state_log_interval, [this, client]() {
+      client->network_controller_factory_.LogCongestionControllerStats(Now());
+    });
+  }
+
+  Every(client->GetNetworkControllerProcessInterval(),
+        [this, client] { client->CongestionProcess(Now()); });
+  Every(TimeDelta::ms(5), [this, client] { client->PacerProcess(Now()); });
+  instant_clients_.emplace_back(client);
+  return client;
+}
+
 SimulationNode* Scenario::CreateSimulationNode(
     std::function<void(NetworkNodeConfig*)> config_modifier) {
   NetworkNodeConfig config;
