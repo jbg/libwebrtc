8c965215236c8baac867fbd2727bbfbd13cbc0b2
Danil Chapovalov <danilchap@webrtc.org>
2018-10-02 20:12:18 +0200

    Move RtpHeaderExtensionMap::GetTotalLengthInBytes into own file
    Rename to better match what it does,
    Adjust to support two-byte header extension
    
    Bug: webrtc:7990
    Change-Id: I2786d70e7cf9cd3d722f54fb1d07c9cfaafab947

---
+++ modules/rtp_rtcp/source/rtp_header_extension_size.cc
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "modules/rtp_rtcp/source/rtp_header_extension_size.h"
+
+#include "api/rtpparameters.h"
+
+namespace webrtc {
+
+size_t RtpHeaderExtensionSize(
+    rtc::ArrayView<const RtpExtensionSize> extensions,
+    const RtpHeaderExtensionMap& registered_extensions) {
+  // Header size of the extension block, see RFC3550 Section 5.3.1
+  static constexpr size_t kExtensionBlockHeaderSize = 4;
+  // Max value size of a one-byte header extension, see RFC8285 Section 4.2-4.3.
+  static constexpr size_t kOneByteHeaderExtensionMaxValueSize = 16;
+
+  size_t values_size = 0;
+  size_t num_extensions = 0;
+  size_t each_extension_header_size = 1;
+  for (const RtpExtensionSize& extension : extensions) {
+    int id = registered_extensions.GetId(extension.type);
+    if (id == RtpHeaderExtensionMap::kInvalidId)
+      continue;
+    if (id > RtpExtension::kOneByteHeaderExtensionMaxId ||
+        extension.value_size > kOneByteHeaderExtensionMaxValueSize) {
+      each_extension_header_size = 2;
+    }
+    values_size += extension.value_size;
+    num_extensions++;
+  }
+  if (values_size == 0)
+    return 0;
+  size_t size = kExtensionBlockHeaderSize +
+                each_extension_header_size * num_extensions + values_size;
+  // Extension size specified in 32bit words,
+  // so result must be mutple of 4 bytes. Round up.
+  return size + 3 - (size + 3) % 4;
+}
+
+}  // namespace webrtc
