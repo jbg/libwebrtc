bd3ca4bd5aa8aefad27af7006578dd3b34643f91
Niels Möller <nisse@webrtc.org>
2018-10-04 15:51:56 +0200

    Set ChannelReceive transport at construction time.
    Followup to cl https://webrtc-review.googlesource.com/c/src/+/103640.
    Set the rtcp_send_transport at construction time, delete
    RegisterTransport, and the proxying of transport methods.
    
    In addition, delete the unused RtcpRtpStats argument from the
    constructor.
    
    Bug: webrtc:9801
    Change-Id: I80f25bc08dc2130386053568ddce4ef91654caeb

--- audio/audio_receive_stream.cc
+++ audio/audio_receive_stream.cc
@@ -73,7 +73,7 @@ std::unique_ptr<voe::ChannelReceiveProxy> CreateChannelAndProxy(
   return absl::make_unique<voe::ChannelReceiveProxy>(
       absl::make_unique<voe::ChannelReceive>(
           module_process_thread, internal_audio_state->audio_device_module(),
-          nullptr /* RtcpRttStats */, event_log, config.rtp.remote_ssrc,
+          config.rtcp_send_transport, event_log, config.rtp.remote_ssrc,
           config.jitter_buffer_max_packets,
           config.jitter_buffer_fast_accelerate, config.decoder_factory,
           config.codec_pair_id));
@@ -114,8 +114,6 @@ AudioReceiveStream::AudioReceiveStream(
 
   module_process_thread_checker_.DetachFromThread();
 
-  channel_proxy_->RegisterTransport(config.rtcp_send_transport);
-
   // Configure bandwidth estimation.
   channel_proxy_->RegisterReceiverCongestionControlObjects(packet_router);
 
@@ -131,7 +129,6 @@ AudioReceiveStream::~AudioReceiveStream() {
   RTC_LOG(LS_INFO) << "~AudioReceiveStream: " << config_.rtp.remote_ssrc;
   Stop();
   channel_proxy_->DisassociateSendChannel();
-  channel_proxy_->RegisterTransport(nullptr);
   channel_proxy_->ResetReceiverCongestionControlObjects();
 }
 
