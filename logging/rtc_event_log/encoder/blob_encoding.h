4105c4949fa34e3e4b88d4a8d1fb40da5e7e07fb
Elad Alon <eladalon@webrtc.org>
2018-10-06 00:28:18 +0200

    Add blob-encoding support for RTC event logs
    A blob is a string of binary information, whose length may not
    necessarily be determined by looking into the string, so that
    concatenating all blobs without explicitly including their lengths
    as part of their encoding is not a viable option.
    
    Bug: webrtc:8111
    Change-Id: I89fdca660e89a6a71eff3ecb7b86416312b81f23

---
+++ logging/rtc_event_log/encoder/blob_encoding.h
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef LOGGING_RTC_EVENT_LOG_ENCODER_BLOB_ENCODING_H_
+#define LOGGING_RTC_EVENT_LOG_ENCODER_BLOB_ENCODING_H_
+
+#include <string>
+#include <vector>
+
+#include "absl/strings/string_view.h"
+
+namespace webrtc {
+
+extern const size_t kMaxVarIntLengthBytes;
+
+// Encode/decode a sequence of strings, whose length is not known to be
+// discernable from the blob itself (i.e. without being transmitted OOB),
+// in a away that would allow us to separate them again on the decoding side.
+//
+// EncodeBlobs() must be given a non-empty vector. The blobs themselves may
+// be equal to "", though.
+// EncodeBlobs() may not fail.
+// EncodeBlobs() never returns the empty string.
+//
+// Calling DecodeBlobs() on a non-empty string, or with |num_of_blobs| set to 0,
+// is an error.
+// DecodeBlobs() returns an empty vector if it fails, e.g. due to a mismatch
+// between |num_of_blobs| and |encoded_blobs|, which can happen if
+// |encoded_blobs| is corrupted.
+// When successful, DecodeBlobs() returns a vector of string_view objects,
+// which refer to the original input (|encoded_blobs|), and therefore may
+// not outlive it.
+std::string EncodeBlobs(const std::vector<std::string>& blobs);
+std::vector<absl::string_view> DecodeBlobs(absl::string_view encoded_blobs,
+                                           size_t num_of_blobs);
+
+}  // namespace webrtc
+
+#endif  // LOGGING_RTC_EVENT_LOG_ENCODER_BLOB_ENCODING_H_
