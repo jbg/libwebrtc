9f012bc77f390ec51f749d398972fc53746263ca
Johannes Kron <kron@webrtc.org>
2018-10-08 14:07:52 +0200

    Remove deprecated cricket::RtpHeaderExtension
    Part of refactor to prepare support for two-byte RTP header extensions.
    
    Bug: webrtc:7990
    Change-Id: I9f46009e052d0d5f03aa4bfa30add18638e05a36

--- media/base/fakemediaengine.h
+++ media/base/fakemediaengine.h
@@ -745,16 +745,6 @@ class FakeBaseEngine {
     capabilities_.header_extensions = extensions;
   }
 
-  void set_rtp_header_extensions(
-      const std::vector<cricket::RtpHeaderExtension>& extensions) {
-    for (const cricket::RtpHeaderExtension& ext : extensions) {
-      RtpExtension webrtc_ext;
-      webrtc_ext.uri = ext.uri;
-      webrtc_ext.id = ext.id;
-      capabilities_.header_extensions.push_back(webrtc_ext);
-    }
-  }
-
  protected:
   // Flag used by optionsmessagehandler_unittest for checking whether any
   // relevant setting has been updated.
@@ -898,15 +888,6 @@ class FakeMediaEngine
   }
   void SetVideoRtpHeaderExtensions(
       const std::vector<RtpExtension>& extensions) {
-    video().set_rtp_header_extensions(extensions);
-  }
-
-  void SetAudioRtpHeaderExtensions(
-      const std::vector<cricket::RtpHeaderExtension>& extensions) {
-    voice().set_rtp_header_extensions(extensions);
-  }
-  void SetVideoRtpHeaderExtensions(
-      const std::vector<cricket::RtpHeaderExtension>& extensions) {
     video().set_rtp_header_extensions(extensions);
   }
 
--- pc/sessiondescription.h
+++ pc/sessiondescription.h
@@ -118,13 +118,6 @@ class MediaContentDescription {
     rtp_header_extensions_.push_back(ext);
     rtp_header_extensions_set_ = true;
   }
-  void AddRtpHeaderExtension(const cricket::RtpHeaderExtension& ext) {
-    webrtc::RtpExtension webrtc_extension;
-    webrtc_extension.uri = ext.uri;
-    webrtc_extension.id = ext.id;
-    rtp_header_extensions_.push_back(webrtc_extension);
-    rtp_header_extensions_set_ = true;
-  }
   void ClearRtpHeaderExtensions() {
     rtp_header_extensions_.clear();
     rtp_header_extensions_set_ = true;
