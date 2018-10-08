af382237eadb0627a27f06e03c4a318157e58fe1
Christoffer Rodbro <crodbro@webrtc.org>
2018-10-08 12:29:32 +0200

    New method for precise packet reception time measurement.
    Bug: b/113283295
    Change-Id: I43a32122e9af992b5e0ba8b187c9ad4f22aba80d

--- call/receive_time_calculator.cc
+++ call/receive_time_calculator.cc
@@ -9,59 +9,85 @@
  */
 
 #include "call/receive_time_calculator.h"
+
+#include <algorithm>
+
 #include "absl/memory/memory.h"
 #include "rtc_base/logging.h"
+#include "rtc_base/timeutils.h"
 #include "system_wrappers/include/field_trial.h"
 
 namespace webrtc {
 namespace {
-using ::webrtc::field_trial::FindFullName;
 using ::webrtc::field_trial::IsEnabled;
 
-const char kBweReceiveTimeCorrection[] = "WebRTC-BweReceiveTimeCorrection";
+const char kBweReceiveTimeCorrection[] = "WebRTC-Bwe-ReceiveTimeFix";
 }  // namespace
 
-ReceiveTimeCalculator::ReceiveTimeCalculator(int64_t min_delta_ms,
-                                             int64_t max_delta_diff_ms)
-    : min_delta_us_(min_delta_ms * 1000),
-      max_delta_diff_us_(max_delta_diff_ms * 1000) {}
+ReceiveTimeCalculator::ReceiveTimeCalculator() {}
 
 std::unique_ptr<ReceiveTimeCalculator>
 ReceiveTimeCalculator::CreateFromFieldTrial() {
   if (!IsEnabled(kBweReceiveTimeCorrection))
     return nullptr;
-  int min, max;
-  if (sscanf(FindFullName(kBweReceiveTimeCorrection).c_str(), "Enabled,%d,%d",
-             &min, &max) != 2) {
-    RTC_LOG(LS_WARNING) << "Invalid number of parameters provided.";
-    return nullptr;
-  }
-  return absl::make_unique<ReceiveTimeCalculator>(min, max);
+  return absl::make_unique<ReceiveTimeCalculator>();
 }
 
-int64_t ReceiveTimeCalculator::ReconcileReceiveTimes(int64_t packet_time_us_,
-                                                     int64_t safe_time_us_) {
-  if (!receive_time_offset_us_) {
-    receive_time_offset_us_ = safe_time_us_ - packet_time_us_;
-  } else {
-    int64_t safe_delta_us = safe_time_us_ - last_safe_time_us_;
-    int64_t packet_delta_us_ = packet_time_us_ - last_packet_time_us_;
-    int64_t delta_diff = packet_delta_us_ - safe_delta_us;
-    // Packet time should not decrease significantly, a large decrease indicates
-    // a reset of the packet time clock and we should reset the offest
-    // parameter. The safe reference time can increase in large jumps if the
-    // thread measuring it is backgrounded for longer periods. But if the packet
-    // time increases significantly more than the safe time, it indicates a
-    // clock reset and we should reset the offset.
+int64_t ReceiveTimeCalculator::ReconcileReceiveTimes(int64_t packet_time_us,
+                                                     int64_t system_time_us,
+                                                     int64_t safe_time_us) {
+  // The delay should be positive. If not, system time was probably moved
+  // backwards between reads in socket and here.
+  int64_t time_delta_us = std::max(0l, system_time_us - packet_time_us);
+  int64_t corrected_time_us =
+      uncaught_initial_reset_us_ + safe_time_us - time_delta_us;
 
-    if (packet_delta_us_ < min_delta_us_ || delta_diff > max_delta_diff_us_) {
-      RTC_LOG(LS_WARNING) << "Received a clock jump of " << delta_diff
-                          << " resetting offset";
-      receive_time_offset_us_ = safe_time_us_ - packet_time_us_;
+  // All repairs depend on variables being intialized
+  if (last_packet_time_us_ > 0) {
+    int64_t packet_time_skip_us = packet_time_us - last_packet_time_us_;
+    int64_t system_skip_us = system_time_us - last_system_time_us_;
+
+    // Save for case where clock resets downwards during initial stall.
+    total_system_time_passed_us_ += std::max(0l, system_skip_us);
+    if (packet_time_skip_us < 0 &&
+        total_system_time_passed_us_ < rtc::kNumMicrosecsPerMillisec) {
+      uncaught_initial_reset_us_ = -packet_time_skip_us;
+    }
+
+    // Save for case where system time was moved forwards in between reads in
+    // socket and app. Advance time by (capped) packet time increase.
+    if (corrected_time_us < last_corrected_time_us_) {
+      int64_t update_us = std::max(
+          0l,
+          std::min(100 * rtc::kNumMicrosecsPerMillisec, packet_time_skip_us));
+      corrected_time_us = last_corrected_time_us_ + update_us;
+    }
+
+    // Save for the easily detectable case where system time became smaller than
+    // packet time due to reset between the two reads. Advance time by packet
+    // time increase.
+    if (packet_time_skip_us >= 0 && system_time_us < packet_time_us) {
+      corrected_time_us = last_corrected_time_us_ + packet_time_skip_us;
+    }
+
+    // Save for the harder case where system time was moved backwards in between
+    // reads in socket and app right before a stall. Advance time by packet time
+    // increase. Track case as state until stall ends.
+    if (system_skip_us > 0)
+      negative_skip_in_stall_state_ = false;
+    if (packet_time_skip_us >= 0 && system_skip_us >= 0 &&
+        (negative_skip_in_stall_state_ ||
+         packet_time_skip_us < system_skip_us) &&
+        corrected_time_us > last_corrected_time_us_ + packet_time_skip_us) {
+      corrected_time_us = last_corrected_time_us_ + packet_time_skip_us;
+      negative_skip_in_stall_state_ = true;
     }
   }
-  last_packet_time_us_ = packet_time_us_;
-  last_safe_time_us_ = safe_time_us_;
-  return packet_time_us_ + *receive_time_offset_us_;
+
+  last_corrected_time_us_ = corrected_time_us;
+  last_packet_time_us_ = packet_time_us;
+  last_system_time_us_ = system_time_us;
+  return corrected_time_us;
 }
+
 }  // namespace webrtc
