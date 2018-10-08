5820e2097d49ffecd632830ff8019b0026c2f1a7
Christoffer Rodbro <crodbro@webrtc.org>
2018-10-08 15:39:18 +0200

    New method for precise packet reception time measurement.
    Bug: b/113283295
    Change-Id: I43a32122e9af992b5e0ba8b187c9ad4f22aba80d

--- call/receive_time_calculator.cc
+++ call/receive_time_calculator.cc
@@ -9,59 +9,98 @@
  */
 
 #include "call/receive_time_calculator.h"
+
+#include <string>
+
 #include "absl/memory/memory.h"
+#include "rtc_base/experiments/field_trial_parser.h"
 #include "rtc_base/logging.h"
+#include "rtc_base/numerics/safe_minmax.h"
 #include "system_wrappers/include/field_trial.h"
 
 namespace webrtc {
 namespace {
-using ::webrtc::field_trial::FindFullName;
 using ::webrtc::field_trial::IsEnabled;
 
-const char kBweReceiveTimeCorrection[] = "WebRTC-BweReceiveTimeCorrection";
+const char kBweReceiveTimeCorrection[] = "WebRTC-Bwe-ReceiveTimeFix";
 }  // namespace
 
-ReceiveTimeCalculator::ReceiveTimeCalculator(int64_t min_delta_ms,
-                                             int64_t max_delta_diff_ms)
-    : min_delta_us_(min_delta_ms * 1000),
-      max_delta_diff_us_(max_delta_diff_ms * 1000) {}
+ReceiveTimeCalculatorConfig::ReceiveTimeCalculatorConfig()
+    : max_packet_time_repair("maxrep", TimeDelta::ms(100)),
+      initial_stall_thresh("istall", TimeDelta::ms(1)) {
+  std::string trial_string =
+      field_trial::FindFullName(kBweReceiveTimeCorrection);
+  ParseFieldTrial({&max_packet_time_repair, &initial_stall_thresh},
+                  trial_string);
+}
+ReceiveTimeCalculatorConfig::ReceiveTimeCalculatorConfig(
+    const ReceiveTimeCalculatorConfig&) = default;
+ReceiveTimeCalculatorConfig::~ReceiveTimeCalculatorConfig() = default;
+
+ReceiveTimeCalculator::ReceiveTimeCalculator()
+    : config_(ReceiveTimeCalculatorConfig()) {}
 
 std::unique_ptr<ReceiveTimeCalculator>
 ReceiveTimeCalculator::CreateFromFieldTrial() {
   if (!IsEnabled(kBweReceiveTimeCorrection))
     return nullptr;
-  int min, max;
-  if (sscanf(FindFullName(kBweReceiveTimeCorrection).c_str(), "Enabled,%d,%d",
-             &min, &max) != 2) {
-    RTC_LOG(LS_WARNING) << "Invalid number of parameters provided.";
-    return nullptr;
-  }
-  return absl::make_unique<ReceiveTimeCalculator>(min, max);
+  return absl::make_unique<ReceiveTimeCalculator>();
 }
 
-int64_t ReceiveTimeCalculator::ReconcileReceiveTimes(int64_t packet_time_us_,
-                                                     int64_t safe_time_us_) {
-  if (!receive_time_offset_us_) {
-    receive_time_offset_us_ = safe_time_us_ - packet_time_us_;
-  } else {
-    int64_t safe_delta_us = safe_time_us_ - last_safe_time_us_;
-    int64_t packet_delta_us_ = packet_time_us_ - last_packet_time_us_;
-    int64_t delta_diff = packet_delta_us_ - safe_delta_us;
-    // Packet time should not decrease significantly, a large decrease indicates
-    // a reset of the packet time clock and we should reset the offest
-    // parameter. The safe reference time can increase in large jumps if the
-    // thread measuring it is backgrounded for longer periods. But if the packet
-    // time increases significantly more than the safe time, it indicates a
-    // clock reset and we should reset the offset.
+int64_t ReceiveTimeCalculator::ReconcileReceiveTimes(int64_t packet_time_us,
+                                                     int64_t system_time_us,
+                                                     int64_t safe_time_us) {
+  // The stall should be positive. If not, system time was probably moved
+  // backwards between reads in socket and here.
+  int64_t stall_time_us = rtc::SafeMax(0, system_time_us - packet_time_us);
+  int64_t corrected_time_us =
+      uncaught_initial_reset_us_ + safe_time_us - stall_time_us;
 
-    if (packet_delta_us_ < min_delta_us_ || delta_diff > max_delta_diff_us_) {
-      RTC_LOG(LS_WARNING) << "Received a clock jump of " << delta_diff
-                          << " resetting offset";
-      receive_time_offset_us_ = safe_time_us_ - packet_time_us_;
+  // All repairs depend on variables being intialized
+  if (last_packet_time_us_ > 0) {
+    int64_t packet_time_delta_us = packet_time_us - last_packet_time_us_;
+    int64_t system_delta_us = system_time_us - last_system_time_us_;
+
+    // Save for case where clock resets downwards during initial stall.
+    total_system_time_passed_us_ += rtc::SafeMax(0, system_delta_us);
+    if (packet_time_delta_us < 0 &&
+        total_system_time_passed_us_ < config_.initial_stall_thresh->us()) {
+      uncaught_initial_reset_us_ = -packet_time_delta_us;
+    }
+
+    // Save for case where system time was moved forwards in between reads in
+    // socket and app. Advance time by (capped) packet time increase.
+    if (corrected_time_us < last_corrected_time_us_) {
+      int64_t update_us = rtc::SafeClamp(packet_time_delta_us, 0,
+                                         config_.max_packet_time_repair->us());
+      corrected_time_us = last_corrected_time_us_ + update_us;
+    }
+
+    // Save for the easily detectable case where system time became smaller than
+    // packet time due to reset between the two reads. Advance time by packet
+    // time increase.
+    if (packet_time_delta_us >= 0 && system_time_us < packet_time_us) {
+      corrected_time_us = last_corrected_time_us_ + packet_time_delta_us;
+    }
+
+    // Save for the harder case where system time was moved backwards in between
+    // reads in socket and app right before a stall. Advance time by packet time
+    // increase. Track case as state until stall ends.
+    if (system_delta_us > 0)
+      negative_delta_in_stall_state_ = false;
+    if (packet_time_delta_us >= 0 && system_delta_us >= 0 &&
+        (negative_delta_in_stall_state_ ||
+         packet_time_delta_us < system_delta_us) &&
+        corrected_time_us > last_corrected_time_us_ + packet_time_delta_us) {
+      corrected_time_us = last_corrected_time_us_ + packet_time_delta_us;
+      negative_delta_in_stall_state_ = true;
     }
   }
-  last_packet_time_us_ = packet_time_us_;
-  last_safe_time_us_ = safe_time_us_;
-  return packet_time_us_ + *receive_time_offset_us_;
+
+  last_corrected_time_us_ = corrected_time_us;
+  last_packet_time_us_ = packet_time_us;
+  last_system_time_us_ = system_time_us;
+  return corrected_time_us;
 }
+
 }  // namespace webrtc
