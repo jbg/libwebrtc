159035a802c5a2e35f1500755a8ae35b572f9731
Anton Sukhanov <sukhanov@google.com>
2018-10-08 17:15:34 -0700

    Integration of media_transport in JSepTransportController
    Basic integration of media_transport in JSepTransportController.
    
    - Creates media_transport if media transport factory provided in jsetp transport controller configuration.
    - Unittest that makes sure media_transport is created with correct caller or callee setting.
    - Added fake_media_transport, for now simple implementation which only stores caller/callee, but in the future fake media transport will be expanded to pass frames between two fake media_transports, which will enable audio / video integration tests.
    
    NEXT STEPS: Once integration of media_transport with PeerConnection (https://webrtc-review.googlesource.com/c/src/+/103860) lands, we can start passing media transport factory from peer connection to jsep transport controller.
    
    NOTE: Includes missing include change from https://webrtc-review.googlesource.com/c/src/+/103540 (otherwise this change will not compile)
    
    Bug: webrtc:9719
    Change-Id: I1e8a521beab445aa9f7ea93c8d7a537dc137d11c

--- pc/jseptransport.cc
+++ pc/jseptransport.cc
@@ -13,7 +13,6 @@
 #include <memory>
 #include <utility>  // for std::pair
 
-#include "absl/memory/memory.h"
 #include "api/candidate.h"
 #include "p2p/base/p2pconstants.h"
 #include "p2p/base/p2ptransportchannel.h"
@@ -94,11 +93,13 @@ JsepTransport::JsepTransport(
     std::unique_ptr<webrtc::SrtpTransport> sdes_transport,
     std::unique_ptr<webrtc::DtlsSrtpTransport> dtls_srtp_transport,
     std::unique_ptr<DtlsTransportInternal> rtp_dtls_transport,
-    std::unique_ptr<DtlsTransportInternal> rtcp_dtls_transport)
+    std::unique_ptr<DtlsTransportInternal> rtcp_dtls_transport,
+    std::unique_ptr<webrtc::MediaTransportInterface> media_transport)
     : mid_(mid),
       local_certificate_(local_certificate),
       rtp_dtls_transport_(std::move(rtp_dtls_transport)),
-      rtcp_dtls_transport_(std::move(rtcp_dtls_transport)) {
+      rtcp_dtls_transport_(std::move(rtcp_dtls_transport)),
+      media_transport_(std::move(media_transport)) {
   RTC_DCHECK(rtp_dtls_transport_);
   if (unencrypted_rtp_transport) {
     RTC_DCHECK(!sdes_transport);
@@ -407,6 +408,7 @@ bool JsepTransport::SetRtcpMux(bool enable,
 
   auto transport = rtp_transport();
   transport->SetRtcpMuxEnabled(rtcp_mux_negotiator_.IsActive());
+
   return ret;
 }
 
@@ -419,14 +421,16 @@ void JsepTransport::ActivateRtcpMux() {
     RTC_DCHECK(!unencrypted_rtp_transport_);
     RTC_DCHECK(!dtls_srtp_transport_);
     sdes_transport_->SetRtcpPacketTransport(nullptr);
-  } else {
+  } else if (dtls_srtp_transport_) {
     RTC_DCHECK(dtls_srtp_transport_);
     RTC_DCHECK(!unencrypted_rtp_transport_);
     RTC_DCHECK(!sdes_transport_);
     dtls_srtp_transport_->SetDtlsTransports(rtp_dtls_transport(),
                                             /*rtcp_dtls_transport=*/nullptr);
   }
+
   rtcp_dtls_transport_.reset();
+
   // Notify the JsepTransportController to update the aggregate states.
   SignalRtcpMuxActive();
 }
