2aafa01908af77cfdd3f683ddab9121ed8b7588d
Jonas Olsson <jonasolsson@webrtc.org>
2018-10-10 15:41:36 +0200

    Compute RTCConnectionState and RTCIceConnectionState.
    Compute these states in jseptransportController and store them. Eventually they should be passed on to the peer connection observer and exposed in the blink layer.
    
    Bug: webrtc:9308
    Change-Id: Ifdec39c24a607fcb8211c4acf6b9704eaff371b1

--- pc/peerconnection.h
+++ pc/peerconnection.h
@@ -146,6 +146,8 @@ class PeerConnection : public PeerConnectionInternal,
   SignalingState signaling_state() override;
 
   IceConnectionState ice_connection_state() override;
+  IceConnectionState standardized_ice_connection_state() override;
+  PeerConnectionState peer_connection_state() override;
   IceGatheringState ice_gathering_state() override;
 
   const SessionDescriptionInterface* local_description() const override;
@@ -373,6 +375,11 @@ class PeerConnection : public PeerConnectionInternal,
           receiver);
 
   void SetIceConnectionState(IceConnectionState new_state);
+  void SetStandardizedIceConnectionState(
+      PeerConnectionInterface::IceConnectionState new_state);
+  void SetCombinedConnectionState(
+      PeerConnectionInterface::PeerConnectionState new_state);
+
   // Called any time the IceGatheringState changes
   void OnIceGatheringChange(IceGatheringState new_state);
   // New ICE candidate has been gathered.
@@ -931,6 +938,11 @@ class PeerConnection : public PeerConnectionInternal,
 
   SignalingState signaling_state_ = kStable;
   IceConnectionState ice_connection_state_ = kIceConnectionNew;
+  PeerConnectionInterface::IceConnectionState
+      standardized_ice_connection_state_ = kIceConnectionNew;
+  PeerConnectionInterface::PeerConnectionState connection_state_ =
+      kPeerConnectionNew;
+
   IceGatheringState ice_gathering_state_ = kIceGatheringNew;
   PeerConnectionInterface::RTCConfiguration configuration_;
 
