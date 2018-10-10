f2d7ce08a10824f7894568b58d6d559c88ddcdec
Johannes Kron <kron@webrtc.org>
2018-10-10 15:54:59 +0200

    Respond to SDP request extmap-allow-mixed.
    The SDP attribute extmap-allow-mixed shows that the client supports
    mixing of one- and two-byte header extensions within the same stream.
    
    This is supported on the receive side since CL "Parse two-byte header
    extensions", commit 07ba2b9445525da3eabf7c188d631abf32279d98.
    
    Bug: webrtc:7990
    Change-Id: I8419da48673f513fcca21a8722614f4601a075fc

--- pc/sessiondescription.h
+++ pc/sessiondescription.h
@@ -183,6 +183,22 @@ class MediaContentDescription {
     return connection_address_;
   }
 
+  // Determines if it's allowed to mix one- and two-byte rtp header extensions
+  // within the same rtp stream.
+  enum ExtmapAllowMixed { kNo, kSession, kMedia };
+  void set_mixed_one_two_byte_header_extensions_supported(
+      ExtmapAllowMixed supported) {
+    if (supported == kMedia &&
+        mixed_one_two_byte_header_extensions_supported_ == kSession) {
+      // Do not downgrade from session level to media level.
+      return;
+    }
+    mixed_one_two_byte_header_extensions_supported_ = supported;
+  }
+  ExtmapAllowMixed mixed_one_two_byte_header_extensions_supported() const {
+    return mixed_one_two_byte_header_extensions_supported_;
+  }
+
  protected:
   bool rtcp_mux_ = false;
   bool rtcp_reduced_size_ = false;
@@ -196,6 +212,9 @@ class MediaContentDescription {
   webrtc::RtpTransceiverDirection direction_ =
       webrtc::RtpTransceiverDirection::kSendRecv;
   rtc::SocketAddress connection_address_;
+  // Mixed one- and two-byte header not included in offer on media level, but we
+  // will respond that we support it. We use flag on session level in our offer.
+  ExtmapAllowMixed mixed_one_two_byte_header_extensions_supported_ = kNo;
 };
 
 // TODO(bugs.webrtc.org/8620): Remove this alias once downstream projects have
@@ -455,6 +474,23 @@ class SessionDescription {
   }
   int msid_signaling() const { return msid_signaling_; }
 
+  // Determines if it's allowed to mix one- and two-byte rtp header extensions
+  // within the same rtp stream.
+  void set_mixed_one_two_byte_header_extensions_supported(bool supported) {
+    mixed_one_two_byte_header_extensions_supported_ = supported;
+    MediaContentDescription::ExtmapAllowMixed extmap_allow_mixed =
+        supported ? MediaContentDescription::kSession
+                  : MediaContentDescription::kNo;
+    for (auto& content : contents_) {
+      content.media_description()
+          ->set_mixed_one_two_byte_header_extensions_supported(
+              extmap_allow_mixed);
+    }
+  }
+  bool mixed_one_two_byte_header_extensions_supported() const {
+    return mixed_one_two_byte_header_extensions_supported_;
+  }
+
  private:
   SessionDescription(const SessionDescription&);
 
@@ -465,6 +501,9 @@ class SessionDescription {
   // Default to what Plan B would do.
   // TODO(bugs.webrtc.org/8530): Change default to kMsidSignalingMediaSection.
   int msid_signaling_ = kMsidSignalingSsrcAttribute;
+  // Mixed one- and two-byte header not included in offer by default because
+  // clients prior to https://bugs.webrtc.org/9712 cannot parse this correctly.
+  bool mixed_one_two_byte_header_extensions_supported_ = false;
 };
 
 // Indicates whether a session description was sent by the local client or
