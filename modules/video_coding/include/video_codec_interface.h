a20b0dbd230f6cd87bcea05e3dc8d74aeea0bea5
Erik Språng <sprang@webrtc.org>
2018-10-15 10:36:46 +0200

    Move MockVideoDecoder to api/test.
    Move MockVideoDecoder from
    modules/video_coding/include/mock/mock_video_codec_interface.h
    to
    api/test/mock_video_decoder.h
    
    The mock encoder has already moved:
    https://webrtc-review.googlesource.com/c/src/+/105620
    
    Keeping the old header until downstream projects have been updated.
    
    Bug: webrtc:9722
    Change-Id: I4bc849173a04813064212f17761876695ca3fed4

--- modules/video_coding/include/video_codec_interface.h
+++ modules/video_coding/include/video_codec_interface.h
@@ -18,6 +18,7 @@
 #include "api/video_codecs/video_encoder.h"
 #include "common_types.h"  // NOLINT(build/include)
 #include "modules/include/module_common_types.h"
+#include "modules/video_coding/include/video_codec_interface.h"
 #include "modules/video_coding/include/video_error_codes.h"
 
 namespace webrtc {
