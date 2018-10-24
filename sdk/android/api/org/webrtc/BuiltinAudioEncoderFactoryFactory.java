39d346bf7a9f2f9ab39682517d0e5bb36d9a83da
Lennart Kolmodin <kolmodin@webrtc.org>
2018-10-24 19:54:56 +0200

    Implement Injectable Audio Codecs for the Java SDK.
    Support Injectable Audio Codecs from the Java SDK.
    The PeerConnectionFactory.Builder defaults to
    BuiltinAudio(Encoder|Decoder)Factory, but other implementations are
    permitted via the Audio(Encoder|Decoder)FactoryFactory interface.
    
    Bug: webrtc:9916
    Change-Id: I61ad4a6e57666bc1be79daf5f40b129e0eacad84

---
+++ sdk/android/api/org/webrtc/BuiltinAudioEncoderFactoryFactory.java
@@ -0,0 +1,25 @@
+/*
+ *  Copyright 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+package org.webrtc;
+
+/**
+ * This class creates a native {@code webrtc::AudioEncoderFactory} with the builtin audio encoders.
+ */
+public class BuiltinAudioEncoderFactoryFactory implements AudioEncoderFactoryFactory {
+  public BuiltinAudioEncoderFactoryFactory() {}
+
+  @Override
+  public long createNative() {
+    return nativeCreateBuiltinAudioEncoderFactoryFactory();
+  }
+
+  private static native long nativeCreateBuiltinAudioEncoderFactoryFactory();
+}
