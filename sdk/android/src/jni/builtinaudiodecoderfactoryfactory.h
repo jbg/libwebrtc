fa11c53923f5611d7571cfb84da995caa1508f71
Lennart Kolmodin <kolmodin@webrtc.org>
2018-10-25 17:44:33 +0200

    Implement Injectable Audio Codecs for the Java SDK.
    Support Injectable Audio Codecs from the Java SDK.
    The PeerConnectionFactory.Builder defaults to
    BuiltinAudio(Encoder|Decoder)Factory, but other implementations are
    permitted via the Audio(Encoder|Decoder)FactoryFactory interface.
    
    Bug: webrtc:9916
    Change-Id: I61ad4a6e57666bc1be79daf5f40b129e0eacad84

---
+++ sdk/android/src/jni/builtinaudiodecoderfactoryfactory.h
@@ -0,0 +1,29 @@
+/*
+ *  Copyright 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef SDK_ANDROID_SRC_JNI_BUILTINAUDIODECODERFACTORYFACTORY_H_
+#define SDK_ANDROID_SRC_JNI_BUILTINAUDIODECODERFACTORYFACTORY_H_
+
+#include "sdk/android/generated_audio_codecs_jni/jni/BuiltinAudioDecoderFactoryFactory_jni.h"
+#include "sdk/android/native_api/jni/java_types.h"
+#include "sdk/android/src/jni/jni_helpers.h"
+
+namespace webrtc {
+namespace jni {
+
+static jlong
+JNI_BuiltinAudioDecoderFactoryFactory_CreateBuiltinAudioDecoderFactory(
+    JNIEnv* env,
+    const JavaParamRef<jclass>& jcaller);
+
+}  // namespace jni
+}  // namespace webrtc
+
+#endif  // SDK_ANDROID_SRC_JNI_BUILTINAUDIODECODERFACTORYFACTORY_H_
