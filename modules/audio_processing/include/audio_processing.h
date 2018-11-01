e139c3f84e281d3000d68bdba19dc5795dc6e32b
Alex Loiko <aleloi@webrtc.org>
2018-11-01 14:29:01 +0100

    Make the extra seturation margin configurable.
    The extra saturation margin is a setting for the SaturationProtector
    in GainController2. The higher it is, the less gain GC2 will apply. In
    this CL we pipe the setting up to audio_processing.h. Now the setting
    can be set at a high level.
    
    Also in this CL add a few (missing, they should have been there
    already) tests for the GC2 and GC2 with saturation margin.
    
    Bug: webrtc:7494
    Change-Id: I1b61f1662e6c6a8817fd5b0e845339694bf8d50d

--- modules/audio_processing/include/audio_processing.h
+++ modules/audio_processing/include/audio_processing.h
@@ -270,9 +270,11 @@ class AudioProcessing : public rtc::RefCountInterface {
     // setting |fixed_gain_db|, the limiter can be turned into a compressor that
     // first applies a fixed gain. The adaptive digital AGC can be turned off by
     // setting |adaptive_digital_mode=false|.
+    static constexpr float kDefaultExtraSaturationMarginDb = 2.f;
     struct GainController2 {
       bool enabled = false;
       bool adaptive_digital_mode = true;
+      float extra_saturation_margin_db = kDefaultExtraSaturationMarginDb;
       float fixed_gain_db = 0.f;
     } gain_controller2;
 
