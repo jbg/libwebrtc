342fbef00db17673fb1a3f2e959fcf08afbe8ebe
Jesús de Vicente Peña <devicentepena@webrtc.org>
2018-11-05 12:33:45 +0100

    AEC3: Adding a correction factor for the Erle estimation that depends on the portion of the filter that is currently in use.
    In this CL a more precise estimation of the Erle is introduced. This is done by creating different estimators that are specialized in different regions of the linear filter. An estimation of which regions were used for generating the current echo estimate is performed and used for selecting the right Erle estimate.
    
    Bug: webrtc:9961
    Change-Id: Iba6eb24596c067c3c66d40df590be379d3e1bb7b

--- modules/audio_processing/aec3/subband_erle_estimator.h
+++ modules/audio_processing/aec3/subband_erle_estimator.h
@@ -14,10 +14,12 @@
 #include <stddef.h>
 #include <array>
 #include <memory>
+#include <vector>
 
-#include "absl/types/optional.h"
 #include "api/array_view.h"
 #include "modules/audio_processing/aec3/aec3_common.h"
+#include "modules/audio_processing/aec3/erle_correction_factor.h"
+#include "modules/audio_processing/aec3/render_buffer.h"
 #include "modules/audio_processing/logging/apm_data_dumper.h"
 
 namespace webrtc {
@@ -25,21 +27,31 @@ namespace webrtc {
 // Estimates the echo return loss enhancement for each frequency subband.
 class SubbandErleEstimator {
  public:
-  SubbandErleEstimator(float min_erle, float max_erle_lf, float max_erle_hf);
+  SubbandErleEstimator(float min_erle,
+                       float max_erle_lf,
+                       float max_erle_hf,
+                       size_t num_estimators,
+                       size_t main_filter_length_blocks,
+                       size_t delay_headroom_blocks);
   ~SubbandErleEstimator();
 
   // Resets the ERLE estimator.
   void Reset();
 
   // Updates the ERLE estimate.
-  void Update(rtc::ArrayView<const float> X2,
+  void Update(const webrtc::RenderBuffer& render_buffer,
+              const std::vector<std::array<float, kFftLengthBy2Plus1>>&
+                  filter_frequency_response,
+              rtc::ArrayView<const float> X2,
               rtc::ArrayView<const float> Y2,
               rtc::ArrayView<const float> E2,
               bool converged_filter,
               bool onset_detection);
 
   // Returns the ERLE estimate.
-  const std::array<float, kFftLengthBy2Plus1>& Erle() const { return erle_; }
+  const std::array<float, kFftLengthBy2Plus1>& Erle() const {
+    return erle_for_echo_estimate_;
+  }
 
   // Returns the ERLE estimate at onsets.
   const std::array<float, kFftLengthBy2Plus1>& ErleOnsets() const {
@@ -49,47 +61,49 @@ class SubbandErleEstimator {
   void Dump(const std::unique_ptr<ApmDataDumper>& data_dumper) const;
 
  private:
-  void UpdateBands(rtc::ArrayView<const float> X2,
-                   rtc::ArrayView<const float> Y2,
-                   rtc::ArrayView<const float> E2,
+  class AccumulativeSpectra {
+   public:
+    static constexpr int kPointsToAccumulate = 6;
+    AccumulativeSpectra();
+    void Reset();
+    void Update(rtc::ArrayView<const float> X2,
+                rtc::ArrayView<const float> Y2,
+                rtc::ArrayView<const float> E2,
+                bool update_on_low_render);
+    bool EnoughPoints(size_t band) const {
+      return num_points_[band] == kPointsToAccumulate;
+    }
+    void Dump(const std::unique_ptr<ApmDataDumper>& data_dumper) const;
+    std::array<float, kFftLengthBy2Plus1> Y2_;
+    std::array<float, kFftLengthBy2Plus1> E2_;
+    std::array<bool, kFftLengthBy2Plus1> low_render_energy_;
+    std::array<int, kFftLengthBy2Plus1> num_points_;
+  };
+
+  void UpdateBands(const AccumulativeSpectra& accum_spectra,
                    size_t start,
                    size_t stop,
                    float max_erle,
                    bool onset_detection);
   void DecreaseErlePerBandForLowRenderSignals();
 
-  class ErleInstantaneous {
-   public:
-    ErleInstantaneous();
-    ~ErleInstantaneous();
-    // Updates the ERLE for a band with a new block. Returns absl::nullopt
-    // if not enough points were accumulated for doing the estimation,
-    // otherwise, it returns the ERLE. When the ERLE is returned, the
-    // low_render_energy flag contains information on whether the estimation was
-    // done using low level render signals.
-    absl::optional<float> Update(float X2,
-                                 float Y2,
-                                 float E2,
-                                 size_t band,
-                                 bool* low_render_energy);
-    // Resets the ERLE estimator to its initial state.
-    void Reset();
+  void AdjustErlePerNumberActiveFilterGroups(
+      size_t start,
+      size_t stop,
+      float max_erle,
+      rtc::ArrayView<const size_t> n_active_groups);
 
-   private:
-    std::array<float, kFftLengthBy2Plus1> Y2_acum_;
-    std::array<float, kFftLengthBy2Plus1> E2_acum_;
-    std::array<bool, kFftLengthBy2Plus1> low_render_energy_;
-    std::array<int, kFftLengthBy2Plus1> num_points_;
-  };
-
-  ErleInstantaneous instantaneous_erle_;
-  std::array<float, kFftLengthBy2Plus1> erle_;
-  std::array<float, kFftLengthBy2Plus1> erle_onsets_;
-  std::array<bool, kFftLengthBy2Plus1> coming_onset_;
-  std::array<int, kFftLengthBy2Plus1> hold_counters_;
   const float min_erle_;
   const float max_erle_lf_;
   const float max_erle_hf_;
+
+  AccumulativeSpectra accum_spectra_;
+  std::array<float, kFftLengthBy2Plus1> erle_;
+  std::array<float, kFftLengthBy2Plus1> erle_for_echo_estimate_;
+  ErleCorrectionFactor correction_factor_estimator_;
+  std::array<float, kFftLengthBy2Plus1> erle_onsets_;
+  std::array<bool, kFftLengthBy2Plus1> coming_onset_;
+  std::array<int, kFftLengthBy2Plus1> hold_counters_;
   const bool adapt_on_low_render_;
 };
 
