94c2834ceb1fcb2b628377d81abe4a75e80f2ba9
Alessio Bazzica <alessiob@webrtc.org>
2018-11-14 15:42:54 +0100

    Add output directory option for audioproc_f data dump files.
    Bug: webrtc:10000
    Change-Id: Iac21f826e78d6cb339c68fdeeedf9fe39920ac31

--- modules/audio_processing/logging/apm_data_dumper.cc
+++ modules/audio_processing/logging/apm_data_dumper.cc
@@ -10,6 +10,7 @@
 
 #include "modules/audio_processing/logging/apm_data_dumper.h"
 
+#include "rtc_base/logging.h"
 #include "rtc_base/strings/string_builder.h"
 
 // Check to verify that the define is properly set.
@@ -23,12 +24,22 @@ namespace webrtc {
 namespace {
 
 #if WEBRTC_APM_DEBUG_DUMP == 1
-std::string FormFileName(const char* name,
+std::string FormFileName(const char* output_dir,
+                         size_t output_dir_size,
+                         const char* name,
                          int instance_index,
                          int reinit_index,
                          const std::string& suffix) {
   char buf[1024];
   rtc::SimpleStringBuilder ss(buf);
+  // Add output directory if specified and trailing slash if needed.
+  if (output_dir && output_dir_size) {
+    RTC_CHECK_EQ(strlen(output_dir), output_dir_size);
+    ss << output_dir;
+    if (output_dir[strlen(output_dir) - 1] != '/') {
+      ss << "/";
+    }
+  }
   ss << name << "_" << instance_index << "-" << reinit_index << suffix;
   return ss.str();
 }
@@ -40,20 +51,24 @@ std::string FormFileName(const char* name,
 ApmDataDumper::ApmDataDumper(int instance_index)
     : instance_index_(instance_index) {}
 #else
-ApmDataDumper::ApmDataDumper(int instance_index) {}
+ApmDataDumper::ApmDataDumper(int instance_index){};
 #endif
 
-ApmDataDumper::~ApmDataDumper() {}
+ApmDataDumper::~ApmDataDumper() = default;
 
 #if WEBRTC_APM_DEBUG_DUMP == 1
 bool ApmDataDumper::recording_activated_ = false;
-;
+const char* ApmDataDumper::output_dir_ = nullptr;
+size_t ApmDataDumper::output_dir_size_ = 0;
+
 FILE* ApmDataDumper::GetRawFile(const char* name) {
   std::string filename =
-      FormFileName(name, instance_index_, recording_set_index_, ".dat");
+      FormFileName(output_dir_, output_dir_size_, name, instance_index_,
+                   recording_set_index_, ".dat");
   auto& f = raw_files_[filename];
   if (!f) {
     f.reset(fopen(filename.c_str(), "wb"));
+    RTC_CHECK(f.get()) << "Cannot write to " << filename << ".";
   }
   return f.get();
 }
@@ -62,14 +77,14 @@ WavWriter* ApmDataDumper::GetWavFile(const char* name,
                                      int sample_rate_hz,
                                      int num_channels) {
   std::string filename =
-      FormFileName(name, instance_index_, recording_set_index_, ".wav");
+      FormFileName(output_dir_, output_dir_size_, name, instance_index_,
+                   recording_set_index_, ".wav");
   auto& f = wav_files_[filename];
   if (!f) {
     f.reset(new WavWriter(filename.c_str(), sample_rate_hz, num_channels));
   }
   return f.get();
 }
-
 #endif
 
 }  // namespace webrtc
