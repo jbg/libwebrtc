a6004993c6b6faa31cc7c8dd25dfe5c395d37f0d
Ruslan Burakov <kuddai@google.com>
2018-11-20 17:53:55 +0100

    expose jitter buffer flushes metric in stats api
    Bug: chromium:907113
    Change-Id: I1d005291f9b47665f70c26148dbdcbb55564bef8

--- call/audio_receive_stream.h
+++ call/audio_receive_stream.h
@@ -72,6 +72,7 @@ class AudioReceiveStream {
     int32_t decoding_plc_cng = 0;
     int32_t decoding_muted_output = 0;
     int64_t capture_start_ntp_time_ms = 0;
+    int64_t jitter_buffer_flushes = 0;
   };
 
   struct Config {
