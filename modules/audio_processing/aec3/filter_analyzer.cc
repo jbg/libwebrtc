12aac5488c8912d75c8f9d97324d55999de0d8e4
Jesús de Vicente Peña <devicentepena@webrtc.org>
2018-11-21 13:00:53 +0100

    AEC3: Optimizing the Update method of the FilterAnalyzer class by just analyzing regions of the filter at each call and not the whole filter.
    Bug: webrtc-10032
    Change-Id: Ic58be34ba18485311c63e0fed9b6e892f9cb864c

--- modules/audio_processing/aec3/filter_analyzer.cc
+++ modules/audio_processing/aec3/filter_analyzer.cc
@@ -25,10 +25,16 @@
 namespace webrtc {
 namespace {
 
-size_t FindPeakIndex(rtc::ArrayView<const float> filter_time_domain) {
-  size_t peak_index = 0;
-  float max_h2 = filter_time_domain[0] * filter_time_domain[0];
-  for (size_t k = 1; k < filter_time_domain.size(); ++k) {
+constexpr size_t kNumberBlocksToUpdate = 1;
+
+size_t FindPeakIndex(rtc::ArrayView<const float> filter_time_domain,
+                     size_t peak_index_in,
+                     size_t start_sample,
+                     size_t end_sample) {
+  size_t peak_index = std::min(peak_index_in, filter_time_domain.size() - 1);
+  float max_h2 =
+      filter_time_domain[peak_index] * filter_time_domain[peak_index];
+  for (size_t k = start_sample; k < end_sample; ++k) {
     float tmp = filter_time_domain[k] * filter_time_domain[k];
     if (tmp > max_h2) {
       peak_index = k;
@@ -44,6 +50,11 @@ bool EnableFilterPreprocessing() {
       "WebRTC-Aec3FilterAnalyzerPreprocessorKillSwitch");
 }
 
+bool EnableIncrementalAnalysis() {
+  return !field_trial::IsEnabled(
+      "WebRTC-Aec3FilterAnalyzerIncrementalAnalysisKillSwitch");
+}
+
 }  // namespace
 
 int FilterAnalyzer::instance_count_ = 0;
@@ -57,6 +68,7 @@ FilterAnalyzer::FilterAnalyzer(const EchoCanceller3Config& config)
       active_render_threshold_(config.render_levels.active_render_limit *
                                config.render_levels.active_render_limit *
                                kFftLengthBy2),
+      use_incremental_analysis_(EnableIncrementalAnalysis()),
       h_highpass_(GetTimeDomainLength(config.filter.main.length_blocks), 0.f),
       filter_length_blocks_(config.filter.main_initial.length_blocks) {
   Reset();
@@ -69,8 +81,10 @@ void FilterAnalyzer::PreProcessFilter(
   // Minimum phase high-pass filter with cutoff frequency at about 600 Hz.
   constexpr std::array<float, 3> h = {{0.7929742f, -0.36072128f, -0.47047766f}};
 
-  std::fill(h_highpass_.begin(), h_highpass_.end(), 0.f);
-  for (size_t k = h.size() - 1; k < filter_time_domain.size(); ++k) {
+  std::fill(h_highpass_.begin() + region_.start_sample_,
+            h_highpass_.begin() + region_.end_sample_, 0.f);
+  for (size_t k = std::max(h.size() - 1, region_.start_sample_);
+       k < region_.end_sample_; ++k) {
     for (size_t j = 0; j < h.size(); ++j) {
       h_highpass_[k] += filter_time_domain[k - j] * h[j];
     }
@@ -87,12 +101,17 @@ void FilterAnalyzer::Reset() {
   consistent_estimate_counter_ = 0;
   consistent_delay_reference_ = -10;
   gain_ = default_gain_;
+  ResetRegion();
 }
 
-void FilterAnalyzer::Update(
+void FilterAnalyzer::Update(rtc::ArrayView<const float> filter_time_domain,
+                            const RenderBuffer& render_buffer) {
+  SetRegionToAnalyze(filter_time_domain);
+  AnalyzeRegion(filter_time_domain, render_buffer);
+}
+
+void FilterAnalyzer::AnalyzeRegion(
     rtc::ArrayView<const float> filter_time_domain,
-    const std::vector<std::array<float, kFftLengthBy2Plus1>>&
-        filter_freq_response,
     const RenderBuffer& render_buffer) {
   // Preprocess the filter to avoid issues with low-frequency components in the
   // filter.
@@ -103,51 +122,58 @@ void FilterAnalyzer::Update(
       use_preprocessed_filter_ ? h_highpass_ : filter_time_domain;
   RTC_DCHECK_EQ(filter_to_analyze.size(), filter_time_domain.size());
 
-  size_t peak_index = FindPeakIndex(filter_to_analyze);
-  delay_blocks_ = peak_index >> kBlockSizeLog2;
-  UpdateFilterGain(filter_to_analyze, peak_index);
+  region_.peak_index_ =
+      FindPeakIndex(filter_to_analyze, region_.peak_index_,
+                    region_.start_sample_, region_.end_sample_);
 
-  float filter_floor = 0;
-  float filter_secondary_peak = 0;
-  size_t limit1 = peak_index < 64 ? 0 : peak_index - 64;
-  size_t limit2 =
-      peak_index > filter_to_analyze.size() - 129 ? 0 : peak_index + 128;
-
-  for (size_t k = 0; k < limit1; ++k) {
-    float abs_h = fabsf(filter_to_analyze[k]);
-    filter_floor += abs_h;
-    filter_secondary_peak = std::max(filter_secondary_peak, abs_h);
-  }
-  for (size_t k = limit2; k < filter_to_analyze.size(); ++k) {
-    float abs_h = fabsf(filter_to_analyze[k]);
-    filter_floor += abs_h;
-    filter_secondary_peak = std::max(filter_secondary_peak, abs_h);
-  }
-
-  filter_floor /= (limit1 + filter_to_analyze.size() - limit2);
-
-  float abs_peak = fabsf(filter_to_analyze[peak_index]);
-  bool significant_peak_index =
-      abs_peak > 10.f * filter_floor && abs_peak > 2.f * filter_secondary_peak;
-
-  if (consistent_delay_reference_ != delay_blocks_ || !significant_peak_index) {
-    consistent_estimate_counter_ = 0;
-    consistent_delay_reference_ = delay_blocks_;
-  } else {
-    const auto& x = render_buffer.Block(-delay_blocks_)[0];
-    const float x_energy =
-        std::inner_product(x.begin(), x.end(), x.begin(), 0.f);
-    const bool active_render_block = x_energy > active_render_threshold_;
-
-    if (active_render_block) {
-      ++consistent_estimate_counter_;
-    }
-  }
-
-  consistent_estimate_ =
-      consistent_estimate_counter_ > 1.5f * kNumBlocksPerSecond;
-
+  delay_blocks_ = region_.peak_index_ >> kBlockSizeLog2;
+  UpdateFilterGain(filter_to_analyze, region_.peak_index_);
   filter_length_blocks_ = filter_time_domain.size() * (1.f / kBlockSize);
+
+  const auto& x = render_buffer.Block(-delay_blocks_)[0];
+  const float x_energy = std::inner_product(x.begin(), x.end(), x.begin(), 0.f);
+  const bool active_render_block = x_energy > active_render_threshold_;
+
+  if (active_render_block) {
+    ++consistent_estimate_counter_;
+  }
+
+  if (region_.filter_end_reached_) {
+    float filter_floor = 0;
+    float filter_secondary_peak = 0;
+    size_t limit1 = region_.peak_index_ < 64 ? 0 : region_.peak_index_ - 64;
+    size_t limit2 = region_.peak_index_ > filter_to_analyze.size() - 129
+                        ? 0
+                        : region_.peak_index_ + 128;
+
+    for (size_t k = 0; k < limit1; ++k) {
+      float abs_h = fabsf(filter_to_analyze[k]);
+      filter_floor += abs_h;
+      filter_secondary_peak = std::max(filter_secondary_peak, abs_h);
+    }
+
+    // TODO(devicentepena): limit the number of samples that are analyzed in the
+    // next loop.
+    for (size_t k = limit2; k < filter_to_analyze.size(); ++k) {
+      float abs_h = fabsf(filter_to_analyze[k]);
+      filter_floor += abs_h;
+      filter_secondary_peak = std::max(filter_secondary_peak, abs_h);
+    }
+
+    filter_floor /= (limit1 + filter_to_analyze.size() - limit2);
+
+    float abs_peak = fabsf(filter_to_analyze[region_.peak_index_]);
+    bool significant_peak_index = abs_peak > 10.f * filter_floor &&
+                                  abs_peak > 2.f * filter_secondary_peak;
+
+    if (consistent_delay_reference_ != delay_blocks_ ||
+        !significant_peak_index) {
+      consistent_estimate_counter_ = 0;
+      consistent_delay_reference_ = delay_blocks_;
+    }
+    consistent_estimate_ =
+        consistent_estimate_counter_ > 1.5f * kNumBlocksPerSecond;
+  }
 }
 
 void FilterAnalyzer::UpdateFilterGain(
@@ -166,6 +192,30 @@ void FilterAnalyzer::UpdateFilterGain(
 
   if (bounded_erl_ && gain_) {
     gain_ = std::max(gain_, 0.01f);
+  }
+}
+
+void FilterAnalyzer::ResetRegion() {
+  region_.peak_index_ = 0;
+  region_.start_sample_ = 0;
+  region_.end_sample_ = 0;
+  region_.filter_end_reached_ = false;
+}
+
+void FilterAnalyzer::SetRegionToAnalyze(
+    rtc::ArrayView<const float> filter_time_domain) {
+  auto& r = region_;
+  if (use_incremental_analysis_) {
+    r.start_sample_ = r.filter_end_reached_ ? 0 : r.end_sample_;
+    r.end_sample_ =
+        std::min(r.start_sample_ + kNumberBlocksToUpdate * kBlockSize,
+                 filter_time_domain.size());
+    r.filter_end_reached_ = r.end_sample_ == filter_time_domain.size();
+
+  } else {
+    r.start_sample_ = 0;
+    r.end_sample_ = filter_time_domain.size();
+    r.filter_end_reached_ = true;
   }
 }
 
