b2a23e1b47c38944bccda7b20e6d35ce9e946aec
Harald Alvestrand <hta@webrtc.org>
2018-11-25 14:17:50 +0100

    Add API for returning a webrtc::DtlsTransport for a MID on a PC
    This includes a refactoring of jseptransport to store a refcounted
    object instead of a std::unique_ptr to the cricket::DtlsTransport.
    
    Bug: chromium:907849
    Change-Id: Ib557ce72c2e6ce8af297c2b8deb7ec3a103d6d31

---
+++ api/dtlstransportinterface.h
@@ -0,0 +1,29 @@
+/*
+ *  Copyright 2012 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef API_DTLSTRANSPORTINTERFACE_H_
+#define API_DTLSTRANSPORTINTERFACE_H_
+
+#include "rtc_base/refcount.h"
+
+namespace webrtc {
+
+// A DTLS transport, as represented to the outside world.
+// Its role is to report state changes and errors, and make sure information
+// about remote certificates is available.
+class DtlsTransportInterface : public rtc::RefCountInterface {
+  // TODO(hta): Need a notifier interface to transmit state changes and
+  // error events. The generic NotifierInterface of mediasteraminterface.h
+  // may be suitable, or may be copyable.
+};
+
+}  // namespace webrtc
+
+#endif  // API_DTLSTRANSPORTINTERFACE_H_
---
+++ pc/dtlstransport.cc
@@ -0,0 +1,21 @@
+/*
+ *  Copyright 2012 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "pc/dtlstransport.h"
+
+#include <utility>
+
+namespace webrtc {
+
+DtlsTransport::DtlsTransport(
+    std::unique_ptr<cricket::DtlsTransportInternal> internal)
+    : internal_dtls_transport_(std::move(internal)) {}
+
+}  // namespace webrtc
