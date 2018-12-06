568953da33f5c5568a400b221ed7342f28cd37c0
Jeroen de Borst <jeroendb@webrtc.org>
2018-12-06 14:47:56 -0800

    Adding metrics for hostname candidate use.
    These metrics by themselves won't be as useful, unless they can be correlated to the use of the
    feature 'WebRtcHideLocalIpsWithMdns'. This can be done by running a finch experiment where we turn
    the feature on for a % of users, we can then compare these metrics for users with and without
    the feature turned on.
    
    A complementary change is required in Chrome:
    tools/metrics/histograms/enums.xml
    
    Bug: 10091
    Change-Id: Ibc6d16dec95a8e3943ce40063c02903769fe1cb4

--- pc/peerconnection.cc
+++ pc/peerconnection.cc
@@ -277,16 +277,36 @@ IceCandidatePairType GetIceCandidatePairCounter(
   const auto& relay = RELAY_PORT_TYPE;
   const auto& prflx = PRFLX_PORT_TYPE;
   if (l == host && r == host) {
+    bool local_hostname =
+        !local.address().hostname().empty() && local.address().IsUnresolvedIP();
+    bool remote_hostname = !remote.address().hostname().empty() &&
+                           remote.address().IsUnresolvedIP();
     bool local_private = IPIsPrivate(local.address().ipaddr());
     bool remote_private = IPIsPrivate(remote.address().ipaddr());
-    if (local_private) {
-      if (remote_private) {
+    RTC_LOG(LS_ERROR) << local.ToString() << " -- " << remote.ToString();
+    RTC_LOG(LS_ERROR) << local.address().hostname() << " -- "
+                      << remote.address().hostname();
+    RTC_LOG(LS_ERROR) << local_hostname << " -- " << remote_hostname;
+    if (local_hostname) {
+      if (remote_hostname) {
+        return kIceCandidatePairHostNameHostName;
+      } else if (remote_private) {
+        return kIceCandidatePairHostNameHostPrivate;
+      } else {
+        return kIceCandidatePairHostNameHostPublic;
+      }
+    } else if (local_private) {
+      if (remote_hostname) {
+        return kIceCandidatePairHostPrivateHostName;
+      } else if (remote_private) {
         return kIceCandidatePairHostPrivateHostPrivate;
       } else {
         return kIceCandidatePairHostPrivateHostPublic;
       }
     } else {
-      if (remote_private) {
+      if (remote_hostname) {
+        return kIceCandidatePairHostPublicHostName;
+      } else if (remote_private) {
         return kIceCandidatePairHostPublicHostPrivate;
       } else {
         return kIceCandidatePairHostPublicHostPublic;
