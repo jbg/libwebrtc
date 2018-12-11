533553889d317a20249631bb9976b7b68c074a89
Fredrik Solenberg <solenberg@webrtc.org>
2018-12-11 09:34:32 +0100

    Remove CodecInst pt.2
    The following APIs on AudioCodingModule are deprecated with this CL:
    
      static int NumberOfCodecs();
      static int Codec(int, CodecInst*);
      static int Codec(const char*, CodecInst*, int, size_t);
      static int Codec(const char*, int, size_t);
      absl::optional<CodecInst> SendCodec() const;
      bool RegisterReceiveCodec(int, const SdpAudioFormat&);
      int RegisterExternalReceiveCodec(int, AudioDecoder*, int, int, const std::string&);
      int UnregisterReceiveCodec(uint8_t);
      int32_t ReceiveCodec(CodecInst*);
      absl::optional<SdpAudioFormat> ReceiveFormat();
    
    Bug: webrtc:7626
    Change-Id: I1230732136f1fe9048cf74afdeab767ca57ac9ce

--- audio/channel_receive.h
+++ audio/channel_receive.h
@@ -13,6 +13,7 @@
 
 #include <map>
 #include <memory>
+#include <utility>
 #include <vector>
 
 #include "absl/types/optional.h"
@@ -79,7 +80,8 @@ class ChannelReceiveInterface : public RtpPacketSinkInterface {
   virtual void StartPlayout() = 0;
   virtual void StopPlayout() = 0;
 
-  virtual bool GetRecCodec(CodecInst* codec) const = 0;
+  virtual absl::optional<std::pair<int, SdpAudioFormat>>
+      GetReceiveCodec() const = 0;
 
   virtual bool ReceivedRTCPPacket(const uint8_t* data, size_t length) = 0;
 
