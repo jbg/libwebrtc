5b2d386775e0e5f43b8e262ca98e44282dc1cab8
Niels Möller <nisse@webrtc.org>
2018-12-14 13:51:11 +0100

    Add AudioDecoderFactory to NetEqTest constructor.
    Update EventLogAnalyzer to not depend on builtin audio decoders.
    
    Bug: webrtc:8396, webrtc:10080
    Change-Id: Ie02ed9cda6d4f11bfdf2e65eb6482283b7520738

--- modules/audio_coding/neteq/tools/neteq_test_factory.cc
+++ modules/audio_coding/neteq/tools/neteq_test_factory.cc
@@ -21,6 +21,7 @@
 #include <utility>
 
 #include "absl/memory/memory.h"
+#include "api/audio_codecs/builtin_audio_decoder_factory.h"
 #include "modules/audio_coding/neteq/include/neteq.h"
 #include "modules/audio_coding/neteq/tools/fake_decode_from_file.h"
 #include "modules/audio_coding/neteq/tools/input_audio_file.h"
@@ -474,9 +475,9 @@ std::unique_ptr<NetEqTest> NetEqTestFactory::InitializeTest(
   config.sample_rate_hz = *sample_rate_hz;
   config.max_packets_in_buffer = FLAG_max_nr_packets_in_buffer;
   config.enable_fast_accelerate = FLAG_enable_fast_accelerate;
-  return absl::make_unique<NetEqTest>(config, codecs, ext_codecs_,
-                                      std::move(input), std::move(output),
-                                      callbacks);
+  return absl::make_unique<NetEqTest>(
+      config, CreateBuiltinAudioDecoderFactory(), codecs, ext_codecs_,
+      std::move(input), std::move(output), callbacks);
 }
 
 }  // namespace test
