7de02af32715d814344fbe2c809a478de2aa6fd3
Ying Wang <yinwa@webrtc.org>
2019-01-07 17:43:31 +0100

    Add unittest for cwnd pushback in goog_cc.
    Bug: none
    Change-Id: Idc4ed71d8e12335eeaccbf1181eff36657f122d0

--- modules/congestion_controller/goog_cc/goog_cc_network_control_unittest.cc
+++ modules/congestion_controller/goog_cc/goog_cc_network_control_unittest.cc
@@ -210,6 +210,45 @@ TEST_F(GoogCcNetworkControllerTest, ReactsToChangedNetworkConditions) {
             kInitialBitrate * kDefaultPacingRate);
 }
 
+TEST_F(GoogCcNetworkControllerTest, CongestionWindowPushbackOnNetworkDelay) {
+  ScopedFieldTrials trial(
+      "WebRTC-CongestionWindowPushback/Enabled/WebRTC-CwndExperiment/"
+      "Enabled-200/");
+  Scenario s("googcc_unit/cwnd_on_delay", false);
+  NetworkNodeConfig net_conf;
+  auto send_net = s.CreateSimulationNode([=](NetworkNodeConfig* c) {
+    c->simulation.bandwidth = DataRate::kbps(1000);
+    c->simulation.delay = TimeDelta::ms(100);
+    c->update_frequency = TimeDelta::ms(5);
+  });
+  auto ret_net = s.CreateSimulationNode([](NetworkNodeConfig* c) {
+    c->simulation.delay = TimeDelta::ms(100);
+    c->update_frequency = TimeDelta::ms(5);
+  });
+  SimulatedTimeClientConfig config;
+  config.transport.cc =
+      TransportControllerConfig::CongestionController::kGoogCc;
+  // Start high so bandwidth drop has max effect.
+  config.transport.rates.start_rate = DataRate::kbps(1000);
+  config.transport.rates.max_rate = DataRate::kbps(2000);
+  config.transport.rates.min_rate = DataRate::kbps(10);
+  config.transport.rates.max_padding_rate = config.transport.rates.max_rate;
+  SimulatedTimeClient* client = s.CreateSimulatedTimeClient(
+      "send", config, {PacketStreamConfig()}, {send_net}, {ret_net});
+
+  // Run for a few seconds to allow the controller to stabilize.
+  s.RunFor(TimeDelta::seconds(10));
+
+  // Check that padding rate matches target rate.
+  EXPECT_NEAR(client->target_rate_kbps(), 1000, 100);
+
+  // Check this is also the case when congestion window pushback kicks in.
+  send_net->PauseTransmissionUntil(s.Now() + TimeDelta::seconds(10));
+  s.RunFor(TimeDelta::seconds(3));
+  // kDefaultMinPushbackTargetBitrateBps
+  EXPECT_NEAR(client->target_rate_kbps(), 30, 20);
+}
+
 TEST_F(GoogCcNetworkControllerTest, OnNetworkRouteChanged) {
   NetworkControlUpdate update;
   DataRate new_bitrate = DataRate::bps(200000);
