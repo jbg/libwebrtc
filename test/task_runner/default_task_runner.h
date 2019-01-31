e284df5e310311294b22bcfd1ed15f1284db9ea6
Sebastian Jansson <srte@webrtc.org>
2019-01-31 14:25:47 +0100

    Introducing TaskRunner for emulating task queue behavior.
    This CL introduces the TaskRunner system to allow similar behavior as
    for task queues in both real and simulated time.
    
    Bug: webrtc:9510
    Change-Id: I1f00a6c555a54098243a3dbb5468115da0c05f1f

---
+++ test/task_runner/default_task_runner.h
@@ -0,0 +1,43 @@
+/*
+ *  Copyright 2019 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#ifndef TEST_TASK_RUNNER_DEFAULT_TASK_RUNNER_H_
+#define TEST_TASK_RUNNER_DEFAULT_TASK_RUNNER_H_
+#include <memory>
+#include <string>
+
+#include "rtc_base/task_queue.h"
+#include "test/task_runner/task_runner_interface.h"
+
+namespace webrtc {
+class DefaultTaskRunner : public TaskRunnerImplInterface {
+ public:
+  DefaultTaskRunner(absl::string_view queue_name, TaskQueuePriority priority)
+      : task_queue_(std::string(queue_name).c_str(), priority) {}
+
+  void Invoke(std::unique_ptr<PendingTaskInterface> task) override;
+  void Post(TimeDelta delay,
+            std::unique_ptr<PendingTaskInterface> task) override;
+  RepeatingTaskHandleImplInterface* Repeat(
+      TimeDelta delay,
+      std::unique_ptr<RepeatingTaskInterface> task) override;
+  bool IsCurrent() const override;
+
+ private:
+  rtc::TaskQueue task_queue_;
+};
+
+class DefaultTaskRunnerFactory : public TaskRunnerFactory {
+ protected:
+  TaskRunnerImplInterface* Create(absl::string_view queue_name,
+                                  TaskQueuePriority priority) override;
+};
+}  // namespace webrtc
+
+#endif  // TEST_TASK_RUNNER_DEFAULT_TASK_RUNNER_H_
