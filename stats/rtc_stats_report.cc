a1ba3ff5febf4d15c0146aab223cfb4576f0061b
Mirko Bonadei <mbonadei@webrtc.org>
2019-01-31 22:01:43 +0100

    Fix RTCStatsReport::ConstIterator move constructor.
    This problem has been originally discovered by clang-tidy while trying
    to apply performance-move-const-arg [1] on [2].
    
    [1] - https://clang.llvm.org/extra/clang-tidy/checks/performance-move-const-arg.html
    [2] - https://webrtc-review.googlesource.com/c/src/+/120350
    
    Bug: webrtc:10252
    Change-Id: I2b0a116f78ca8096a6cf2bc23e2f4b8f372ca04f

--- stats/rtc_stats_report.cc
+++ stats/rtc_stats_report.cc
@@ -23,7 +23,7 @@ RTCStatsReport::ConstIterator::ConstIterator(
     StatsMap::const_iterator it)
     : report_(report), it_(it) {}
 
-RTCStatsReport::ConstIterator::ConstIterator(const ConstIterator&& other)
+RTCStatsReport::ConstIterator::ConstIterator(ConstIterator&& other)
     : report_(std::move(other.report_)), it_(std::move(other.it_)) {}
 
 RTCStatsReport::ConstIterator::~ConstIterator() {}
