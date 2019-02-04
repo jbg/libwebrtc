1be3daf0e550d50861354e83ff3426502b1b9555
Ilya Nikolaevskiy <ilnik@webrtc.org>
2019-02-04 10:17:08 +0100

    Partial frame capture API part 4
    Wire-up PartialFrameCompressor to VideoStreamEncoder.
    
    Bug:webrtc:10152
    Change-Id: I6a3df28e392cf3f47aec1c97abb1d4d73d5f7e2a

--- video/video_stream_encoder.cc
+++ video/video_stream_encoder.cc
@@ -336,6 +336,7 @@ class VideoStreamEncoder::VideoSourceProxy {
     }
     // Limit to configured max framerate.
     wants.max_framerate_fps = std::min(max_framerate_, wants.max_framerate_fps);
+    wants.partial_frames = true;
     return wants;
   }
 
@@ -712,7 +713,24 @@ void VideoStreamEncoder::ConfigureQualityScaler() {
 
 void VideoStreamEncoder::OnFrame(const VideoFrame& video_frame) {
   RTC_DCHECK_RUNS_SERIALIZED(&incoming_frame_race_checker_);
+
+  const VideoFrame::PartialFrameDescription* partial_desc =
+      video_frame.partial_frame_description();
   VideoFrame incoming_frame = video_frame;
+
+  if (video_frame.cache_buffer_for_partial_updates()) {
+    VideoFrameBuffer* input_buffer = video_frame.video_frame_buffer();
+    if (!partial_frame_assembler_.ApplyPartialUpdate(
+            input_buffer, &incoming_frame, partial_desc)) {
+      // Can't apply new image to the cached buffer - nothing sensible to
+      // encode.
+      RTC_LOG(LS_ERROR) << "Can't apply a partial update."
+                           "Discarding incoming frame.";
+      return;
+    }
+  } else {
+    partial_frame_assembler_.Reset();
+  }
 
   // Local time in webrtc time base.
   int64_t current_time_us = clock_->TimeInMicroseconds();
