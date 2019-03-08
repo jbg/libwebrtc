9b8f4390e80ee457d46849fa1328f4e1a2336546
Elad Alon <eladalon@webrtc.org>
2019-03-08 15:16:30 +0100

    IGNORE ME; WIP
    Change-Id: I67e68470b392b00ef1cca6556846494449dbb993

--- api/video_codecs/vp8_temporal_layers.cc
+++ api/video_codecs/vp8_temporal_layers.cc
@@ -10,10 +10,67 @@
 
 #include "api/video_codecs/vp8_temporal_layers.h"
 
+#include "rtc_base/checks.h"
+
 namespace webrtc {
 
-void Vp8TemporalLayers::OnPacketLossRateUpdate(float packet_loss_rate) {}
+Vp8TemporalLayers::Vp8TemporalLayers(
+    std::vector<std::unique_ptr<Vp8FrameBufferController>>* controllers) {
+  controllers_.swap(*controllers);
+}
 
-void Vp8TemporalLayers::OnRttUpdate(int64_t rtt_ms) {}
+size_t Vp8TemporalLayers::StreamCount() const {
+  return controllers_.size();
+}
+
+bool Vp8TemporalLayers::SupportsEncoderFrameDropping(
+    size_t stream_index) const {
+  RTC_DCHECK_LT(stream_index, controllers_.size());
+  return controllers_[stream_index]->SupportsEncoderFrameDropping(0);
+}
+
+void Vp8TemporalLayers::OnRatesUpdated(
+    size_t stream_index,
+    const std::vector<uint32_t>& bitrates_bps,
+    int framerate_fps) {
+  RTC_DCHECK_LT(stream_index, controllers_.size());
+  return controllers_[stream_index]->OnRatesUpdated(0, bitrates_bps,
+                                                    framerate_fps);
+}
+
+bool Vp8TemporalLayers::UpdateConfiguration(size_t stream_index,
+                                            Vp8EncoderConfig* cfg) {
+  RTC_DCHECK_LT(stream_index, controllers_.size());
+  return controllers_[stream_index]->UpdateConfiguration(0, cfg);
+}
+
+Vp8FrameConfig Vp8TemporalLayers::UpdateLayerConfig(size_t stream_index,
+                                                    uint32_t rtp_timestamp) {
+  RTC_DCHECK_LT(stream_index, controllers_.size());
+  return controllers_[stream_index]->UpdateLayerConfig(0, rtp_timestamp);
+}
+
+void Vp8TemporalLayers::OnEncodeDone(size_t stream_index,
+                                     uint32_t rtp_timestamp,
+                                     size_t size_bytes,
+                                     bool is_keyframe,
+                                     int qp,
+                                     CodecSpecificInfo* info) {
+  RTC_DCHECK_LT(stream_index, controllers_.size());
+  return controllers_[stream_index]->OnEncodeDone(0, rtp_timestamp, size_bytes,
+                                                  is_keyframe, qp, info);
+}
+
+void Vp8TemporalLayers::OnPacketLossRateUpdate(float packet_loss_rate) {
+  for (auto& controller : controllers_) {
+    controller->OnPacketLossRateUpdate(packet_loss_rate);
+  }
+}
+
+void Vp8TemporalLayers::OnRttUpdate(int64_t rtt_ms) {
+  for (auto& controller : controllers_) {
+    controller->OnRttUpdate(rtt_ms);
+  }
+}
 
 }  // namespace webrtc
--- modules/video_coding/video_codec_initializer_unittest.cc
+++ modules/video_coding/video_codec_initializer_unittest.cc
@@ -20,9 +20,9 @@
 #include "api/video/video_bitrate_allocation.h"
 #include "api/video/video_bitrate_allocator.h"
 #include "api/video/video_bitrate_allocator_factory.h"
-#include "api/video_codecs/create_vp8_temporal_layers.h"
 #include "api/video_codecs/video_encoder.h"
 #include "api/video_codecs/vp8_temporal_layers.h"
+#include "api/video_codecs/vp8_temporal_layers_factory.h"
 #include "common_types.h"  // NOLINT(build/include)
 #include "modules/video_coding/codecs/vp9/include/vp9_globals.h"
 #include "rtc_base/checks.h"
@@ -86,7 +86,7 @@ class VideoCodecInitializerTest : public ::testing::Test {
 
   bool InitializeCodec() {
     codec_out_ = VideoCodec();
-    temporal_layers_.clear();
+    frame_buffer_controllers_.clear();
     if (!VideoCodecInitializer::SetupCodec(config_, streams_, &codec_out_)) {
       return false;
     }
@@ -98,10 +98,10 @@ class VideoCodecInitializerTest : public ::testing::Test {
 
     // Make sure temporal layers instances have been created.
     if (codec_out_.codecType == VideoCodecType::kVideoCodecVP8) {
+      // TODO(bugs.webrtc.org/10382): Inject the factory.
+      Vp8TemporalLayersFactory factory;
       for (int i = 0; i < codec_out_.numberOfSimulcastStreams; ++i) {
-        temporal_layers_.emplace_back(
-            CreateVp8TemporalLayers(Vp8TemporalLayersType::kFixedPattern,
-                                    codec_out_.VP8()->numberOfTemporalLayers));
+        frame_buffer_controllers_.push_back(factory.Create(codec_out_));
       }
     }
     return true;
@@ -139,7 +139,8 @@ class VideoCodecInitializerTest : public ::testing::Test {
   // Output.
   VideoCodec codec_out_;
   std::unique_ptr<VideoBitrateAllocator> bitrate_allocator_;
-  std::vector<std::unique_ptr<Vp8TemporalLayers>> temporal_layers_;
+  std::vector<std::unique_ptr<Vp8FrameBufferController>>
+      frame_buffer_controllers_;
 };
 
 TEST_F(VideoCodecInitializerTest, SingleStreamVp8Screenshare) {
