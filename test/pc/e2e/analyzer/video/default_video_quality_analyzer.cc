333cdc5684338b8228935763e2e4c3c76cd2e72c
Mirko Bonadei <mbonadei@webrtc.org>
2019-04-12 13:34:25 +0200

    Add Video Bwe stats collection to DefaultVideoQualityAnalyzer.
    This CL adds the possibility to collect the following Video BWE stats:
    - available_send_bandwidth
    - transmission_bitrate
    - retransmission_bitrate
    - actual_encode_bitrate
    - target_encode_bitrate
    
    Example of the output:
    
    RESULT available_send_bandwidth: smoke_test/alice= {288305.5,18652.394} bits
    RESULT transmission_bitrate: smoke_test/alice= {267164.5,124387.5} bit/s
    RESULT retransmission_bitrate: smoke_test/alice= {8315.6667,10257.883} bit/s
    RESULT actual_encode_bitrate: smoke_test/alice= {248297.33,116856.95} bit/s
    RESULT target_encode_bitrate: smoke_test/alice= {278902.33,14916.152} bit/s
    RESULT available_send_bandwidth: smoke_test/bob= {315545.17,32648.568} bits
    RESULT transmission_bitrate: smoke_test/bob= {289454,134413.41} bit/s
    RESULT retransmission_bitrate: smoke_test/bob= {0,0} bit/s
    RESULT actual_encode_bitrate: smoke_test/bob= {277975.83,132850.94} bit/s
    RESULT target_encode_bitrate: smoke_test/bob= {314151,32335.949} bit/s
    
    Bug: webrtc:10138
    Change-Id: I0414055af0010b8fb4d909297e6da86d398157c2

--- test/pc/e2e/analyzer/video/default_video_quality_analyzer.cc
+++ test/pc/e2e/analyzer/video/default_video_quality_analyzer.cc
@@ -352,6 +352,52 @@ AnalyzerStats DefaultVideoQualityAnalyzer::GetAnalyzerStats() const {
   return analyzer_stats_;
 }
 
+// TODO(bugs.webrtc.org/10430): Migrate to the new GetStats as soon as
+// bugs.webrtc.org/10428 is fixed.
+void DefaultVideoQualityAnalyzer::OnStatsReports(
+    absl::string_view pc_label,
+    const StatsReports& stats_reports) {
+  for (const StatsReport* stats_report : stats_reports) {
+    // The only stats collected by this analyzer are present in
+    // kStatsReportTypeBwe reports, so all other reports are just ignored.
+    if (stats_report->type() != StatsReport::StatsType::kStatsReportTypeBwe) {
+      continue;
+    }
+    const webrtc::StatsReport::Value* available_send_bandwidth =
+        stats_report->FindValue(
+            StatsReport::StatsValueName::kStatsValueNameAvailableSendBandwidth);
+    const webrtc::StatsReport::Value* retransmission_bitrate =
+        stats_report->FindValue(
+            StatsReport::StatsValueName::kStatsValueNameRetransmitBitrate);
+    const webrtc::StatsReport::Value* transmission_bitrate =
+        stats_report->FindValue(
+            StatsReport::StatsValueName::kStatsValueNameTransmitBitrate);
+    const webrtc::StatsReport::Value* actual_encode_bitrate =
+        stats_report->FindValue(
+            StatsReport::StatsValueName::kStatsValueNameActualEncBitrate);
+    const webrtc::StatsReport::Value* target_encode_bitrate =
+        stats_report->FindValue(
+            StatsReport::StatsValueName::kStatsValueNameTargetEncBitrate);
+    RTC_CHECK(available_send_bandwidth);
+    RTC_CHECK(retransmission_bitrate);
+    RTC_CHECK(transmission_bitrate);
+    RTC_CHECK(actual_encode_bitrate);
+    RTC_CHECK(target_encode_bitrate);
+
+    VideoBweStats& video_bwe_stats = video_bwe_stats_[pc_label];
+    video_bwe_stats.available_send_bandwidth.AddSample(
+        available_send_bandwidth->int_val());
+    video_bwe_stats.transmission_bitrate.AddSample(
+        transmission_bitrate->int_val());
+    video_bwe_stats.retransmission_bitrate.AddSample(
+        retransmission_bitrate->int_val());
+    video_bwe_stats.actual_encode_bitrate.AddSample(
+        actual_encode_bitrate->int_val());
+    video_bwe_stats.target_encode_bitrate.AddSample(
+        target_encode_bitrate->int_val());
+  }
+}
+
 void DefaultVideoQualityAnalyzer::AddComparison(
     absl::optional<VideoFrame> captured,
     absl::optional<VideoFrame> rendered,
@@ -500,6 +546,24 @@ void DefaultVideoQualityAnalyzer::ReportResults() {
   RTC_LOG(INFO) << "comparisons_done=" << analyzer_stats_.comparisons_done;
   RTC_LOG(INFO) << "overloaded_comparisons_done="
                 << analyzer_stats_.overloaded_comparisons_done;
+  for (const auto& item : video_bwe_stats_) {
+    ReportVideoBweResults(GetTestCaseName(item.first), item.second);
+  }
+}
+
+void DefaultVideoQualityAnalyzer::ReportVideoBweResults(
+    std::string test_case_name,
+    const VideoBweStats video_bwe_stats) {
+  ReportResult("available_send_bandwidth", test_case_name,
+               video_bwe_stats.available_send_bandwidth, "bits");
+  ReportResult("transmission_bitrate", test_case_name,
+               video_bwe_stats.transmission_bitrate, "bit/s");
+  ReportResult("retransmission_bitrate", test_case_name,
+               video_bwe_stats.retransmission_bitrate, "bit/s");
+  ReportResult("actual_encode_bitrate", test_case_name,
+               video_bwe_stats.actual_encode_bitrate, "bit/s");
+  ReportResult("target_encode_bitrate", test_case_name,
+               video_bwe_stats.target_encode_bitrate, "bit/s");
 }
 
 void DefaultVideoQualityAnalyzer::ReportResults(std::string test_case_name,
