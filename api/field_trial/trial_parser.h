f3d5f675803c859c6b0ee0750fa83fc16be1cb0b
Sebastian Jansson <srte@webrtc.org>
2019-04-12 16:45:31 +0200

    Exposing field trial parser in api.
    Bug: None
    Change-Id: I52e6222e0b42f5d0faa9b7e7114d1ce46c59a1a6

---
+++ api/field_trial/trial_parser.h
@@ -0,0 +1,18 @@
+/*
+ *  Copyright 2019 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#ifndef API_FIELD_TRIAL_TRIAL_PARSER_H_
+#define API_FIELD_TRIAL_TRIAL_PARSER_H_
+
+// TODO(srte): Move included filed directly to API.
+#include "rtc_base/experiments/field_trial_list.h"
+#include "rtc_base/experiments/field_trial_parser.h"
+#include "rtc_base/experiments/field_trial_units.h"
+
+#endif  // API_FIELD_TRIAL_TRIAL_PARSER_H_
