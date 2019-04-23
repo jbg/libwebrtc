cf27676a3099501edfbac4cf2e166c3d31bdd80f
Elad Alon <eladalon@webrtc.org>
2019-04-23 14:48:57 +0200

    Avoid unnecessary rescaling in LibvpxVp8Encoder::Encode
    Bug: webrtc:10571
    Change-Id: I3e41b299eb13a53178fd9179875239919ac57a28

--- modules/video_coding/codecs/vp8/libvpx_vp8_encoder.cc
+++ modules/video_coding/codecs/vp8/libvpx_vp8_encoder.cc
@@ -887,6 +887,21 @@ int LibvpxVp8Encoder::Encode(const VideoFrame& frame,
     framerate_controller_.AddFrame(frame.timestamp() / kRtpTicksPerMs);
   }
 
+  vpx_enc_frame_flags_t flags[kMaxSimulcastStreams];
+  Vp8FrameConfig tl_configs[kMaxSimulcastStreams];
+  for (size_t i = 0; i < encoders_.size(); ++i) {
+    tl_configs[i] =
+        frame_buffer_controller_->NextFrameConfig(i, frame.timestamp());
+    if (tl_configs[i].drop_frame) {
+      if (send_key_frame) {
+        continue;
+      }
+      // Drop this frame.
+      return WEBRTC_VIDEO_CODEC_OK;
+    }
+    flags[i] = EncodeFlags(tl_configs[i]);
+  }
+
   input_image_ = frame.video_frame_buffer()->ToI420();
   // Since we are extracting raw pointers from |input_image_| to
   // |raw_images_[0]|, the resolution of these frames must match.
@@ -922,20 +937,6 @@ int LibvpxVp8Encoder::Encode(const VideoFrame& frame,
         raw_images_[i].d_h, libyuv::kFilterBilinear);
   }
 
-  vpx_enc_frame_flags_t flags[kMaxSimulcastStreams];
-  Vp8FrameConfig tl_configs[kMaxSimulcastStreams];
-  for (size_t i = 0; i < encoders_.size(); ++i) {
-    tl_configs[i] =
-        frame_buffer_controller_->NextFrameConfig(i, frame.timestamp());
-    if (tl_configs[i].drop_frame) {
-      if (send_key_frame) {
-        continue;
-      }
-      // Drop this frame.
-      return WEBRTC_VIDEO_CODEC_OK;
-    }
-    flags[i] = EncodeFlags(tl_configs[i]);
-  }
   if (send_key_frame) {
     // Adapt the size of the key frame when in screenshare with 1 temporal
     // layer.
