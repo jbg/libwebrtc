a455b0f385818ead10b9f2d82c98297448fd16ab
Per Kjellander <perkj@webrtc.org>
2019-04-26 10:05:10 +0200

    Change AlrDetector to be able to set field trials that only affect AlrDetector
    Bug: webrtc:10542
    Change-Id: If7cb4086dfcfb313c6ffc0b8f662b8eae5bd4355

--- modules/congestion_controller/goog_cc/alr_detector.h
+++ modules/congestion_controller/goog_cc/alr_detector.h
@@ -17,6 +17,8 @@
 #include "absl/types/optional.h"
 #include "api/transport/webrtc_key_value_config.h"
 #include "modules/pacing/interval_budget.h"
+#include "rtc_base/experiments/alr_experiment.h"
+#include "rtc_base/experiments/field_trial_units.h"
 
 namespace webrtc {
 
@@ -57,11 +59,19 @@ class AlrDetector {
   void UpdateBudgetWithElapsedTime(int64_t delta_time_ms);
   void UpdateBudgetWithBytesSent(size_t bytes_sent);
 
+  int bandwidth_usage_percent() const;
+  int start_budget_level_percent() const;
+  int stop_budget_level_percent() const;
+
  private:
+  AlrDetector(const WebRtcKeyValueConfig* key_value_config,
+              RtcEventLog* event_log,
+              absl::optional<AlrExperimentSettings> experiment_settings);
+
   friend class GoogCcStatePrinter;
-  int bandwidth_usage_percent_;
-  int alr_start_budget_level_percent_;
-  int alr_stop_budget_level_percent_;
+  FieldTrialParameter<int> bandwidth_usage_percent_;
+  FieldTrialParameter<int> alr_start_budget_level_percent_;
+  FieldTrialParameter<int> alr_stop_budget_level_percent_;
 
   absl::optional<int64_t> last_send_time_ms_;
 
