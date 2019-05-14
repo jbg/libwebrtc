db9a68d95b5c17b4f8e83a5ee2dbadc28138993e
Mirko Bonadei <mbonadei@webrtc.org>
2019-05-14 22:17:06 +0200

    Switch reference_less_video_analysis to ABSL_FLAG.
    Bug: webrtc:10616
    Change-Id: I0cd4a54cbc4decea5b530dbda8a672118ba5a9b3

--- rtc_tools/frame_analyzer/reference_less_video_analysis.cc
+++ rtc_tools/frame_analyzer/reference_less_video_analysis.cc
@@ -9,36 +9,39 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string>
 
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
 #include "rtc_tools/frame_analyzer/reference_less_video_analysis_lib.h"
-#include "rtc_tools/simple_command_line_parser.h"
+
+ABSL_FLAG(
+    std::string,
+    video_file,
+    "",
+    "Path of the video file to be analyzer. Only y4m file format is supported");
 
 int main(int argc, char* argv[]) {
   // This captures the freezing metrics for reference less video analysis.
-  std::string program_name = argv[0];
-  std::string usage =
-      "Outputs the freezing score by comparing current frame "
-      "with the previous frame.\nExample usage:\n" +
-      program_name +
-      " --video_file=video_file.y4m\n"
-      "Command line flags:\n"
-      "  - video_file(string): Path of the video "
-      "file to be analyzed. Only y4m file format is supported.\n";
 
-  webrtc::test::CommandLineParser parser;
+  absl::ParseCommandLine(argc, argv);
 
-  // Init the parser and set the usage message.
-  parser.Init(argc, argv);
-  parser.SetUsageMessage(usage);
+  // TODO(bugs.webrtc.org/10616): Add program usage message when Abseil
+  // flags supports it.
+  //
+  // std::string usage =
+  //    "Outputs the freezing score by comparing current frame "
+  //    "with the previous frame.\nExample usage:\n" +
+  //    program_name +
+  //    " --video_file=video_file.y4m\n"
+  //    "Command line flags:\n"
+  //    "  - video_file(string): Path of the video "
+  //    "file to be analyzed. Only y4m file format is supported.\n";
 
-  parser.SetFlag("video_file", "");
-  parser.ProcessFlags();
-  if (parser.GetFlag("video_file").empty()) {
-    parser.PrintUsageMessage();
+  if (absl::GetFlag(FLAGS_video_file).empty()) {
     exit(EXIT_SUCCESS);
   }
-  std::string video_file = parser.GetFlag("video_file");
+  std::string video_file = absl::GetFlag(FLAGS_video_file);
 
   return run_analysis(video_file);
 }
