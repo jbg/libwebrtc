30b3920e64710d423b2b998a473b7ea085a3f93c
Anton Sukhanov <sukhanov@webrtc.org>
2019-05-22 15:35:41 -0700

    Datagram Transport Integration
    - Implement datagram transport adaptor, which wraps datagram transport in DtlsTransportInternal. Datagram adaptor owns both ICE and Datagram Transports.
    - Implement setup of datagram transport based on RTCConfiguration flag use_datagram_transport. This is very similar to MediaTransport setup with the exception that we create DTLS datagram adaptor.
    - Propagate maximum datagram size to video encoder via MediaTransportConfig.
    
    TODO: Currently this CL can only be tested in downstream projects. Once we add fake datagram transport, we will be able to implement unit tests similar to loopback media transport.
    
    Bug: webrtc:9719
    Change-Id: I4fa4a5725598dfee5da4f0f374269a7e289d48ed

--- api/media_transport_config.cc
+++ api/media_transport_config.cc
@@ -10,11 +10,32 @@
 
 #include "api/media_transport_config.h"
 
+#include "rtc_base/checks.h"
+#include "rtc_base/string_utils.h"
+#include "rtc_base/strings/string_builder.h"
+
 namespace webrtc {
 
+MediaTransportConfig::MediaTransportConfig(
+    MediaTransportInterface* media_transport)
+    : media_transport(media_transport) {
+  RTC_DCHECK(media_transport != nullptr);
+}
+
+MediaTransportConfig::MediaTransportConfig(size_t rtp_max_packet_size)
+    : rtp_max_packet_size(rtp_max_packet_size) {
+  RTC_DCHECK_GT(rtp_max_packet_size, 0);
+}
+
 std::string MediaTransportConfig::DebugString() const {
-  return (media_transport != nullptr ? "{media_transport: (Transport)}"
-                                     : "{media_transport: null}");
+  rtc::StringBuilder result;
+  result << "{media_transport:"
+         << (media_transport != nullptr ? "(Transport)" : "null")
+         << ", rtp_max_packet_size:"
+         << (rtp_max_packet_size ? rtc::ToString(rtp_max_packet_size.value())
+                                 : "nullopt")
+         << "}";
+  return result.Release();
 }
 
 }  // namespace webrtc
