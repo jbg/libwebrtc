85329cf2a0e89db0403fffd00a2b2114dd6be49f
Harald Alvestrand <hta@webrtc.org>
2019-07-21 14:58:35 -0400

    Deprecate old-style OnFailure callbacks
    Also delete default implementation of new-style OnFailure,
    since it can't call the deprecated function.
    
    Bug: chromium:589455
    Change-Id: Icf529ddb02d99ad9e205095d5a1fbeb0da91dd0e

--- api/jsep.h
+++ api/jsep.h
@@ -221,8 +221,8 @@ class RTC_EXPORT CreateSessionDescriptionObserver
   // is deprecated; in order to let clients remove the old version, it has a
   // default implementation. If both versions are unimplemented, the
   // result will be a runtime error (stack overflow). This is intentional.
-  virtual void OnFailure(RTCError error);
-  virtual void OnFailure(const std::string& error);
+  virtual void OnFailure(RTCError error) = 0;
+  RTC_DEPRECATED virtual void OnFailure(const std::string& error);
 
  protected:
   ~CreateSessionDescriptionObserver() override = default;
@@ -233,9 +233,9 @@ class RTC_EXPORT SetSessionDescriptionObserver : public rtc::RefCountInterface {
  public:
   virtual void OnSuccess() = 0;
   // See description in CreateSessionDescriptionObserver for OnFailure.
-  virtual void OnFailure(RTCError error);
+  virtual void OnFailure(RTCError error) = 0;
 
-  virtual void OnFailure(const std::string& error);
+  RTC_DEPRECATED virtual void OnFailure(const std::string& error);
 
  protected:
   ~SetSessionDescriptionObserver() override = default;
