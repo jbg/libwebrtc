76858f102e391413ce9c2f5450fb712e556a9676
Kári Tristan Helgason <kthelgason@webrtc.org>
2019-08-19 15:07:50 +0200

    Declare references as constant in the metal renderers.
    This silences a warning that appeared with iOS 13, and is more efficient
    in general.
    
    Bug: webrtc:10866
    Change-Id: I23db6b78af36e59b1d825d3f0cccc6008f9b626a

--- sdk/objc/components/renderer/metal/RTCMTLNV12Renderer.mm
+++ sdk/objc/components/renderer/metal/RTCMTLNV12Renderer.mm
@@ -34,19 +34,20 @@ static NSString *const shaderSource = MTL_STRINGIFY(
       float2 texcoord;
     } Varyings;
 
-    vertex Varyings vertexPassthrough(device Vertex * verticies[[buffer(0)]],
-                                      unsigned int vid[[vertex_id]]) {
+    vertex Varyings vertexPassthrough(constant Vertex * verticies [[buffer(0)]],
+                                      unsigned int vid [[vertex_id]]) {
       Varyings out;
-      device Vertex &v = verticies[vid];
+      constant Vertex &v = verticies[vid];
       out.position = float4(float2(v.position), 0.0, 1.0);
       out.texcoord = v.texcoord;
       return out;
     }
 
     // Receiving YCrCb textures.
-    fragment half4 fragmentColorConversion(
-        Varyings in[[stage_in]], texture2d<float, access::sample> textureY[[texture(0)]],
-        texture2d<float, access::sample> textureCbCr[[texture(1)]]) {
+    fragment half4 fragmentColorConversion(Varyings in [[stage_in]],
+                                           texture2d<float, access::sample> textureY [[texture(0)]],
+                                           texture2d<float, access::sample> textureCbCr
+                                           [[texture(1)]]) {
       constexpr sampler s(address::clamp_to_edge, filter::linear);
       float y;
       float2 uv;
