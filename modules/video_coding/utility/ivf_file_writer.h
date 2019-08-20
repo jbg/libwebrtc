d47ed1603f072a13b23aaaba754344aa0595cec8
Markus Handell <handellm@webrtc.org>
2019-08-20 18:01:50 +0200

    Test
    Change-Id: I286e9a891f6bc6e866e0e5071da3aca0b4a3733d

--- modules/video_coding/utility/ivf_file_writer.h
+++ modules/video_coding/utility/ivf_file_writer.h
@@ -16,6 +16,7 @@
 
 #include <memory>
 
+#include "api/output_stream.h"
 #include "api/video/encoded_image.h"
 #include "rtc_base/constructor_magic.h"
 #include "rtc_base/system/file_wrapper.h"
@@ -25,20 +26,28 @@ namespace webrtc {
 
 class IvfFileWriter {
  public:
-  // Takes ownership of the file, which will be closed either through
-  // Close or ~IvfFileWriter. If writing a frame would take the file above the
-  // |byte_limit| the file will be closed, the write (and all future writes)
-  // will fail. A |byte_limit| of 0 is equivalent to no limit.
+  // Takes ownership of the file, which will be closed on ~IvfFileWriter. If
+  // writing a frame would take the file above the |byte_limit| the file will be
+  // closed, the write (and all future writes) will fail. A |byte_limit| of 0 is
+  // equivalent to no limit.
   static std::unique_ptr<IvfFileWriter> Wrap(FileWrapper file,
                                              size_t byte_limit);
+
+  // Wraps an output stream which will be used to pass data in IVF file format.
+  // The caller needs to guarantee that the |output_stream| object exists
+  // through the IvfFileWriter's existence.
+  static std::unique_ptr<IvfFileWriter> Wrap(
+      std::unique_ptr<RewindableOutputStream> output_stream,
+      size_t byte_limit);
+
   ~IvfFileWriter();
 
   bool WriteFrame(const EncodedImage& encoded_image, VideoCodecType codec_type);
-  bool Close();
 
  private:
-  explicit IvfFileWriter(FileWrapper file, size_t byte_limit);
-
+  explicit IvfFileWriter(std::unique_ptr<RewindableOutputStream> output_stream,
+                         size_t byte_limit);
+  void Close();
   bool WriteHeader();
   bool InitFromFirstFrame(const EncodedImage& encoded_image,
                           VideoCodecType codec_type);
@@ -52,7 +61,7 @@ class IvfFileWriter {
   int64_t last_timestamp_;
   bool using_capture_timestamps_;
   rtc::TimestampWrapAroundHandler wrap_handler_;
-  FileWrapper file_;
+  std::unique_ptr<RewindableOutputStream> output_stream_;
 
   RTC_DISALLOW_COPY_AND_ASSIGN(IvfFileWriter);
 };
