063e1c93bacaaa33f2a25489206b21c9ed7a9da2
Artem Titov <titovartem@webrtc.org>
2019-09-06 12:18:46 +0200

    Add support of displaying video during the PC level test
    Bug: webrtc:10138
    Change-Id: Ic74b58bc4f1be1793e0dd1a0c286f8d4200fe6f2

--- test/pc/e2e/analyzer/video/video_quality_analyzer_injection_helper.cc
+++ test/pc/e2e/analyzer/video/video_quality_analyzer_injection_helper.cc
@@ -16,6 +16,7 @@
 #include "test/pc/e2e/analyzer/video/quality_analyzing_video_decoder.h"
 #include "test/pc/e2e/analyzer/video/quality_analyzing_video_encoder.h"
 #include "test/pc/e2e/analyzer/video/simulcast_dummy_buffer_helper.h"
+#include "test/video_renderer.h"
 
 namespace webrtc {
 namespace webrtc_pc_e2e {
@@ -35,6 +36,21 @@ class VideoWriter final : public rtc::VideoSinkInterface<VideoFrame> {
 
  private:
   test::VideoFrameWriter* video_writer_;
+};
+
+class VideoRenderedsink final : public rtc::VideoSinkInterface<VideoFrame> {
+ public:
+  VideoRenderedsink(std::string title, size_t width, size_t height)
+      : title_(std::move(title)) {
+    renderer_ = absl::WrapUnique(
+        test::VideoRenderer::Create(title_.c_str(), width, height));
+  }
+
+  void OnFrame(const VideoFrame& frame) override { renderer_->OnFrame(frame); }
+
+ private:
+  const std::string title_;
+  std::unique_ptr<test::VideoRenderer> renderer_;
 };
 
 // Intercepts generated frames and passes them also to video quality analyzer
@@ -58,8 +74,8 @@ class AnalyzingFrameGenerator final : public test::FrameGenerator {
     uint16_t frame_id = analyzer_->OnFrameCaptured(stream_label_, *frame);
     frame->set_id(frame_id);
 
-    for (auto& listener : sinks_) {
-      listener->OnFrame(*frame);
+    for (auto& sink : sinks_) {
+      sink->OnFrame(*frame);
     }
     return frame;
   }
@@ -94,8 +110,8 @@ class AnalyzingVideoSink final : public rtc::VideoSinkInterface<VideoFrame> {
       return;
     }
     analyzer_->OnFrameRendered(frame);
-    for (auto& listener : sinks_) {
-      listener->OnFrame(frame);
+    for (auto& sink : sinks_) {
+      sink->OnFrame(frame);
     }
   }
 
@@ -143,24 +159,33 @@ VideoQualityAnalyzerInjectionHelper::WrapVideoDecoderFactory(
 
 std::unique_ptr<test::FrameGenerator>
 VideoQualityAnalyzerInjectionHelper::WrapFrameGenerator(
-    std::string stream_label,
+    const VideoConfig& config,
     std::unique_ptr<test::FrameGenerator> delegate,
     test::VideoFrameWriter* writer) const {
   std::vector<std::unique_ptr<rtc::VideoSinkInterface<VideoFrame>>> sinks;
   if (writer) {
     sinks.push_back(absl::make_unique<VideoWriter>(writer));
   }
+  if (config.show_on_screen) {
+    sinks.push_back(absl::make_unique<VideoRenderedsink>(
+        *config.stream_label + "-capture", config.width, config.height));
+  }
   return absl::make_unique<AnalyzingFrameGenerator>(
-      std::move(stream_label), std::move(delegate), analyzer_.get(),
+      std::move(*config.stream_label), std::move(delegate), analyzer_.get(),
       std::move(sinks));
 }
 
 std::unique_ptr<rtc::VideoSinkInterface<VideoFrame>>
 VideoQualityAnalyzerInjectionHelper::CreateVideoSink(
+    const VideoConfig& config,
     test::VideoFrameWriter* writer) const {
   std::vector<std::unique_ptr<rtc::VideoSinkInterface<VideoFrame>>> sinks;
   if (writer) {
     sinks.push_back(absl::make_unique<VideoWriter>(writer));
+  }
+  if (config.show_on_screen) {
+    sinks.push_back(absl::make_unique<VideoRenderedsink>(
+        *config.stream_label + "-render", config.width, config.height));
   }
   return absl::make_unique<AnalyzingVideoSink>(analyzer_.get(),
                                                std::move(sinks));
