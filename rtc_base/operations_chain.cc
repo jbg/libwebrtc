dbd0ae6a0785f712235ae6f26aa0f10c9a3b79e7
Henrik Boström <hbos@webrtc.org>
2019-10-14 18:32:22 +0200

    Implement an OperationsChain, to be used by PeerConnection in follow-up.
    This allows asynchronous tasks to be queued to be executed in order.
    The class is motivated by the "operations chain" in the spec:
    https://w3c.github.io/webrtc-pc/#dfn-operations-chain
    
    In a follow-up CL I intend to use this in PeerConnection's
    CreateOffer(), CreateAnswer() SetLocalDescription() and
    SetRemoteDescription() and unblock https://crbug.com/980885.
    
    Bug: webrtc:11019
    Change-Id: I982e4a1c0e77fa62096c16deed459d9d9e9b63f0

---
+++ rtc_base/operations_chain.cc
@@ -0,0 +1,53 @@
+/*
+ *  Copyright 2019 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "rtc_base/operations_chain.h"
+
+#include "rtc_base/checks.h"
+
+namespace rtc {
+
+// static
+scoped_refptr<OperationsChainCallback> OperationsChainCallback::Create(
+    scoped_refptr<OperationsChain> operations_chain) {
+  return new OperationsChainCallback(std::move(operations_chain));
+}
+
+OperationsChainCallback::OperationsChainCallback(
+    scoped_refptr<OperationsChain> operations_chain)
+    : operations_chain_(std::move(operations_chain)) {}
+
+OperationsChainCallback::~OperationsChainCallback() {
+  RTC_DCHECK(has_run_);
+}
+
+void OperationsChainCallback::OnOperationComplete() {
+  RTC_DCHECK(!has_run_);
+  has_run_ = true;
+  operations_chain_->OnOperationComplete();
+}
+
+// static
+scoped_refptr<OperationsChain> OperationsChain::Create() {
+  return new OperationsChain();
+}
+
+OperationsChain::OperationsChain() : RefCountedObject() {
+  RTC_DCHECK_RUN_ON(&sequence_checker_);
+}
+
+OperationsChain::~OperationsChain() {
+  // Operations keep the chain alive through reference counting so this should
+  // not be possible. The fact that the chain is empty makes it safe to
+  // destroy the OperationsChain on any sequence.
+  RTC_DCHECK(chained_operations_.empty());
+}
+
+}  // namespace rtc
