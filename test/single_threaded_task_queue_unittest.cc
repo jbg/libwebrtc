ae12fccd8610a7964ee9770e6a4d7174032236f8
Danil Chapovalov <danilchap@webrtc.org>
2019-10-15 11:58:29 +0200

    Merge SendTask implementation for SingleThreadedTaskQueueForTesting and TaskQueueForTest
    Bug: webrtc:10933
    Change-Id: I60738434b46e77b4644173ad168bc0efa58459b6

--- test/single_threaded_task_queue_unittest.cc
+++ test/single_threaded_task_queue_unittest.cc
@@ -16,6 +16,7 @@
 
 #include "api/task_queue/task_queue_test.h"
 #include "rtc_base/event.h"
+#include "rtc_base/task_queue_for_test.h"
 #include "test/gtest.h"
 
 namespace webrtc {
@@ -320,9 +321,9 @@ TEST(DEPRECATED_SingleThreadedTaskQueueForTestingTest, SendTask) {
 
   std::atomic<bool> executed(false);
 
-  task_queue.SendTask([&executed]() {
-    // Intentionally delay, so that if SendTask didn't block, the sender thread
-    // would have time to read |executed|.
+  SendTask(RTC_FROM_HERE, &task_queue, [&executed]() {
+    // Intentionally delay, so that if SendTask didn't block, the sender
+    // thread would have time to read |executed|.
     rtc::Event delay;
     ASSERT_FALSE(delay.Wait(1000));
     executed.store(true);
