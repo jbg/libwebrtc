88088399212b56dd6507e21b387d8820eb0325ef
Ilya Nikolaevskiy <ilnik@webrtc.org>
2019-10-25 15:00:52 +0200

    Fixes
    Add debug output:
    
    
    disable all extra tests
    
    Change-Id: Ieca5bfc3d27861d33bbcf065b77c6df6a6c19a14

--- modules/audio_coding/codecs/opus/opus_complexity_unittest.cc
+++ modules/audio_coding/codecs/opus/opus_complexity_unittest.cc
@@ -59,7 +59,7 @@ int64_t RunComplexityTest(const AudioEncoderOpusConfig& config) {
 // mobiles, the regular complexity is 5, and we expect the resulting ratio to
 // be higher, since we have explicitly asked for a higher complexity setting at
 // the lower rate.
-TEST(AudioEncoderOpusComplexityAdaptationTest, AdaptationOn) {
+TEST(AudioEncoderOpusComplexityAdaptationTest, DISABLED_AdaptationOn) {
   // Create config.
   AudioEncoderOpusConfig config;
   // The limit -- including the hysteresis window -- at which the complexity
@@ -79,7 +79,7 @@ TEST(AudioEncoderOpusComplexityAdaptationTest, AdaptationOn) {
 // This test is identical to the one above, but without the complexity
 // adaptation enabled (neither on desktop, nor on mobile). The expectation is
 // that the resulting ratio is less than 100% at all times.
-TEST(AudioEncoderOpusComplexityAdaptationTest, AdaptationOff) {
+TEST(AudioEncoderOpusComplexityAdaptationTest, DISABLED_AdaptationOff) {
   // Create config.
   AudioEncoderOpusConfig config;
   // The limit -- including the hysteresis window -- at which the complexity
