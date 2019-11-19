f8ca0c8e5480879294e9176f4fce059e26924062
Danil Chapovalov <danilchap@webrtc.org>
2019-11-19 14:20:49 +0100

    Add RtpDepacketizerAv1::AssembleFrame function
    Bug: webrtc:11042
    Change-Id: I677fc6a9affacf3b7c80adc2c3493c16806db1f6

--- modules/rtp_rtcp/source/rtp_depacketizer_av1.cc
+++ modules/rtp_rtcp/source/rtp_depacketizer_av1.cc
@@ -58,7 +58,115 @@ namespace {
 //    +-+-+-+-+-+-+-+-+
 //    |  OBU payload  |
 //    |     ...       |
+class ArrayOfArrayViews {
+ public:
+  class const_iterator;
+  ArrayOfArrayViews() = default;
+  ArrayOfArrayViews(const ArrayOfArrayViews&) = default;
+  ArrayOfArrayViews& operator=(const ArrayOfArrayViews&) = default;
+  ~ArrayOfArrayViews() = default;
+
+  const_iterator begin() const;
+  const_iterator end() const;
+  bool empty() const { return data_.empty(); }
+  size_t size() const { return size_; }
+  void CopyTo(rtc::ArrayView<uint8_t> destination, const_iterator first) const;
+
+  void Add(const uint8_t* data, size_t size) {
+    data_.emplace_back(data, size);
+    size_ += size;
+  }
+
+ private:
+  using Storage = absl::InlinedVector<rtc::ArrayView<const uint8_t>, 2>;
+
+  size_t size_ = 0;
+  Storage data_;
+};
+
+class ArrayOfArrayViews::const_iterator {
+ public:
+  const_iterator() = default;
+  const_iterator(ArrayOfArrayViews::Storage::const_iterator outer, size_t inner)
+      : outer_(outer), inner_(inner) {}
+  const_iterator(const const_iterator&) = default;
+  const_iterator& operator=(const const_iterator&) = default;
+
+  const_iterator& operator++() {
+    if (++inner_ == outer_->size()) {
+      ++outer_;
+      inner_ = 0;
+    }
+    return *this;
+  }
+  uint8_t operator*() const { return (*outer_)[inner_]; }
+
+  friend bool operator==(const const_iterator& lhs, const const_iterator& rhs) {
+    return lhs.outer_ == rhs.outer_ && lhs.inner_ == rhs.inner_;
+  }
+
+ private:
+  friend ArrayOfArrayViews;
+  Storage::const_iterator outer_;
+  size_t inner_;
+};
+
+ArrayOfArrayViews::const_iterator ArrayOfArrayViews::begin() const {
+  return const_iterator(data_.begin(), 0);
+}
+
+ArrayOfArrayViews::const_iterator ArrayOfArrayViews::end() const {
+  return const_iterator(data_.end(), 0);
+}
+
+void ArrayOfArrayViews::CopyTo(rtc::ArrayView<uint8_t> destination,
+                               const_iterator first) const {
+  if (first == end()) {
+    // Empty payload. That is valid, e.g. Temporal Delimiters are always empty.
+    RTC_DCHECK_EQ(destination.size(), 0);
+    return;
+  }
+  uint8_t* to = destination.data();
+  uint8_t* const to_end = destination.data() + destination.size();
+  size_t first_chunk_size = first.outer_->size() - first.inner_;
+  RTC_DCHECK_LE(first_chunk_size, to_end - to);
+  memcpy(to, first.outer_->data() + first.inner_, first_chunk_size);
+  to += first_chunk_size;
+  for (auto it = std::next(first.outer_); it != data_.end(); ++it) {
+    RTC_DCHECK_LE(it->size(), to_end - to);
+    memcpy(to, it->data(), it->size());
+    to += it->size();
+  }
+  RTC_DCHECK_EQ(to_end - to, 0);
+}
+
+struct ObuInfo {
+  // Size of the obu_header and obu_size fields in the ouput frame.
+  size_t prefix_size = 0;
+  // Size of the obu payload in the output frame, i.e. excluding header
+  size_t payload_size = 0;
+  // leb128 encoded payload_size.
+  uint8_t encoded_payload_size[8];
+  // iterator pointing to the beginning of the obu payload.
+  ArrayOfArrayViews::const_iterator payload_offset;
+  // OBU payloads as written in the rtp packet payloads.
+  ArrayOfArrayViews data;
+};
+// Expect that majority of the frame won't use more than 4 obus.
+// In a simple stream delta frame consist of single Frame OBU, while key frame
+// also has Sequence Header OBU.
+using VectorObuInfo = absl::InlinedVector<ObuInfo, 4>;
+
 constexpr int kObuTypeSequenceHeader = 1;
+constexpr uint8_t kObuHasSizeBit = 0b0'0000'010;
+
+bool ObuHasExtension(uint8_t obu_header) {
+  return obu_header & 0b0'0000'100u;
+}
+
+bool ObuHasSize(uint8_t obu_header) {
+  return obu_header & kObuHasSizeBit;
+}
 
 int ObuType(uint8_t obu_header) {
   return (obu_header & 0b0'1111'000u) >> 3;
@@ -74,7 +182,190 @@ int RtpNumObus(uint8_t aggregation_header) {  // 0 for any number of obus.
   return (aggregation_header & 0b0011'0000u) >> 4;
 }
 
+// Reorgonizes array of rtp payloads into array of obus:
+// fills ObuInfo::data field.
+VectorObuInfo ParseObus(
+    rtc::ArrayView<const rtc::ArrayView<const uint8_t>> rtp_payloads) {
+  VectorObuInfo obus;
+  bool expect_continues_obu = false;
+  for (rtc::ArrayView<const uint8_t> rtp_payload : rtp_payloads) {
+    rtc::ByteBufferReader payload(
+        reinterpret_cast<const char*>(rtp_payload.data()), rtp_payload.size());
+    uint8_t aggregation_header;
+    if (!payload.ReadUInt8(&aggregation_header)) {
+      RTC_DLOG(WARNING) << "Failed to find aggregation header in the packet";
+      return {};
+    }
+    // Z-bit: 1 if the first OBU contained in the packet is a continuation of a
+    // previous OBU.
+    bool continues_obu = RtpStartsWithFragment(aggregation_header);
+    if (continues_obu != expect_continues_obu) {
+      RTC_DLOG(WARNING) << "Unexpected Z-bit " << continues_obu;
+      return {};
+    }
+    if (!continues_obu) {
+      obus.emplace_back();
+    }
+    int num_expected_obus = RtpNumObus(aggregation_header);
+    for (int obu_index = 1; payload.Length() > 0; ++obu_index) {
+      ObuInfo& obu = obu_index == 1 ? obus.back() : obus.emplace_back();
+      uint64_t fragment_size;
+      // When num_expected_obus > 0, last OBU (fragment) is not preceeded by
+      // the size field. See W field in
+      // https://aomediacodec.github.io/av1-rtp-spec/#43-av1-aggregation-header
+      bool has_fragment_size = (obu_index != num_expected_obus);
+      if (has_fragment_size) {
+        if (!payload.ReadUVarint(&fragment_size)) {
+          RTC_DLOG(WARNING) << "Failed to reader fragment size.";
+          return {};
+        }
+        if (fragment_size > payload.Length()) {
+          // Malformed input: written size is larger than remaining buffer.
+          RTC_DLOG(WARNING) << "Malformed fragment size " << fragment_size
+                            << " is larger than remaining size "
+                            << payload.Length() << " while reading obu #"
+                            << obu_index << "/" << num_expected_obus;
+          return {};
+        }
+      } else {
+        fragment_size = payload.Length();
+      }
+      // While it is in-practical to pass empty fragments, it is still possible.
+      if (fragment_size > 0) {
+        obu.data.Add(reinterpret_cast<const uint8_t*>(payload.Data()),
+                     fragment_size);
+        payload.Consume(fragment_size);
+      }
+    }
+    // Z flag should be same as Y flag of the next packet.
+    expect_continues_obu = RtpEndsWithFragment(aggregation_header);
+  }
+  if (expect_continues_obu) {
+    RTC_DLOG(WARNING) << "Last packet shouldn't have last obu fragmented.";
+    return {};
+  }
+  return obus;
+}
+
+// Returns number of bytes consumed.
+int WriteLeb128(size_t value, uint8_t* buffer) {
+  RTC_DCHECK_LT(value, size_t{1} << 32);
+  int size = 0;
+  while (value >= 0x80) {
+    buffer[size] = 0x80 | (value & 0x7F);
+    ++size;
+    value >>= 7;
+  }
+  buffer[size] = value;
+  ++size;
+  return size;
+}
+
+// Calculates sizes for the Obu:
+//  Changes payload_size from the size of all fragments into the size of obu
+//  payload. Extracts and validates obu size if present. Finds offset for the
+//  obu payload. Calculates total_size obu would occupy in the frame.
+// Returns false if obu found to be misformed.
+bool CalculateObuSizes(ObuInfo* obu) {
+  if (obu->data.empty()) {
+    RTC_DLOG(WARNING) << "Invalid bitstream: empty obu provided.";
+    return false;
+  }
+  auto it = obu->data.begin();
+  uint8_t obu_header = *it;
+  ++it;
+  if (!ObuHasExtension(obu_header)) {
+    obu->prefix_size = 1;
+  } else {
+    obu->prefix_size = 2;
+    if (it == obu->data.end()) {
+      return false;
+    }
+    ++it;
+  }
+  // Read, validate, and skip size, if present.
+  if (!ObuHasSize(obu_header)) {
+    obu->payload_size = obu->data.size() - obu->prefix_size;
+  } else {
+    uint64_t signaled_size = 0;
+    int size_size = 0;
+    uint8_t byte;
+    do {
+      if (it == obu->data.end() || size_size >= 8) {
+        RTC_DLOG(WARNING) << "Failed to read obu_size. Size field is too long: "
+                          << size_size << " bytes processed.";
+        return false;
+      }
+      byte = *it;
+      signaled_size |= (byte & 0x7F) << (size_size * 7);
+      ++size_size;
+      ++it;
+    } while ((byte & 0x80) != 0);
+
+    obu->payload_size = obu->data.size() - obu->prefix_size - size_size;
+    if (signaled_size != obu->payload_size) {
+      // obu_size was present in the bitstream and mismatches calculated size.
+      RTC_DLOG(WARNING) << "Mismatch in obu_size. signaled: " << signaled_size
+                        << ", actual: " << obu->payload_size;
+      return false;
+    }
+  }
+  // At this point obu->data should never change since, so it should be ok
+  // to store iterator into it.
+  obu->payload_offset = it;
+  obu->prefix_size += WriteLeb128(obu->payload_size, obu->encoded_payload_size);
+  return true;
+}
+
+// Writes obu into raw buffer.
+void StoreObu(const ObuInfo& obu, uint8_t* write_at) {
+  const uint8_t* const end_write_at =
+      write_at + obu.prefix_size + obu.payload_size;
+  auto it = obu.data.begin();
+  uint8_t obu_header = *it;
+  *write_at = obu_header | kObuHasSizeBit;
+  ++write_at;
+  int size_size = obu.prefix_size - 1;
+  if (ObuHasExtension(obu_header)) {
+    *write_at = *++it;  // extension_header
+    ++write_at;
+    --size_size;
+  }
+  // Copy the obu_size field.
+  memcpy(write_at, obu.encoded_payload_size, size_size);
+  write_at += size_size;
+  // Copy the payload.
+  obu.data.CopyTo(rtc::MakeArrayView(write_at, end_write_at - write_at),
+                  obu.payload_offset);
+}
+
 }  // namespace
+
+rtc::scoped_refptr<EncodedImageBuffer> RtpDepacketizerAv1::AssembleFrame(
+    rtc::ArrayView<const rtc::ArrayView<const uint8_t>> rtp_payloads) {
+  VectorObuInfo obus = ParseObus(rtp_payloads);
+  if (obus.empty()) {
+    return nullptr;
+  }
+
+  size_t frame_size = 0;
+  for (ObuInfo& obu : obus) {
+    if (!CalculateObuSizes(&obu)) {
+      return nullptr;
+    }
+    frame_size += (obu.prefix_size + obu.payload_size);
+  }
+
+  rtc::scoped_refptr<EncodedImageBuffer> bitstream =
+      EncodedImageBuffer::Create(frame_size);
+  uint8_t* write_at = bitstream->data();
+  for (const ObuInfo& obu : obus) {
+    StoreObu(obu, write_at);
+    write_at += (obu.prefix_size + obu.payload_size);
+  }
+  RTC_CHECK_EQ(write_at - bitstream->data(), bitstream->size());
+  return bitstream;
+}
 
 bool RtpDepacketizerAv1::Parse(ParsedPayload* parsed_payload,
                                const uint8_t* payload_data,
