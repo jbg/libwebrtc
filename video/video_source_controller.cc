e396dd156fe1df555a3c97125348aeef9a0f0e8d
Henrik Boström <hbos@webrtc.org>
2019-12-19 15:32:30 +0100

    Make VideoStreamEncoder responsible for reconfiguring its source/sink.
    Prior to this CL the VideoSourceProxy (residing inside
    OveruseFrameDetectorResourceAdaptationModule) was responsible for
    performing AddOrUpdateSink(). This could happen for various reasons:
    - AdaptUp() or AdaptDown() modified the "sink wants", informing the
      VideoStreamEncoder of changes to VideoSourceRestrictions, which then
      asked the module to reconfigure the VideoSourceProxy.
    - The VideoStreamEncoder asked to reconfigure the source for reasons
      other than restrictions changing, such as
      SetMaxFramerateAndAlignment() and SetWantsRotationApplied(). These
      requests were plumbed through the adaptation module even though they
      were unrelated to resource adaptation.
    
    (WIP, TODO):
    
    After this CL, VideoSourceController has replaced VideoSourceProxy. The
    controller is owned by the VideoStreamEncoder which is now directly
    responsible for any reconfiguration of the video source. This means
    that the OveruseFrameDetectorResourceAdaptationModule is no longer
    aware of any settings other than VideoSourceRestrictions.
    
    Bug: webrtc:11222
    Change-Id: I0f058c4700ca108e2d9f212e38b61f6f728aa419

---
+++ video/video_source_controller.cc
@@ -0,0 +1,161 @@
+/*
+ *  Copyright 2019 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "video/video_source_controller.h"
+
+#include <algorithm>
+#include <limits>
+#include <utility>
+
+#include "absl/types/optional.h"
+#include "rtc_base/numerics/safe_conversions.h"
+
+namespace webrtc {
+
+namespace {
+
+int OptionalSizeTToInt(const absl::optional<size_t>& optional) {
+  return optional.has_value() ? static_cast<int>(optional.value())
+                              : std::numeric_limits<int>::max();
+}
+
+absl::optional<int> OptionalSizeTToOptionalInt(
+    const absl::optional<size_t>& optional) {
+  return optional.has_value()
+             ? absl::optional<int>(
+                   rtc::dchecked_cast<int, size_t>(optional.value()))
+             : absl::nullopt;
+}
+
+int OptionalDoubleToInt(const absl::optional<double>& optional) {
+  return optional.has_value() ? static_cast<int>(optional.value())
+                              : std::numeric_limits<int>::max();
+}
+
+}  // namespace
+
+VideoSourceController::VideoSourceController(
+    rtc::VideoSinkInterface<VideoFrame>* sink,
+    rtc::VideoSourceInterface<VideoFrame>* source)
+    : sink_(sink), source_(source) {}
+
+void VideoSourceController::SetSource(
+    rtc::VideoSourceInterface<VideoFrame>* source) {
+  rtc::VideoSourceInterface<VideoFrame>* old_source;
+  rtc::VideoSinkWants wants;
+  {
+    rtc::CritScope lock(&crit_);
+    old_source = source_;
+    source_ = source;
+    wants = CurrentSettingsToSinkWants();
+  }
+  if (old_source != source && old_source)
+    old_source->RemoveSink(sink_);
+  if (!source)
+    return;
+  source->AddOrUpdateSink(sink_, wants);
+}
+
+void VideoSourceController::PushSourceSinkSettings() {
+  rtc::VideoSourceInterface<VideoFrame>* source;
+  rtc::VideoSinkWants wants;
+  {
+    rtc::CritScope lock(&crit_);
+    source = source_;
+    wants = CurrentSettingsToSinkWants();
+  }
+  if (!source)
+    return;
+  source->AddOrUpdateSink(sink_, wants);
+}
+
+VideoSourceRestrictions VideoSourceController::restrictions() const {
+  rtc::CritScope lock(&crit_);
+  return restrictions_;
+}
+
+absl::optional<size_t> VideoSourceController::pixels_per_frame_upper_limit()
+    const {
+  rtc::CritScope lock(&crit_);
+  return pixels_per_frame_upper_limit_;
+}
+
+absl::optional<double> VideoSourceController::frame_rate_upper_limit() const {
+  rtc::CritScope lock(&crit_);
+  return frame_rate_upper_limit_;
+}
+
+bool VideoSourceController::rotation_applied() const {
+  rtc::CritScope lock(&crit_);
+  return rotation_applied_;
+}
+
+int VideoSourceController::resolution_alignment() const {
+  rtc::CritScope lock(&crit_);
+  return resolution_alignment_;
+}
+
+void VideoSourceController::SetRestrictions(
+    VideoSourceRestrictions restrictions) {
+  rtc::CritScope lock(&crit_);
+  restrictions_ = std::move(restrictions);
+}
+
+void VideoSourceController::SetPixelsPerFrameUpperLimit(
+    absl::optional<size_t> pixels_per_frame_upper_limit) {
+  rtc::CritScope lock(&crit_);
+  pixels_per_frame_upper_limit_ = std::move(pixels_per_frame_upper_limit);
+}
+
+void VideoSourceController::SetFrameRateUpperLimit(
+    absl::optional<double> frame_rate_upper_limit) {
+  rtc::CritScope lock(&crit_);
+  frame_rate_upper_limit_ = std::move(frame_rate_upper_limit);
+}
+
+void VideoSourceController::SetRotationApplied(bool rotation_applied) {
+  rtc::CritScope lock(&crit_);
+  rotation_applied_ = rotation_applied;
+}
+
+void VideoSourceController::SetResolutionAlignment(int resolution_alignment) {
+  rtc::CritScope lock(&crit_);
+  resolution_alignment_ = resolution_alignment;
+}
+
+rtc::VideoSinkWants
+VideoSourceController::CurrentSettingsToSinkWantsForTesting() const {
+  rtc::CritScope lock(&crit_);
+  return CurrentSettingsToSinkWants();
+}
+
+rtc::VideoSinkWants VideoSourceController::CurrentSettingsToSinkWants() const {
+  rtc::VideoSinkWants wants;
+  wants.rotation_applied = rotation_applied_;
+  // |wants.black_frames| is not used, it always has its default value false.
+  wants.max_pixel_count =
+      OptionalSizeTToInt(restrictions_.max_pixels_per_frame());
+  if (pixels_per_frame_upper_limit_.has_value()) {
+    wants.max_pixel_count =
+        std::min(wants.max_pixel_count,
+                 OptionalSizeTToInt(pixels_per_frame_upper_limit_));
+  }
+  wants.target_pixel_count =
+      OptionalSizeTToOptionalInt(restrictions_.target_pixels_per_frame());
+  wants.max_framerate_fps = OptionalDoubleToInt(restrictions_.max_frame_rate());
+  if (frame_rate_upper_limit_.has_value()) {
+    wants.max_framerate_fps = std::min(
+        wants.max_framerate_fps, OptionalDoubleToInt(frame_rate_upper_limit_));
+  }
+  wants.resolution_alignment = resolution_alignment_;
+  return wants;
+}
+
+}  // namespace webrtc
