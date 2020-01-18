81bbceca555f6a5f0b0e4527f76a3b50f96e6802
Augusto Righetto <aurighet@microsoft.com>
2020-01-17 16:57:46 -0800

    Video Capture using WinRT's Windows::Media APIs
    This CL adds a new video capture module using WinRT's Windows::Media APIs. The legacy video capture module using DirectShow continues existing as default implementation because of the Windows 7 support (https://cloud.google.com/blog/products/chrome-enterprise/how-chrome-is-helping-enterprises-still-using-windows-7). DirectShow is a legacy Windows API that is no longer supported. Besides that, modern Windows Apps can't consume DirectShow APIs.
    This new video capture module can be used by Desktop apps and Modern Windows Apps (UWP) as drop-in replacement for the DShow video capture module on platforms supporting WinRT APIs.
    The design for this new module follows the same patterns used by the DirectShow video capture module.
    
    Here are some design decisions:
    1. To keep compatibility with current consumers of VideoCaptureFactory, GPU accelerated capabilities were not used by this CL. This might be addressed in a future CL.
    2. To have minimal impact on build times, WinRT headers do not mix up with WebRTC headers.
    3. Only color cameras are enumerated.
    
    Noticeable differences are:
    1. UWPs don't have the capabilities needed to create an experience like what DisplayCaptureSettingsDialogBox offers on desktop apps. Following the Linux video capture module, calling DisplayCaptureSettingsDialogBox returns an error.
    2. Camera enumeration do not filter out Video for Windows drivers or "Google Camera Adapter".
    3. Some older cameras with custom camera drivers will not register under the KSCATEGORY_VIDEO_CAMERA. So, it's possible to get more cameras via the DShow enumeration than the DeviceInformation enumeration. Similarly, some cameras which cannot work through the DShow code path (such as network cameras, custom media source cameras), will not register under the old DShow category.
    
    Bug: webrtc:11304
    Change-Id: If44c60b6d416d1ef547a09cf94d80619a786d9b4

---
+++ modules/video_capture/windows/help_functions_winrt.h
@@ -0,0 +1,108 @@
+/*
+ *  Copyright (c) 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef MODULES_VIDEO_CAPTURE_WINDOWS_HELP_FUNCTIONS_WINRT_H_
+#define MODULES_VIDEO_CAPTURE_WINDOWS_HELP_FUNCTIONS_WINRT_H_
+
+#include <windows.foundation.h>
+#include <windows.media.mediaproperties.h>
+#include <wrl/client.h>
+#include <wrl/event.h>
+#include <wrl/wrappers/corewrappers.h>
+
+#include <cassert>
+
+#include "modules/video_capture/video_capture_defines.h"
+
+// Evaluates an expression returning a HRESULT.
+// It jumps to a label named Cleanup on failure.
+#define THR(expr)   \
+  do {              \
+    hr = (expr);    \
+    if (FAILED(hr)) \
+      goto Cleanup; \
+  } while (false)
+
+namespace webrtc {
+namespace videocapturemodule {
+
+uint32_t SafelyComputeMediaRatio(
+    ABI::Windows::Media::MediaProperties::IMediaRatio* ratio_no_ref);
+
+VideoType ToVideoType(const Microsoft::WRL::Wrappers::HString& sub_type);
+
+HRESULT WaitForAsyncAction(
+    Microsoft::WRL::ComPtr<ABI::Windows::Foundation::IAsyncAction>
+        async_action);
+
+template <typename T>
+HRESULT WaitForAsyncOperation(
+    Microsoft::WRL::ComPtr<ABI::Windows::Foundation::IAsyncOperation<T>>&
+        async_op) {
+  HRESULT hr, hr_async_error;
+  const DWORD timeout_ms = 2000;
+  Microsoft::WRL::ComPtr<ABI::Windows::Foundation::IAsyncInfo> async_info;
+  Microsoft::WRL::Wrappers::Event event_completed;
+
+#if _DEBUG
+  APTTYPE apt_type;
+  APTTYPEQUALIFIER apt_qualifier;
+  THR(CoGetApartmentType(&apt_type, &apt_qualifier));
+  // Please make the caller of this API run on a MTA appartment type.
+  // The caller shouldn't be running on the UI thread (STA).
+  assert(apt_type == APTTYPE_MTA);
+#endif  // _DEBUG
+
+  // Creates the Event to be used to block and suspend until the async
+  // operation finishes.
+  event_completed = Microsoft::WRL::Wrappers::Event(
+      ::CreateEventEx(nullptr, nullptr, CREATE_EVENT_MANUAL_RESET,
+                      WRITE_OWNER | EVENT_ALL_ACCESS));
+  THR(event_completed.IsValid() ? S_OK : E_HANDLE);
+
+  // Defines the callback that will signal the event to unblock and resume.
+  THR(async_op->put_Completed(
+      Microsoft::WRL::Callback<
+          ABI::Windows::Foundation::IAsyncOperationCompletedHandler<T>>(
+          [&event_completed](
+              ABI::Windows::Foundation::IAsyncOperation<T>*,
+              ABI::Windows::Foundation::AsyncStatus async_status) -> HRESULT {
+            HRESULT hr;
+
+            THR(async_status == ABI::Windows::Foundation::AsyncStatus::Completed
+                    ? S_OK
+                    : E_ABORT);
+
+          Cleanup:
+            ::SetEvent(event_completed.Get());
+
+            return hr;
+          })
+          .Get()));
+
+  // Block and suspend thread until the async operation finishes or timeout.
+  THR(::WaitForSingleObjectEx(event_completed.Get(), timeout_ms, FALSE) ==
+              WAIT_OBJECT_0
+          ? S_OK
+          : E_FAIL);
+
+  // Checks if async operation completed successfully.
+  THR(async_op.template As<ABI::Windows::Foundation::IAsyncInfo>(&async_info));
+  THR(async_info->get_ErrorCode(&hr_async_error));
+  THR(hr_async_error);
+
+Cleanup:
+  return hr;
+}
+
+}  // namespace videocapturemodule
+}  // namespace webrtc
+
+#endif  // MODULES_VIDEO_CAPTURE_WINDOWS_HELP_FUNCTIONS_WINRT_H_
