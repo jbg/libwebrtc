335219075ca2b48b339e703fc1e31a54e200183f
Augusto Righetto <aurighet@microsoft.com>
2020-01-17 16:57:46 -0800

    Video Capture using WinRT APIs
    This CL adds a new video capture module using WinRT APIs. The legacy video capture module using DirectShow continues existing as default implementation because of Windows 7 support (https://cloud.google.com/blog/products/chrome-enterprise/how-chrome-is-helping-enterprises-still-using-windows-7). DirectShow is a legacy Windows API that is no longer supported. Besides that, UWP apps can't consume DirectShow APIs.
    This new video capture module can be used by UWP apps and Desktop apps as drop-in replacement for the DShow video capture module on platforms supporting WinRT APIs.
    A new build option (rtc_win_video_capture_api) has been added to choose which API to use. The current options are "dshow", the default option, and "winrt".
    The design for this new module follows the pattern used by the DirectShow video capture module.
    
    Here are some design decisions:
    1. To keep compatibility with current consumers of VideoCaptureFactory, GPU accelerated capabilities were not used by this CL. This might be addressed in a future CL.
    2. To have minimal impact on build times, WinRT headers do not mix up with WebRTC headers.
    3. Only color cameras are enumerated.
    
    Noticeable differences are:
    1. UWPs don't have the capabilities needed to create an experience like what DisplayCaptureSettingsDialogBox offers on desktop apps. Following the Linux video capture module, calling DisplayCaptureSettingsDialogBox returns an error.
    2. Camera enumeration do not filter out Video for Windows drivers or "Google Camera Adapter".
    3. Some older cameras with custom camera drivers will not register under the KSCATEGORY_VIDEO_CAMERA. So, it's possible to get more cameras via the DShow enumeration than the DeviceInformation enumeration. Similarly, some cameras which cannot work through the DShow code path (such as network cameras, custom media source cameras), will not register under the old DShow category.
    
    /std:c++17 and /EHsc must be added to build\config\win\BUILD.gn to build this CL. A CL to the build repo for setting the flags for builds consuming WinRT APIs is coming soon.
    
    Issue: webrtc:11304
    Change-Id: If44c60b6d416d1ef547a09cf94d80619a786d9b4

---
+++ modules/video_capture/windows/device_info_winrt.h
@@ -0,0 +1,54 @@
+/*
+ *  Copyright (c) 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef MODULES_VIDEO_CAPTURE_MAIN_SOURCE_WINDOWS_DEVICE_INFO_WINRT_H_
+#define MODULES_VIDEO_CAPTURE_MAIN_SOURCE_WINDOWS_DEVICE_INFO_WINRT_H_
+
+#include "modules/video_capture/device_info_impl.h"
+
+namespace webrtc {
+namespace videocapturemodule {
+
+class DeviceInfoWinRT : public DeviceInfoImpl {
+ public:
+  // Factory function.
+  static DeviceInfoWinRT* Create();
+
+  DeviceInfoWinRT() = default;
+  ~DeviceInfoWinRT() override = default;
+
+  int32_t Init() override;
+  uint32_t NumberOfDevices() override;
+
+  /*
+   * Returns the available capture devices.
+   */
+  int32_t GetDeviceName(uint32_t deviceNumber,
+                        char* deviceNameUTF8,
+                        uint32_t deviceNameLength,
+                        char* deviceUniqueIdUTF8,
+                        uint32_t deviceUniqueIdUTF8Length,
+                        char* productUniqueIdUTF8,
+                        uint32_t productUniqueIdUTF8Length) override;
+
+  /*
+   * Display OS /capture device specific settings dialog
+   */
+  int32_t DisplayCaptureSettingsDialogBox(const char* deviceUniqueIdUTF8,
+                                          const char* dialogTitleUTF8,
+                                          void* parentWindow,
+                                          uint32_t positionX,
+                                          uint32_t positionY) override;
+
+  int32_t CreateCapabilityMap(const char* deviceUniqueIdUTF8) override;
+};
+}  // namespace videocapturemodule
+}  // namespace webrtc
+#endif  // MODULES_VIDEO_CAPTURE_MAIN_SOURCE_WINDOWS_DEVICE_INFO_WINRT_H_
