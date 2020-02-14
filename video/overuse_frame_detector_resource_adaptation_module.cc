ba8447ea1f9db5a65337b46a9cc0ea73a49a31fd
Evan Shrubsole <eshr@google.com>
2020-02-14 14:30:09 +0100

    Configure quality scaling in overuse module on all encoder updates
    This shouldn't change the behaviour as the scaler was already updated
    on all encoder reconfiguration. The only movement is the setting of the
    degeredation preference, where the quality scaler will be set as well.
    
    Bug: webrtc:11338
    Change-Id: I085afa7cfc8e17306439f03545a54d21b35dd60b

--- video/overuse_frame_detector_resource_adaptation_module.cc
+++ video/overuse_frame_detector_resource_adaptation_module.cc
@@ -478,6 +478,7 @@ void OveruseFrameDetectorResourceAdaptationModule::SetEncoderSettings(
       LastInputFrameSizeOrDefault(),
       encoder_settings_->video_codec().maxBitrate);
   MaybeUpdateTargetFrameRate();
+  ConfigureQualityScaler();
 }
 
 void OveruseFrameDetectorResourceAdaptationModule::SetStartBitrate(
@@ -579,8 +580,9 @@ void OveruseFrameDetectorResourceAdaptationModule::UpdateQualityScalerSettings(
   initial_frame_dropper_->OnQualityScalerSettingsUpdated();
 }
 
-void OveruseFrameDetectorResourceAdaptationModule::ConfigureQualityScaler(
-    const VideoEncoder::EncoderInfo& encoder_info) {
+void OveruseFrameDetectorResourceAdaptationModule::ConfigureQualityScaler() {
+  const VideoEncoder::EncoderInfo& encoder_info =
+      encoder_settings_->encoder_info();
   const auto scaling_settings = encoder_info.scaling_settings;
   const bool quality_scaling_allowed =
       IsResolutionScalingEnabled(degradation_preference_) &&
