9fb8edd834dbea4f31875026d59fae8f96ad028f
Evan Shrubsole <eshr@google.com>
2020-02-26 13:11:27 +0100

    QualityScalerResource configures itself
    Bug: webrtc:11377
    Change-Id: Iced0664770e1861a20773297f0561796ad69e3ea

--- video/quality_scaler_resource.cc
+++ video/quality_scaler_resource.cc
@@ -12,29 +12,74 @@
 
 #include <utility>
 
+#include "api/rtp_parameters.h"
+#include "api/video/video_stream_encoder_observer.h"
+
 namespace webrtc {
 
-QualityScalerResource::QualityScalerResource() : quality_scaler_(nullptr) {}
+namespace {
+
+bool IsResolutionScalingEnabled(DegradationPreference degradation_preference) {
+  return degradation_preference == DegradationPreference::MAINTAIN_FRAMERATE ||
+         degradation_preference == DegradationPreference::BALANCED;
+}
+
+}  // namespace
+
+QualityScalerResource::QualityScalerResource()
+    : quality_scaler_(nullptr),
+      quality_scaling_experiment_enabled_(QualityScalingExperiment::Enabled()) {
+}
+
+QualityScalerResource::~QualityScalerResource() {
+  RTC_DCHECK(!is_started());
+}
 
 bool QualityScalerResource::is_started() const {
   return quality_scaler_.get();
 }
 
-void QualityScalerResource::StartCheckForOveruse(
-    VideoEncoder::QpThresholds qp_thresholds) {
-  RTC_DCHECK(!is_started());
-  quality_scaler_ =
-      std::make_unique<QualityScaler>(this, std::move(qp_thresholds));
-}
-
 void QualityScalerResource::StopCheckForOveruse() {
-  quality_scaler_.reset();
+  quality_scaler_ = nullptr;
 }
 
-void QualityScalerResource::SetQpThresholds(
-    VideoEncoder::QpThresholds qp_thresholds) {
-  RTC_DCHECK(is_started());
-  quality_scaler_->SetQpThresholds(std::move(qp_thresholds));
+void QualityScalerResource::Configure(
+    const VideoEncoder::EncoderInfo& encoder_info,
+    DegradationPreference degradation_preference,
+    VideoCodecType codec_type,
+    int pixels) {
+  const auto scaling_settings = encoder_info.scaling_settings;
+  const bool quality_scaling_allowed =
+      IsResolutionScalingEnabled(degradation_preference) &&
+      scaling_settings.thresholds;
+
+  if (quality_scaling_allowed) {
+    if (!quality_scaler_) {
+      // Use experimental thresholds if available.
+      absl::optional<VideoEncoder::QpThresholds> experimental_thresholds;
+      if (quality_scaling_experiment_enabled_) {
+        experimental_thresholds =
+            QualityScalingExperiment::GetQpThresholds(codec_type);
+      }
+      VideoEncoder::QpThresholds qp_thresholds =
+          experimental_thresholds ? *experimental_thresholds
+                                  : *(scaling_settings.thresholds);
+      quality_scaler_ = std::make_unique<QualityScaler>(this, qp_thresholds);
+    }
+  } else {
+    quality_scaler_.reset();
+  }
+
+  // Set the qp-thresholds to the balanced settings if balanced mode.
+  if (degradation_preference == DegradationPreference::BALANCED &&
+      quality_scaler_) {
+    absl::optional<VideoEncoder::QpThresholds> thresholds =
+        balanced_settings_.GetQpThresholds(codec_type, pixels);
+    if (thresholds) {
+      VideoEncoder::QpThresholds qp_thresholds = *thresholds;
+      quality_scaler_->SetQpThresholds(qp_thresholds);
+    }
+  }
 }
 
 bool QualityScalerResource::QpFastFilterLow() {
@@ -47,7 +92,6 @@ void QualityScalerResource::OnEncodeCompleted(const EncodedImage& encoded_image,
   if (quality_scaler_ && encoded_image.qp_ >= 0)
     quality_scaler_->ReportQp(encoded_image.qp_, time_sent_in_us);
 }
-
 void QualityScalerResource::OnFrameDropped(
     EncodedImageCallback::DropReason reason) {
   if (!quality_scaler_)
@@ -61,12 +105,10 @@ void QualityScalerResource::OnFrameDropped(
       break;
   }
 }
-
 void QualityScalerResource::AdaptUp(AdaptReason reason) {
   RTC_DCHECK_EQ(reason, AdaptReason::kQuality);
   OnResourceUsageStateMeasured(ResourceUsageState::kUnderuse);
 }
-
 bool QualityScalerResource::AdaptDown(AdaptReason reason) {
   RTC_DCHECK_EQ(reason, AdaptReason::kQuality);
   return OnResourceUsageStateMeasured(ResourceUsageState::kOveruse) !=
