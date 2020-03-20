730b50a3e87f492a269643bac3b47b2ba0cd3ccd
Sam Zackrisson <saza@webrtc.org>
2020-03-20 11:09:22 +0100

    Add empty placeholder target for rms_level.*
    A follow-up CL will move the rms_level.* files into the new target.
    
    Bug: webrtc:11226
    Change-Id: I59579b026346f627c0a2739d25f90c12bffbf248

---
+++ modules/audio_processing/dummy.h
@@ -0,0 +1,13 @@
+/*
+ *  Copyright (c) 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef MODULES_AUDIO_PROCESSING_DUMMY_H_
+#define MODULES_AUDIO_PROCESSING_DUMMY_H_
+#endif  // MODULES_AUDIO_PROCESSING_DUMMY_H_
