b9c3eaec3140fe46ed021a48576b418a79763951
Artem Titov <titovartem@google.com>
2020-03-24 12:40:29 +0100

    Move calculation of target_encode_bitrate to DefaultVideoQualityAnalyzer
    To migrate on new GetStats API and properly support target encode bitrate
    for regular, simulcast and svc cases we need to calculate it inside video
    quality analyzer getting values from SetRates in VideoEncoder.
    
    Bug: webrtc:11381
    Change-Id: Ia37acac764ed3c30f64cdbfda8906d543fa03ae2

--- test/pc/e2e/analyzer/video/default_video_quality_analyzer.cc
+++ test/pc/e2e/analyzer/video/default_video_quality_analyzer.cc
@@ -26,6 +26,7 @@ namespace {
 constexpr int kMaxActiveComparisons = 10;
 constexpr int kFreezeThresholdMs = 150;
 constexpr int kMicrosPerSecond = 1000000;
+constexpr int kBitsInByte = 8;
 
 void LogFrameCounters(const std::string& name, const FrameCounters& counters) {
   RTC_LOG(INFO) << "[" << name << "] Captured    : " << counters.captured;
@@ -180,7 +181,8 @@ void DefaultVideoQualityAnalyzer::OnFramePreEncode(
 
 void DefaultVideoQualityAnalyzer::OnFrameEncoded(
     uint16_t frame_id,
-    const webrtc::EncodedImage& encoded_image) {
+    const webrtc::EncodedImage& encoded_image,
+    const EncoderStats& stats) {
   rtc::CritScope crit(&lock_);
   auto it = frame_stats_.find(frame_id);
   RTC_DCHECK(it != frame_stats_.end());
@@ -192,7 +194,12 @@ void DefaultVideoQualityAnalyzer::OnFrameEncoded(
     stream_frame_counters_[it->second.stream_label].encoded++;
   }
   it->second.encoded_time = Now();
+<<<<<<< HEAD
   it->second.encoded_image_size = encoded_image.size();
+||||||| constructed merge base
+=======
+  it->second.target_encode_bitrate = stats.target_encode_bitrate;
+>>>>>>> Calculate target_encode_bitrate in DVQA
 }
 
 void DefaultVideoQualityAnalyzer::OnFrameDropped(
@@ -226,8 +233,7 @@ void DefaultVideoQualityAnalyzer::OnFramePreDecode(
 
 void DefaultVideoQualityAnalyzer::OnFrameDecoded(
     const webrtc::VideoFrame& frame,
-    absl::optional<int32_t> decode_time_ms,
-    absl::optional<uint8_t> qp) {
+    const DecoderStats& stats) {
   rtc::CritScope crit(&lock_);
   auto it = frame_stats_.find(frame.id());
   RTC_DCHECK(it != frame_stats_.end());
@@ -517,6 +523,7 @@ void DefaultVideoQualityAnalyzer::ProcessComparison(
         (frame_stats.encoded_time - frame_stats.pre_encode_time).ms());
     stats->encode_frame_rate.AddEvent(frame_stats.encoded_time);
     stats->total_encoded_images_payload += frame_stats.encoded_image_size;
+    stats->target_encode_bitrate.AddSample(frame_stats.target_encode_bitrate);
   } else {
     if (frame_stats.pre_encode_time.IsFinite()) {
       stats->dropped_by_encoder++;
@@ -670,6 +677,9 @@ void DefaultVideoQualityAnalyzer::ReportResults(
                     /*important=*/false, ImproveDirection::kSmallerIsBetter);
   ReportResult("max_skipped", test_case_name, stats.skipped_between_rendered,
                "count", ImproveDirection::kSmallerIsBetter);
+  ReportResult("target_encode_bitrate", test_case_name,
+               stats.target_encode_bitrate / kBitsInByte, "bytesPerSecond",
+               ImproveDirection::kNone);
   test::PrintResult(
       "actual_encode_bitrate", "", test_case_name,
       static_cast<double>(stats.total_encoded_images_payload) /
