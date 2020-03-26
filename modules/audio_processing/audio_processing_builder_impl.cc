50ed3524f49544083b76991d1b55c00b19f942f2
Per Åhgren <peah@webrtc.org>
2020-03-26 14:07:37 +0100

    APM: Add build flag to allow building WebRTC without APM
    This CL adds a build flag to allow building the non-test parts
    of WebRTC without the audio processing module.
    The CL also ensures that the WebRTC code correctly handles
    the case when no APM is available.
    
    Bug: webrtc:5298
    Change-Id: I5c8b5d1f7115e5cce2af4c2b5ff701fa1c54e49e

---
+++ modules/audio_processing/audio_processing_builder_impl.cc
@@ -0,0 +1,83 @@
+/*
+ *  Copyright (c) 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "modules/audio_processing/include/audio_processing.h"
+
+#include <memory>
+
+#include "modules/audio_processing/audio_processing_impl.h"
+#include "rtc_base/ref_counted_object.h"
+
+namespace webrtc {
+
+AudioProcessingBuilder::AudioProcessingBuilder() = default;
+AudioProcessingBuilder::~AudioProcessingBuilder() = default;
+
+AudioProcessingBuilder& AudioProcessingBuilder::SetCapturePostProcessing(
+    std::unique_ptr<CustomProcessing> capture_post_processing) {
+  capture_post_processing_ = std::move(capture_post_processing);
+  return *this;
+}
+
+AudioProcessingBuilder& AudioProcessingBuilder::SetRenderPreProcessing(
+    std::unique_ptr<CustomProcessing> render_pre_processing) {
+  render_pre_processing_ = std::move(render_pre_processing);
+  return *this;
+}
+
+AudioProcessingBuilder& AudioProcessingBuilder::SetCaptureAnalyzer(
+    std::unique_ptr<CustomAudioAnalyzer> capture_analyzer) {
+  capture_analyzer_ = std::move(capture_analyzer);
+  return *this;
+}
+
+AudioProcessingBuilder& AudioProcessingBuilder::SetEchoControlFactory(
+    std::unique_ptr<EchoControlFactory> echo_control_factory) {
+  echo_control_factory_ = std::move(echo_control_factory);
+  return *this;
+}
+
+AudioProcessingBuilder& AudioProcessingBuilder::SetEchoDetector(
+    rtc::scoped_refptr<EchoDetector> echo_detector) {
+  echo_detector_ = std::move(echo_detector);
+  return *this;
+}
+
+AudioProcessing* AudioProcessingBuilder::Create() {
+  webrtc::Config config;
+  return Create(config);
+}
+
+#if defined(WEBRTC_EXCLUDE_AUDIO_PROCESSING_MODULE) && \
+    WEBRTC_EXCLUDE_AUDIO_PROCESSING_MODULE == 1
+
+// Implementation returning a null pointer for using when the APM is excluded
+// from the build..
+AudioProcessing* AudioProcessingBuilder::Create(const webrtc::Config& config) {
+  return nullptr;
+}
+
+#else
+
+// Standard implementation.
+AudioProcessing* AudioProcessingBuilder::Create(const webrtc::Config& config) {
+  AudioProcessingImpl* apm = new rtc::RefCountedObject<AudioProcessingImpl>(
+      config, std::move(capture_post_processing_),
+      std::move(render_pre_processing_), std::move(echo_control_factory_),
+      std::move(echo_detector_), std::move(capture_analyzer_));
+  if (apm->Initialize() != AudioProcessing::kNoError) {
+    delete apm;
+    apm = nullptr;
+  }
+  return apm;
+}
+#endif
+
+}  // namespace webrtc
