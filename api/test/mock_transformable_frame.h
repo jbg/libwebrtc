8f401642dfecca4a920c973de33e2cc990e3201a
Marina Ciocea <marinaciocea@webrtc.org>
2020-04-27 22:53:08 +0200

    Define MockTransformableFrame in api/test/.
    The mock is to be used in frame transformer unit tests.
    
    Bug: webrtc:11380
    Change-Id: Id3f6ec71712333232873d8de30e3c7392dc7f5e3

---
+++ api/test/mock_transformable_frame.h
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (c) 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef API_TEST_MOCK_TRANSFORMABLE_FRAME_H_
+#define API_TEST_MOCK_TRANSFORMABLE_FRAME_H_
+
+#include "api/frame_transformer_interface.h"
+#include "test/gmock.h"
+
+namespace webrtc {
+
+class MockTransformableFrame : public TransformableFrameInterface {
+ public:
+  MockTransformableFrame() = default;
+  ~MockTransformableFrame() override = default;
+
+  MOCK_METHOD(rtc::ArrayView<const uint8_t>, GetData, (), (const, override));
+  MOCK_METHOD(void, SetData, (rtc::ArrayView<const uint8_t>), (override));
+  MOCK_METHOD(uint32_t, GetTimestamp, (), (const, override));
+  MOCK_METHOD(uint32_t, GetSsrc, (), (const, override));
+};
+
+}  // namespace webrtc
+
+#endif  // API_TEST_MOCK_TRANSFORMABLE_FRAME_H_
