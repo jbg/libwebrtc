028acd09f06b0299936ec4b20458bf78a9aae921
Evan Shrubsole <eshr@google.com>
2020-05-07 11:16:46 +0200

    Add comparison methods for VideoSourceRestrictions
    Bug: None
    Change-Id: Ia67f39e9b17e37294462823dd6f6ca365c7fd46b

--- call/adaptation/video_source_restrictions_unittest.cc
+++ call/adaptation/video_source_restrictions_unittest.cc
@@ -16,6 +16,13 @@ namespace webrtc {
 
 namespace {
 
+const size_t kHdPixels = 1280 * 720;
+
+const VideoSourceRestrictions kUnlimited;
+const VideoSourceRestrictions k15fps(absl::nullopt, absl::nullopt, 15.0);
+const VideoSourceRestrictions kHd(kHdPixels, kHdPixels, absl::nullopt);
+const VideoSourceRestrictions kHd15fps(kHdPixels, kHdPixels, 15.0);
+
 VideoSourceRestrictions RestrictionsFromMaxPixelsPerFrame(
     size_t max_pixels_per_frame) {
   return VideoSourceRestrictions(max_pixels_per_frame, absl::nullopt,
@@ -68,6 +75,34 @@ TEST(VideoSourceRestrictionsTest, DidDecreaseFrameRate) {
   // larger restrictions -> samller restrictions
   EXPECT_TRUE(DidDecreaseFrameRate(RestrictionsFromMaxFrameRate(10),
                                    RestrictionsFromMaxFrameRate(9)));
+}
+
+TEST(VideoSourceRestrictionsTest, DidRestrictionsChangeFalseForSame) {
+  EXPECT_FALSE(DidRestrictionsDecrease(kUnlimited, kUnlimited));
+  EXPECT_FALSE(DidRestrictionsIncrease(kUnlimited, kUnlimited));
+}
+
+TEST(VideoSourceRestrictions,
+     DidRestrictionsIncreaseTrueWhenPixelsOrFrameRateDecreased) {
+  EXPECT_TRUE(DidRestrictionsIncrease(kUnlimited, kHd));
+  EXPECT_TRUE(DidRestrictionsIncrease(kUnlimited, k15fps));
+  EXPECT_TRUE(DidRestrictionsIncrease(kHd, kHd15fps));
+  EXPECT_TRUE(DidRestrictionsIncrease(kUnlimited, kHd15fps));
+}
+
+TEST(VideoSourceRestrictions,
+     DidRestrictionsDecreaseTrueWhenPixelsOrFrameRateIncreased) {
+  EXPECT_TRUE(DidRestrictionsDecrease(kHd, kUnlimited));
+  EXPECT_TRUE(DidRestrictionsDecrease(k15fps, kUnlimited));
+  EXPECT_TRUE(DidRestrictionsDecrease(kHd15fps, kHd));
+  EXPECT_TRUE(DidRestrictionsDecrease(kHd15fps, kUnlimited));
+}
+
+TEST(VideoSourceRestrictions,
+     DidRestrictionsChangeFalseWhenFrameRateAndPixelsChangeDifferently) {
+  // One changed framerate, the other resolution; not an increase or decrease.
+  EXPECT_FALSE(DidRestrictionsIncrease(kHd, k15fps));
+  EXPECT_FALSE(DidRestrictionsDecrease(kHd, k15fps));
 }
 
 }  // namespace webrtc
