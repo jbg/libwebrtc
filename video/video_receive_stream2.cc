2cf4017d819856998f3f71e4d36ec0d9a9b72894
Tommi <tommi@webrtc.org>
2020-05-08 21:41:02 +0200

    Call OnDecodedFrame asynchronously on the worker thread.
    This offloads the decoder thread with managing histograms,
    moves the management over to the thread on which they're queried.
    This will allow us to remove more locking from the decoder threads
    and avoid contention when querying for stats.
    
    Bug:webrtc:11489
    Change-Id: I563c90a0ed01e0b3598ee314d8118622216a2e0f

--- video/video_receive_stream2.cc
+++ video/video_receive_stream2.cc
@@ -696,8 +696,13 @@ void VideoReceiveStream2::HandleFrameBufferTimeout() {
   // To avoid spamming keyframe requests for a stream that is not active we
   // check if we have received a packet within the last 5 seconds.
   bool stream_is_active = last_packet_ms && now_ms - *last_packet_ms < 5000;
-  if (!stream_is_active)
-    stats_proxy_.OnStreamInactive();
+  if (!stream_is_active) {
+    worker_thread_->PostTask(ToQueuedTask([safety = task_safety_flag_, this]() {
+      if (!safety->alive())
+        return;
+      stats_proxy_.OnStreamInactive();
+    }));
+  }
 
   if (stream_is_active && !IsReceivingKeyFrame(now_ms) &&
       (!config_.crypto_options.sframe.require_frame_encryption ||
