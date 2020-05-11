de82c94644358d2d2c13a889f897c8e19567f9de
Danil Chapovalov <danilchap@webrtc.org>
2020-05-11 13:09:37 +0200

    Introduce layering controller interface for av1 encoder
    Add TODOs into AV1 encoder wrapper where it suppose to be used.
    
    Bug: webrtc:11404
    Change-Id: If049066b84be72829867d5084827a7d275648a7b

---
+++ modules/video_coding/codecs/av1/frame_dependencies_controller.h
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (c) 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#ifndef MODULES_VIDEO_CODING_CODECS_AV1_FRAME_DEPENDENCIES_CONTROLLER_H_
+#define MODULES_VIDEO_CODING_CODECS_AV1_FRAME_DEPENDENCIES_CONTROLLER_H_
+
+#include <vector>
+
+#include "api/transport/rtp/dependency_descriptor.h"
+#include "common_video/generic_frame_descriptor/generic_frame_info.h"
+
+namespace webrtc {
+
+class FrameDependenciesController {
+ public:
+  virtual ~FrameDependenciesController() = default;
+
+  virtual FrameDependencyStructure DependencyStructure() const = 0;
+  // Returns list of configurations the frame should be encoded with.
+  // Returns empty list to indicate frame should be dropped.
+  // Normally returns one configuration per active spatial layer.
+  // When `restart` is true, first GenericFrameInfo should have be a key frame.
+  virtual std::vector<GenericFrameInfo> NextFrameConfig(bool restart) = 0;
+};
+
+}  // namespace webrtc
+
+#endif  // MODULES_VIDEO_CODING_CODECS_AV1_FRAME_DEPENDENCIES_CONTROLLER_H_
