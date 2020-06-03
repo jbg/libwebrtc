3a3dea78fa4948f9ae959988a680adc7440f043e
Markus Handell <handellm@webrtc.org>
2020-06-03 10:16:26 +0200

    RtpTransceiverInterface: introduce SetOfferedRtpHeaderExtensions.
    This change adds exposure of a new transceiver method for
    modifying the extensions offered in the next SDP negotiation,
    following spec details in https://w3c.github.io/webrtc-extensions/#rtcrtptransceiver-interface.
    
    Features:
    - The interface allows to control the negotiated direction as
      per https://tools.ietf.org/html/rfc5285#page-7.
    - The interface allows to remove an extension from SDP
      negotiation by modifying the direction to
      RtpTransceiverDirection::kStopped.
    
    https://chromestatus.com/feature/5680189201711104.
    go/rtp-header-extension-ip
    Intent to prototype: https://groups.google.com/a/chromium.org/g/blink-dev/c/65YdUi02yZk
    
    Bug: chromium:1051821
    Change-Id: Iaabc34446f038c46d93c442e90c2a77f77d542d4

--- pc/rtp_transceiver.h
+++ pc/rtp_transceiver.h
@@ -195,6 +195,9 @@ class RtpTransceiver final
   }
   std::vector<RtpHeaderExtensionCapability> HeaderExtensionsToOffer()
       const override;
+  RTCError SetOfferedRtpHeaderExtensions(
+      rtc::ArrayView<RtpHeaderExtensionCapability> header_extensions_to_offer)
+      override;
 
  private:
   void OnFirstPacketReceived(cricket::ChannelInterface* channel);
@@ -220,7 +223,7 @@ class RtpTransceiver final
   cricket::ChannelInterface* channel_ = nullptr;
   cricket::ChannelManager* channel_manager_ = nullptr;
   std::vector<RtpCodecCapability> codec_preferences_;
-  std::vector<RtpHeaderExtensionCapability> HeaderExtensionsToOffer_;
+  std::vector<RtpHeaderExtensionCapability> header_extensions_to_offer_;
 };
 
 BEGIN_SIGNALING_PROXY_MAP(RtpTransceiver)
