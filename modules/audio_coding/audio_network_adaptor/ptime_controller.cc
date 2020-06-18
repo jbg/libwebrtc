6b2a7bfba3f9e5277c76ead3e5d18a8b6932c9d0
Jakob Ivarsson <jakobi@webrtc.org>
2020-06-18 08:30:22 +0200

    Add a simple PtimeController which makes a combined decision for frame length and payload bitrate.
    This will be used when adaptivePtime is enabled.
    
    Bug: chromium:1086942
    Change-Id: I63c947c53a8c5b8e0825b78b847c3f7900197d6c

---
+++ modules/audio_coding/audio_network_adaptor/ptime_controller.cc
@@ -0,0 +1,67 @@
+/*
+ *  Copyright (c) 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "modules/audio_coding/audio_network_adaptor/ptime_controller.h"
+
+#include <algorithm>
+
+#include "absl/algorithm/container.h"
+
+namespace webrtc {
+
+PtimeController::PtimeController(
+    rtc::ArrayView<const int> encoder_frame_lengths_ms,
+    int min_payload_bitrate_bps,
+    bool use_stable_target_bitrate)
+    : encoder_frame_lengths_ms_(encoder_frame_lengths_ms.begin(),
+                                encoder_frame_lengths_ms.end()),
+      min_payload_bitrate_bps_(min_payload_bitrate_bps),
+      use_stable_target_bitrate_(use_stable_target_bitrate) {}
+
+void PtimeController::UpdateNetworkMetrics(
+    const NetworkMetrics& network_metrics) {
+  if (network_metrics.target_audio_bitrate_bps) {
+    target_bitrate_bps_ = network_metrics.target_audio_bitrate_bps;
+  }
+  if (network_metrics.overhead_bytes_per_packet) {
+    overhead_bytes_per_packet_ = network_metrics.overhead_bytes_per_packet;
+  }
+  if (network_metrics.uplink_bandwidth_bps) {
+    uplink_bandwidth_bps_ = network_metrics.uplink_bandwidth_bps;
+  }
+}
+
+void PtimeController::MakeDecision(AudioEncoderRuntimeConfig* config) {
+  if (!target_bitrate_bps_ || !overhead_bytes_per_packet_ ||
+      !uplink_bandwidth_bps_) {
+    return;
+  }
+
+  auto it =
+      absl::c_find_if(encoder_frame_lengths_ms_, [&](int frame_length_ms) {
+        int target = use_stable_target_bitrate_ ? *uplink_bandwidth_bps_
+                                                : *target_bitrate_bps_;
+        return target - OverheadBps(frame_length_ms) > min_payload_bitrate_bps_;
+      });
+
+  // Longest frame length is chosen if none match our criteria.
+  int frame_length_ms = it != encoder_frame_lengths_ms_.end()
+                            ? *it
+                            : encoder_frame_lengths_ms_.back();
+  config->frame_length_ms = frame_length_ms;
+  config->bitrate_bps =
+      std::max(0, *target_bitrate_bps_ - OverheadBps(frame_length_ms));
+}
+
+int PtimeController::OverheadBps(int frame_length_ms) const {
+  return *overhead_bytes_per_packet_ * 8 * 1000 / frame_length_ms;
+}
+
+}  // namespace webrtc
---
+++ modules/audio_coding/audio_network_adaptor/ptime_controller.h
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (c) 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef MODULES_AUDIO_CODING_AUDIO_NETWORK_ADAPTOR_PTIME_CONTROLLER_H_
+#define MODULES_AUDIO_CODING_AUDIO_NETWORK_ADAPTOR_PTIME_CONTROLLER_H_
+
+#include <vector>
+
+#include "absl/types/optional.h"
+#include "modules/audio_coding/audio_network_adaptor/controller.h"
+#include "modules/audio_coding/audio_network_adaptor/include/audio_network_adaptor.h"
+
+namespace webrtc {
+
+class PtimeController final : public Controller {
+ public:
+  PtimeController(rtc::ArrayView<const int> encoder_frame_lengths_ms,
+                  int min_payload_bitrate_bps,
+                  bool use_stable_target_bitrate);
+
+  void UpdateNetworkMetrics(const NetworkMetrics& network_metrics) override;
+
+  void MakeDecision(AudioEncoderRuntimeConfig* config) override;
+
+ private:
+  int OverheadBps(int frame_length_ms) const;
+
+  const std::vector<int> encoder_frame_lengths_ms_;
+  const int min_payload_bitrate_bps_;
+  const bool use_stable_target_bitrate_;
+
+  absl::optional<int> uplink_bandwidth_bps_;
+  absl::optional<int> target_bitrate_bps_;
+  absl::optional<int> overhead_bytes_per_packet_;
+};
+
+}  // namespace webrtc
+
+#endif  // MODULES_AUDIO_CODING_AUDIO_NETWORK_ADAPTOR_PTIME_CONTROLLER_H_
