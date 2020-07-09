35bd1f73cc4f1cc1997c44ff08e821eddd77d9cc
Evan Shrubsole <eshr@google.com>
2020-07-09 11:47:29 +0200

    [Adaptation] Move AdaptationConstraints to VideoStreamAdapter
    Bug: webrtc:11754
    Change-Id: Ic51059834ea61c08e977a3a6b5bba48abc86f873

--- call/adaptation/resource_adaptation_processor.h
+++ call/adaptation/resource_adaptation_processor.h
@@ -25,7 +25,6 @@
 #include "api/video/video_adaptation_counters.h"
 #include "api/video/video_frame.h"
 #include "api/video/video_stream_encoder_observer.h"
-#include "call/adaptation/adaptation_constraint.h"
 #include "call/adaptation/resource_adaptation_processor_interface.h"
 #include "call/adaptation/video_source_restrictions.h"
 #include "call/adaptation/video_stream_adapter.h"
@@ -70,10 +69,6 @@ class ResourceAdaptationProcessor : public ResourceAdaptationProcessorInterface,
   void AddResource(rtc::scoped_refptr<Resource> resource) override;
   std::vector<rtc::scoped_refptr<Resource>> GetResources() const override;
   void RemoveResource(rtc::scoped_refptr<Resource> resource) override;
-  void AddAdaptationConstraint(
-      AdaptationConstraint* adaptation_constraint) override;
-  void RemoveAdaptationConstraint(
-      AdaptationConstraint* adaptation_constraint) override;
 
   // ResourceListener implementation.
   // Triggers OnResourceUnderuse() or OnResourceOveruse().
@@ -158,8 +153,6 @@ class ResourceAdaptationProcessor : public ResourceAdaptationProcessorInterface,
   std::vector<ResourceLimitationsListener*> resource_limitations_listeners_
       RTC_GUARDED_BY(resource_adaptation_queue_);
   std::vector<rtc::scoped_refptr<Resource>> resources_
-      RTC_GUARDED_BY(resource_adaptation_queue_);
-  std::vector<AdaptationConstraint*> adaptation_constraints_
       RTC_GUARDED_BY(resource_adaptation_queue_);
   // Purely used for statistics, does not ensure mapped resources stay alive.
   std::map<rtc::scoped_refptr<Resource>,
