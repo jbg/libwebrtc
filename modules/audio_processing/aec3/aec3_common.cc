ec314d72318ece14d3d1d90fff5aef403cd0cc60
Zhaoliang Ma <zhaoliang.ma@intel.com>
2020-07-17 13:11:03 +0800

    aec3: Support AVX2/FMA intrinsics in AEC3
    Bug: webrtc:11663
    Change-Id: Ib75eb616ef0cb62698b0d96af7ebe42e93825222

--- modules/audio_processing/aec3/aec3_common.cc
+++ modules/audio_processing/aec3/aec3_common.cc
@@ -20,7 +20,13 @@ namespace webrtc {
 
 Aec3Optimization DetectOptimization() {
 #if defined(WEBRTC_ARCH_X86_FAMILY)
-  if (WebRtc_GetCPUInfo(kSSE2) != 0) {
+  if (WebRtc_GetCPUInfo(kAVX2) != 0) {
+#if defined(WEBRTC_ENABLE_AVX2)
+    return Aec3Optimization::kAvx2;
+#else
+    return Aec3Optimization::kSse2;
+#endif  // WEBRTC_ENABLE_AVX2
+  } else if (WebRtc_GetCPUInfo(kSSE2) != 0) {
     return Aec3Optimization::kSse2;
   }
 #endif
