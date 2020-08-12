dangxiwang/webrtc

Add Presentation Mode Support for Capturing OpenOffice Impress document windows
* Add OpenOfficeApplicationHandler for MacOS and MS Windows.
* List of available sources for FullScreenWindowDetector on MacOS can include a window with empty title along with titled window for one application.
* List of available sources for FullScreenWindowDetector on MS Windows can include a window with empty title or invisible ones.
Bug: webrtc:11462

Change-Id: Id09537579ef6617dee29759c66dc9f7493166ca8

modules/desktop_capture/win/screen_capture_utils.cc
@@ -105,4 +105,19 @@ DesktopRect GetScreenRect(DesktopCapturer::SourceId screen,
       device_mode.dmPelsWidth, device_mode.dmPelsHeight);
 }
 
+bool IsScreenRect(const DesktopRect& rect) {
+  DesktopCapturer::SourceList screens;
+  if (!GetScreenList(&screens))
+    return false;
+
+  for (size_t i = 0; i < screens.size(); ++i) {
+    std::wstring device_key;
+    if (IsScreenValid(screens[i].id, &device_key) &&
+        rect.equals(GetScreenRect(screens[i].id, device_key))) {
+      return true;
+    }
+  }
+  return false;
+}
+
 }  // namespace webrtc

jbg/libwebrtc

modules/desktop_capture/win/screen_capture_utils.cc
@@ -105,4 +105,19 @@ DesktopRect GetScreenRect(DesktopCapturer::SourceId screen,
       device_mode.dmPelsWidth, device_mode.dmPelsHeight);
 }
 
+bool EqualsScreenRect(const DesktopRect& rect) {
+  DesktopCapturer::SourceList screens;
+  if (!GetScreenList(&screens))
+    return false;
+
+  for (size_t i = 0; i < screens.size(); ++i) {
+    std::wstring device_key;
+    if (IsScreenValid(screens[i].id, &device_key) &&
+        rect.equals(GetScreenRect(screens[i].id, device_key))) {
+      return true;
+    }
+  }
+  return false;
+}
+
 }  // namespace webrtc