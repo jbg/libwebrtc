c1e43fa058c5aa4e1a6800ba50a3ef7a97d31055
Jonas Oreland <jonaso@webrtc.org>
2020-08-19 15:12:05 +0200

    p2p_transport_channel: Add estimated disconnected time to CandidatePairChangeEvent
    This patch adds a computed estimate on how long the ice stack
    was disconnected before switching to a new connection.
    
    The metric is currently computed as now - max(connection->last_data_recevied())
    and has resonably good precision.
    
    Bug: webrtc:11862
    Change-Id: I8950d55f0eadcf164de089cdb715b4f7eed0a4c3

--- sdk/android/api/org/webrtc/CandidatePairChangeEvent.java
+++ sdk/android/api/org/webrtc/CandidatePairChangeEvent.java
@@ -19,13 +19,15 @@ public final class CandidatePairChangeEvent {
   public final IceCandidate remote;
   public final int lastDataReceivedMs;
   public final String reason;
+  public final int estimatedDisconnectedTimeMs;
 
   @CalledByNative
-  CandidatePairChangeEvent(
-      IceCandidate local, IceCandidate remote, int lastDataReceivedMs, String reason) {
+  CandidatePairChangeEvent(IceCandidate local, IceCandidate remote, int lastDataReceivedMs,
+      String reason, int estimatedDisconnectedTimeMs) {
     this.local = local;
     this.remote = remote;
     this.lastDataReceivedMs = lastDataReceivedMs;
     this.reason = reason;
+    this.estimatedDisconnectedTimeMs = estimatedDisconnectedTimeMs;
   }
 }
