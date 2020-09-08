41a80808884cb5f3974b7489888d8e56b98e93e4
Artem Titov <titovartem@webrtc.org>
2020-09-08 12:19:25 +0200

    Use time controller based task queue factory in PC level tests
    Bug: None
    Change-Id: If6dd59691d966378f8ff897c82dee05c1899e9e0

--- test/pc/e2e/peer_connection_quality_test.cc
+++ test/pc/e2e/peer_connection_quality_test.cc
@@ -109,7 +109,7 @@ PeerConnectionE2EQualityTest::PeerConnectionE2EQualityTest(
     std::unique_ptr<AudioQualityAnalyzerInterface> audio_quality_analyzer,
     std::unique_ptr<VideoQualityAnalyzerInterface> video_quality_analyzer)
     : time_controller_(time_controller),
-      task_queue_factory_(CreateDefaultTaskQueueFactory()),
+      task_queue_factory_(time_controller_.CreateTaskQueueFactory()),
       test_case_name_(std::move(test_case_name)),
       executor_(std::make_unique<TestActivitiesExecutor>(
           time_controller_.GetClock())) {
