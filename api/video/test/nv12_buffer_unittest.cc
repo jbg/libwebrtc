3053df8859c4ef4aa79e921a21210dc9465504c3
Evan Shrubsole <eshr@google.com>
2020-09-09 14:23:04 +0200

    Add NV12 video buffer type
    Bug: webrtc:11916
    Change-Id: I30e9c42c0607bee07691930c0248921bba09134c

---
+++ api/video/test/nv12_buffer_unittest.cc
@@ -0,0 +1,109 @@
+/*
+ *  Copyright (c) 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "api/video/nv12_buffer.h"
+
+#include "api/video/i420_buffer.h"
+#include "test/frame_utils.h"
+#include "test/gmock.h"
+#include "test/gtest.h"
+#include "third_party/libyuv/include/libyuv/convert_from.h"
+
+namespace webrtc {
+
+namespace {
+int GetY(rtc::scoped_refptr<NV12BufferInterface> buf, int col, int row) {
+  return buf->DataY()[row * buf->StrideY() + col];
+}
+
+int GetU(rtc::scoped_refptr<NV12BufferInterface> buf, int col, int row) {
+  return buf->DataUV()[(row / 2) * buf->StrideUV() + (col / 2) * 2];
+}
+
+int GetV(rtc::scoped_refptr<NV12BufferInterface> buf, int col, int row) {
+  return buf->DataUV()[(row / 2) * buf->StrideUV() + (col / 2) * 2 + 1];
+}
+
+void FillNV12Buffer(rtc::scoped_refptr<NV12Buffer> buf) {
+  const uint8_t Y = 1;
+  const uint8_t U = 2;
+  const uint8_t V = 3;
+  for (int row = 0; row < buf->height(); ++row) {
+    for (int col = 0; col < buf->width(); ++col) {
+      buf->MutableDataY()[row * buf->StrideY() + col] = Y;
+    }
+  }
+  // Fill interleaving UV values.
+  for (int row = 0; row < buf->ChromaHeight(); row++) {
+    for (int col = 0; col < buf->StrideUV(); col += 2) {
+      int u_index = row * buf->StrideUV() + col;
+      buf->MutableDataUV()[u_index] = U;
+      buf->MutableDataUV()[u_index + 1] = V;
+    }
+  }
+}
+
+}  // namespace
+
+TEST(NV12BufferTest, InitialStrides) {
+  constexpr int stride_y = 3;
+  constexpr int stride_uv = 4;
+  constexpr int width = 3;
+  constexpr int height = 3;
+
+  rtc::scoped_refptr<NV12Buffer> nv12_buffer(NV12Buffer::Create(width, height));
+  EXPECT_EQ(width, nv12_buffer->width());
+  EXPECT_EQ(height, nv12_buffer->height());
+  EXPECT_EQ(stride_y, nv12_buffer->StrideY());
+  EXPECT_EQ(stride_uv, nv12_buffer->StrideUV());
+}
+
+TEST(NV12BufferTest, ReadPixels) {
+  constexpr int width = 3;
+  constexpr int height = 3;
+
+  rtc::scoped_refptr<NV12Buffer> nv12_buffer(NV12Buffer::Create(width, height));
+  // Y = 1, U = 2, V = 3.
+  FillNV12Buffer(nv12_buffer);
+  for (int row = 0; row < height; row++) {
+    for (int col = 0; col < width; col++) {
+      EXPECT_EQ(1, GetY(nv12_buffer, col, row));
+      EXPECT_EQ(2, GetU(nv12_buffer, col, row));
+      EXPECT_EQ(3, GetV(nv12_buffer, col, row));
+    }
+  }
+}
+
+TEST(NV12BufferTest, ToI420) {
+  constexpr int width = 3;
+  constexpr int height = 3;
+  constexpr int size_y = width * height;
+  constexpr int size_u = (width + 1) / 2 * (height + 1) / 2;
+  constexpr int size_v = (width + 1) / 2 * (height + 1) / 2;
+  rtc::scoped_refptr<I420Buffer> reference(I420Buffer::Create(width, height));
+  memset(reference->MutableDataY(), 8, size_y);
+  memset(reference->MutableDataU(), 4, size_u);
+  memset(reference->MutableDataV(), 2, size_v);
+
+  rtc::scoped_refptr<NV12Buffer> nv12_buffer(NV12Buffer::Create(width, height));
+  // Convert the reference buffer to NV12.
+  libyuv::I420ToNV12(
+      reference->DataY(), reference->StrideY(), reference->DataU(),
+      reference->StrideU(), reference->DataV(), reference->StrideV(),
+      nv12_buffer->MutableDataY(), nv12_buffer->StrideY(),
+      nv12_buffer->MutableDataUV(), nv12_buffer->StrideUV(), width, height);
+
+  rtc::scoped_refptr<I420BufferInterface> i420_buffer(nv12_buffer->ToI420());
+  EXPECT_EQ(height, i420_buffer->height());
+  EXPECT_EQ(width, i420_buffer->width());
+  EXPECT_TRUE(test::FrameBufsEqual(reference, i420_buffer));
+}
+
+}  // namespace webrtc
