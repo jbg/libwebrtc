fcb2167539e09a4db8681fc33781efb57d0733a4
Bin Zhu <ricebin@google.com>
2020-09-10 10:22:28 -0700

    expose MicroDirection api
    Bug:None
    Change-Id: I318853a2a1e8c202e3c166d69f5c3347de27f8e3

--- sdk/android/api/org/webrtc/audio/JavaAudioDeviceModule.java
+++ sdk/android/api/org/webrtc/audio/JavaAudioDeviceModule.java
@@ -10,6 +10,7 @@
 
 package org.webrtc.audio;
 
+import android.annotation.FloatRange;
 import android.content.Context;
 import android.media.AudioDeviceInfo;
 import android.media.AudioManager;
@@ -382,6 +383,39 @@ public class JavaAudioDeviceModule implements AudioDeviceModule {
   public void setPreferredInputDevice(AudioDeviceInfo preferredInputDevice) {
     Logging.d(TAG, "setPreferredInputDevice: " + preferredInputDevice);
     audioInput.setPreferredDevice(preferredInputDevice);
+  }
+
+  /**
+   * see
+   * https://developer.android.com/reference/android/media/MicrophoneDirection#setPreferredMicrophoneFieldDimension(float)
+   *
+   * Specifies the logical microphone (for processing). Applications can use this to specify
+   * which side of the device to optimize capture from. Typically used in conjunction with
+   * the camera capturing video.
+   * @param direction MIC_DIRECTION_UNSPECIFIED, MIC_DIRECTION_TOWARDS_USER,
+   *     MIC_DIRECTION_AWAY_FROM_USER, or MIC_DIRECTION_EXTERNAL
+   * @return true if sucessful.
+   */
+  @RequiresApi(Build.VERSION_CODES.Q)
+  public boolean setPreferredMicrophoneDirection(int direction) {
+    return audioInput.setPreferredMicrophoneDirection(direction);
+  }
+
+  /**
+   * see
+   * https://developer.android.com/reference/android/media/MicrophoneDirection#setPreferredMicrophoneFieldDimension(float)
+   *
+   * Specifies the zoom factor (i.e. the field dimension) for the selected microphone
+   * (for processing). The selected microphone is determined by the use-case for the stream.
+   *
+   * @param zoom the desired field dimension of microphone capture. Range is from -1 (wide angle),
+   * though 0 (no zoom) to 1 (maximum zoom).
+   * @return true if sucessful.
+   */
+  @RequiresApi(Build.VERSION_CODES.Q)
+  public boolean setPreferredMicrophoneFieldDimension(
+      @FloatRange(from = -1.0, to = 1.0) float zoom) {
+    return audioInput.setPreferredMicrophoneFieldDimension(zoom);
   }
 
   private static native long nativeCreateAudioDeviceModule(Context context,
