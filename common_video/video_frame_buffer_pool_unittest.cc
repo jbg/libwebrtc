8c96da083a2902405083a8c79ddff8a7e73270fe
Ilya Nikolaevskiy <ilnik@webrtc.org>
2020-09-18 09:15:06 +0200

    Extend I420 frame buffer pool to also create NV12 buffers
    Bug:webrtc:11956
    Change-Id: I758a28f2755cfa72ad486fbe1f9209f356eb5fa1

--- common_video/video_frame_buffer_pool_unittest.cc
+++ common_video/video_frame_buffer_pool_unittest.cc
@@ -8,7 +8,7 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#include "common_video/include/i420_buffer_pool.h"
+#include "common_video/include/video_frame_buffer_pool.h"
 
 #include <stdint.h>
 #include <string.h>
@@ -20,9 +20,9 @@
 
 namespace webrtc {
 
-TEST(TestI420BufferPool, SimpleFrameReuse) {
-  I420BufferPool pool;
-  auto buffer = pool.CreateBuffer(16, 16);
+TEST(TestVideoFrameBufferPool, SimpleFrameReuse) {
+  VideoFrameBufferPool pool;
+  auto buffer = pool.CreateI420Buffer(16, 16);
   EXPECT_EQ(16, buffer->width());
   EXPECT_EQ(16, buffer->height());
   // Extract non-refcounted pointers for testing.
@@ -32,15 +32,15 @@ TEST(TestI420BufferPool, SimpleFrameReuse) {
   // Release buffer so that it is returned to the pool.
   buffer = nullptr;
   // Check that the memory is resued.
-  buffer = pool.CreateBuffer(16, 16);
+  buffer = pool.CreateI420Buffer(16, 16);
   EXPECT_EQ(y_ptr, buffer->DataY());
   EXPECT_EQ(u_ptr, buffer->DataU());
   EXPECT_EQ(v_ptr, buffer->DataV());
 }
 
-TEST(TestI420BufferPool, FrameReuseWithDefaultThenExplicitStride) {
-  I420BufferPool pool;
-  auto buffer = pool.CreateBuffer(15, 16);
+TEST(TestVideoFrameBufferPool, FrameReuseWithDefaultThenExplicitStride) {
+  VideoFrameBufferPool pool;
+  auto buffer = pool.CreateI420Buffer(15, 16);
   EXPECT_EQ(15, buffer->width());
   EXPECT_EQ(16, buffer->height());
   // The default Y stride is width and UV stride is halfwidth (rounded up).
@@ -55,7 +55,7 @@ TEST(TestI420BufferPool, FrameReuseWithDefaultThenExplicitStride) {
   buffer = nullptr;
   // Check that the memory is resued with explicit strides if they match the
   // assumed default above.
-  buffer = pool.CreateBuffer(15, 16, 15, 8, 8);
+  buffer = pool.CreateI420Buffer(15, 16, 15, 8, 8);
   EXPECT_EQ(y_ptr, buffer->DataY());
   EXPECT_EQ(u_ptr, buffer->DataU());
   EXPECT_EQ(v_ptr, buffer->DataV());
@@ -66,40 +66,40 @@ TEST(TestI420BufferPool, FrameReuseWithDefaultThenExplicitStride) {
   EXPECT_EQ(8, buffer->StrideV());
 }
 
-TEST(TestI420BufferPool, FailToReuseWrongSize) {
+TEST(TestVideoFrameBufferPool, FailToReuseWrongSize) {
   // Set max frames to 1, just to make sure the first buffer is being released.
-  I420BufferPool pool(/*zero_initialize=*/false, 1);
-  auto buffer = pool.CreateBuffer(16, 16);
+  VideoFrameBufferPool pool(/*zero_initialize=*/false, 1);
+  auto buffer = pool.CreateI420Buffer(16, 16);
   EXPECT_EQ(16, buffer->width());
   EXPECT_EQ(16, buffer->height());
   // Release buffer so that it is returned to the pool.
   buffer = nullptr;
   // Check that the pool doesn't try to reuse buffers of incorrect size.
-  buffer = pool.CreateBuffer(32, 16);
+  buffer = pool.CreateI420Buffer(32, 16);
   ASSERT_TRUE(buffer);
   EXPECT_EQ(32, buffer->width());
   EXPECT_EQ(16, buffer->height());
 }
 
-TEST(TestI420BufferPool, FailToReuseWrongStride) {
+TEST(TestVideoFrameBufferPool, FailToReuseWrongStride) {
   // Set max frames to 1, just to make sure the first buffer is being released.
-  I420BufferPool pool(/*zero_initialize=*/false, 1);
-  auto buffer = pool.CreateBuffer(32, 32, 32, 16, 16);
+  VideoFrameBufferPool pool(/*zero_initialize=*/false, 1);
+  auto buffer = pool.CreateI420Buffer(32, 32, 32, 16, 16);
   // Make sure the stride was read correctly, for the rest of the test.
   ASSERT_EQ(16, buffer->StrideU());
   ASSERT_EQ(16, buffer->StrideV());
-  buffer = pool.CreateBuffer(32, 32, 32, 20, 20);
+  buffer = pool.CreateI420Buffer(32, 32, 32, 20, 20);
   ASSERT_TRUE(buffer);
   EXPECT_EQ(32, buffer->StrideY());
   EXPECT_EQ(20, buffer->StrideU());
   EXPECT_EQ(20, buffer->StrideV());
 }
 
-TEST(TestI420BufferPool, FrameValidAfterPoolDestruction) {
+TEST(TestVideoFrameBufferPool, FrameValidAfterPoolDestruction) {
   rtc::scoped_refptr<I420Buffer> buffer;
   {
-    I420BufferPool pool;
-    buffer = pool.CreateBuffer(16, 16);
+    VideoFrameBufferPool pool;
+    buffer = pool.CreateI420Buffer(16, 16);
   }
   EXPECT_EQ(16, buffer->width());
   EXPECT_EQ(16, buffer->height());
@@ -107,11 +107,11 @@ TEST(TestI420BufferPool, FrameValidAfterPoolDestruction) {
   memset(buffer->MutableDataY(), 0xA5, 16 * buffer->StrideY());
 }
 
-TEST(TestI420BufferPool, MaxNumberOfBuffers) {
-  I420BufferPool pool(false, 1);
-  auto buffer1 = pool.CreateBuffer(16, 16);
+TEST(TestVideoFrameBufferPool, MaxNumberOfBuffers) {
+  VideoFrameBufferPool pool(false, 1);
+  auto buffer1 = pool.CreateI420Buffer(16, 16);
   EXPECT_NE(nullptr, buffer1.get());
-  EXPECT_EQ(nullptr, pool.CreateBuffer(16, 16).get());
+  EXPECT_EQ(nullptr, pool.CreateI420Buffer(16, 16).get());
 }
 
 }  // namespace webrtc
