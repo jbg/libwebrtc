8afcd26fc4075391f082268ce286a791f859eb9c
Harald Alvestrand <hta@webrtc.org>
2020-09-21 03:51:38 +0000

    Remove stopped transceivers at both local and remote SetDescription
    This should ensure that the correct number of senders and receivers
    are shown.
    
    Bug: webtc:11840
    Change-Id: Id57f8f9b1ceb8900abb3f92bcae79e5f0341de15

--- pc/peer_connection.cc
+++ pc/peer_connection.cc
@@ -1988,13 +1988,7 @@ PeerConnection::GetTransceivers() const {
       << "GetTransceivers is only supported with Unified Plan SdpSemantics.";
   std::vector<rtc::scoped_refptr<RtpTransceiverInterface>> all_transceivers;
   for (const auto& transceiver : transceivers_) {
-    // Temporary fix: Do not show stopped transceivers.
-    // The long term fix is to remove them from transceivers_, but this
-    // turns out to cause issues with audio channel lifetimes.
-    // TODO(https://crbug.com/webrtc/11840): Fix issue.
-    if (!transceiver->stopped()) {
-      all_transceivers.push_back(transceiver);
-    }
+    all_transceivers.push_back(transceiver);
   }
   return all_transceivers;
 }
@@ -2534,6 +2528,36 @@ void PeerConnection::SetLocalDescription(
       });
 }
 
+void PeerConnection::RemoveStoppedTransceivers() {
+  // 3.2.10.1: For each transceiver in the connection's set of transceivers
+  //           run the following steps:
+  if (IsUnifiedPlan()) {
+    for (auto it = transceivers_.begin(); it != transceivers_.end();) {
+      const auto& transceiver = *it;
+      // 3.2.10.1.1: If transceiver is stopped, associated with an m= section
+      //             and the associated m= section is rejected in
+      //             connection.[[CurrentLocalDescription]] or
+      //             connection.[[CurrentRemoteDescription]], remove the
+      //             transceiver from the connection's set of transceivers.
+      if (transceiver->stopped()) {
+        const ContentInfo* content =
+            FindMediaSectionForTransceiver(transceiver, local_description());
+        if (content && content->rejected) {
+          RTC_LOG(LS_INFO) << "Dissociating transceiver"
+                           << " since the media section is being recycled.";
+          (*it)->internal()->set_mid(absl::nullopt);
+          (*it)->internal()->set_mline_index(absl::nullopt);
+          it = transceivers_.erase(it);
+        } else {
+          ++it;
+        }
+      } else {
+        ++it;
+      }
+    }
+  }
+}
+
 void PeerConnection::DoSetLocalDescription(
     std::unique_ptr<SessionDescriptionInterface> desc,
     rtc::scoped_refptr<SetLocalDescriptionObserverInterface> observer) {
@@ -2605,34 +2629,7 @@ void PeerConnection::DoSetLocalDescription(
   RTC_DCHECK(local_description());
 
   if (local_description()->GetType() == SdpType::kAnswer) {
-    // 3.2.10.1: For each transceiver in the connection's set of transceivers
-    //           run the following steps:
-    if (IsUnifiedPlan()) {
-      for (auto it = transceivers_.begin(); it != transceivers_.end();) {
-        const auto& transceiver = *it;
-        // 3.2.10.1.1: If transceiver is stopped, associated with an m= section
-        //             and the associated m= section is rejected in
-        //             connection.[[CurrentLocalDescription]] or
-        //             connection.[[CurrentRemoteDescription]], remove the
-        //             transceiver from the connection's set of transceivers.
-        if (transceiver->stopped()) {
-          const ContentInfo* content =
-              FindMediaSectionForTransceiver(transceiver, local_description());
-
-          if (content && content->rejected) {
-            RTC_LOG(LS_INFO) << "Dissociating transceiver"
-                             << " since the media section is being recycled.";
-            (*it)->internal()->set_mid(absl::nullopt);
-            (*it)->internal()->set_mline_index(absl::nullopt);
-            it = transceivers_.erase(it);
-          } else {
-            ++it;
-          }
-        } else {
-          ++it;
-        }
-      }
-    }
+    RemoveStoppedTransceivers();
 
     // TODO(deadbeef): We already had to hop to the network thread for
     // MaybeStartGathering...
@@ -3100,6 +3097,7 @@ void PeerConnection::DoSetRemoteDescription(
   RTC_DCHECK(remote_description());
 
   if (type == SdpType::kAnswer) {
+    RemoveStoppedTransceivers();
     // TODO(deadbeef): We already had to hop to the network thread for
     // MaybeStartGathering...
     network_thread()->Invoke<void>(
