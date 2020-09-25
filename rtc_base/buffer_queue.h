86d80891b66368a9b2a4b20332e901ed59efa28f
Tomas Gunnarsson <tommi@webrtc.org>
2020-09-25 20:18:31 +0200

    Check BufferQueue threading
    Change-Id: I67b4cec6ee77d73ccffbbc88c9081ebb3c3cc423

--- rtc_base/buffer_queue.h
+++ rtc_base/buffer_queue.h
@@ -19,6 +19,7 @@
 #include "rtc_base/buffer.h"
 #include "rtc_base/constructor_magic.h"
 #include "rtc_base/synchronization/mutex.h"
+#include "rtc_base/synchronization/sequence_checker.h"
 #include "rtc_base/thread_annotations.h"
 
 namespace rtc {
@@ -50,6 +51,7 @@ class BufferQueue {
   virtual void NotifyWritableForTest() {}
 
  private:
+  webrtc::SequenceChecker sequence_checker_;
   size_t capacity_;
   size_t default_size_;
   mutable webrtc::Mutex mutex_;
