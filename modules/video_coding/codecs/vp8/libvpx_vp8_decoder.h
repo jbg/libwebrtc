ac7f14090ac230354b9e55ea0b67df93484ed638
Ilya Nikolaevskiy <ilnik@webrtc.org>
2020-09-28 15:58:20 +0200

    Add NV12 to libvpx wrappers output
    Bug:webrtc:11956
    Change-Id: Id8734b8f0fd87ac9b849d70b0c5764bf1ffd9c75

--- modules/video_coding/codecs/vp8/libvpx_vp8_decoder.h
+++ modules/video_coding/codecs/vp8/libvpx_vp8_decoder.h
@@ -64,6 +64,8 @@ class LibvpxVp8Decoder : public VideoDecoder {
   bool key_frame_required_;
   const absl::optional<DeblockParams> deblock_params_;
   const std::unique_ptr<QpSmoother> qp_smoother_;
+
+  const bool nv12_output_;
 };
 
 }  // namespace webrtc
