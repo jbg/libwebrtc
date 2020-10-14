94fc0427ceb1c1e4e3105ac79a566ee6a21debee
Alessio Bazzica <alessiob@webrtc.org>
2020-10-14 12:46:57 +0200

    AGC2 config change detection fixed
    AGC2 is correctly (re)initialized when its config changes.
    This CL also improves the `AudioProcessingImpl::ApplyConfig`
    readability by defining operator!= also for the AGC1 config.
    
    Bug: webrtc:7494
    Change-Id: I62068de32c941e6b18d4618c656f569647042345

---
+++ modules/audio_processing/include/audio_processing_unittest.cc
@@ -0,0 +1,25 @@
+/*
+ *  Copyright (c) 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "modules/audio_processing/include/audio_processing.h"
+
+namespace webrtc {
+namespace {
+
+TEST(AudioProcessing, GainController1ConfigEqual) {}
+
+TEST(AudioProcessing, GainController1ConfigNotEqual) {}
+
+TEST(AudioProcessing, GainController2ConfigEqual) {}
+
+TEST(AudioProcessing, GainController2ConfigNotEqual) {}
+
+}  // namespace
+}  // namespace webrtc
