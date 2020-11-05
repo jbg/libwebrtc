d171add97a49769e2ed8c37276887b66317dc0e4
Steve Anton <steveanton@webrtc.org>
2020-11-04 19:45:32 -0800

    Add publicly visible mock for RtpTransceiverInterface
    Bug: webrtc:11642
    Change-Id: Iadcaddecb9e02781e1946c37a72eeb678cd91b5b

--- api/test/compile_all_headers.cc
+++ api/test/compile_all_headers.cc
@@ -35,6 +35,7 @@
 #include "api/test/mock_frame_encryptor.h"
 #include "api/test/mock_peer_connection_factory_interface.h"
 #include "api/test/mock_peerconnectioninterface.h"
+#include "api/test/mock_rtp_transceiver.h"
 #include "api/test/mock_rtpreceiver.h"
 #include "api/test/mock_rtpsender.h"
 #include "api/test/mock_transformable_video_frame.h"
