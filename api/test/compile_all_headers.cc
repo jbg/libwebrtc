76431090ad95f97ccc7024e9176427c816e08d34
Steve Anton <steveanton@webrtc.org>
2020-11-04 19:20:08 -0800

    Add publicly visible mocks for AudioSourceInterface and AudioTrackInterface
    Bug: webrtc:11642
    Change-Id: Ia8807623ea7ca2e49fc795b907aec83fd10e3305

--- api/test/compile_all_headers.cc
+++ api/test/compile_all_headers.cc
@@ -33,6 +33,7 @@
 #include "api/test/mock_audio_mixer.h"
 #include "api/test/mock_frame_decryptor.h"
 #include "api/test/mock_frame_encryptor.h"
+#include "api/test/mock_media_stream_interface.h"
 #include "api/test/mock_peer_connection_factory_interface.h"
 #include "api/test/mock_peerconnectioninterface.h"
 #include "api/test/mock_rtpreceiver.h"
