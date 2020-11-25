1a4e1e9e1eb60d33b714c7a39b3c9864c28255e8
Mirko Bonadei <mbonadei@webrtc.org>
2020-11-25 12:00:10 +0100

    Add enable_run_ios_unittests_with_xctest=true on iOS bots.
    As suggested by https://bugs.chromium.org/p/webrtc/issues/detail?id=12134#c21.
    
    Bug: webrtc:12134
    Change-Id: I24c731dee0310e02ae1bbe6c23d359d6fcd18f17

--- examples/objc/AppRTCMobile/tests/main.mm
+++ examples/objc/AppRTCMobile/tests/main.mm
@@ -8,14 +8,20 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 #include "test/ios/coverage_util_ios.h"
 
 int main(int argc, char* argv[]) {
-  rtc::test::ConfigureCoverageReportPath();
+  NSOperatingSystemVersion version = [[NSProcessInfo processInfo] operatingSystemVersion];
+  int32_t major_version = static_cast<int32_t>(version.majorVersion);
+  if (major_version < 14) {
+    rtc::test::ConfigureCoverageReportPath();
 
-  @autoreleasepool {
-    return UIApplicationMain(argc, argv, nil, nil);
+    @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, nil);
+    }
   }
+  return 0;
 }
