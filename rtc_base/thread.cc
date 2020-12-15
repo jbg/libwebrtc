d74f5f892a3242deb3939c2a636016cdf11e1fcd
Ilya Nikolaevskiy <ilnik@webrtc.org>
2020-12-15 15:16:06 +0100

    Ensure that no tasks are posted to wrapped threads
    When rtc::Thread is wrapping current thread it ends up without
    any message processing loop running. Any tasks posted to such thread
    would forever be stuck in a queue.
    
    Bug:None
    Change-Id: I60c5a5a82e06ff5d8f2aad7a0b5afd5797277782

--- rtc_base/thread.cc
+++ rtc_base/thread.cc
@@ -1041,6 +1041,8 @@ bool Thread::IsInvokeToThreadAllowed(rtc::Thread* target) {
 }
 
 void Thread::PostTask(std::unique_ptr<webrtc::QueuedTask> task) {
+  // Unownded or wrapped threads don't run any message processing loops.
+  RTC_CHECK(IsOwned());
   // Though Post takes MessageData by raw pointer (last parameter), it still
   // takes it with ownership.
   Post(RTC_FROM_HERE, &queued_task_handler_,
@@ -1049,6 +1051,8 @@ void Thread::PostTask(std::unique_ptr<webrtc::QueuedTask> task) {
 
 void Thread::PostDelayedTask(std::unique_ptr<webrtc::QueuedTask> task,
                              uint32_t milliseconds) {
+  // Unownded or wrapped threads don't run any message processing loops.
+  RTC_CHECK(IsOwned());
   // Though PostDelayed takes MessageData by raw pointer (last parameter),
   // it still takes it with ownership.
   PostDelayed(RTC_FROM_HERE, milliseconds, &queued_task_handler_,
