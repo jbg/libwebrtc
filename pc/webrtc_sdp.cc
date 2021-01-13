64e32f3e83965c5ccb575c48287cdf3efbd39441
Philipp Hancke <philipp.hancke@googlemail.com>
2021-01-13 10:40:06 +0100

    sdp: cross-check media type and protocol earlier
    catching some unsupported configurations earlier.
    
    BUG=None
    
    Change-Id: I9f366929816fe15031837a3218086aa5a13d787a

--- pc/webrtc_sdp.cc
+++ pc/webrtc_sdp.cc
@@ -2659,6 +2659,10 @@ bool ParseMediaDescription(
     bool bundle_only = false;
     int section_msid_signaling = 0;
     const std::string& media_type = fields[0];
+    if ((media_type == kMediaTypeVideo || media_type == kMediaTypeAudio) &&
+        !cricket::IsRtpProtocol(protocol)) {
+      return ParseFailed(line, "Unsupported protocol for media type", error);
+    }
     if (media_type == kMediaTypeVideo) {
       content = ParseContentDescription<VideoContentDescription>(
           message, cricket::MEDIA_TYPE_VIDEO, mline_index, protocol,
@@ -2695,7 +2699,7 @@ bool ParseMediaDescription(
         }
         data_desc->set_protocol(protocol);
         content = std::move(data_desc);
-      } else {
+      } else if (cricket::IsRtpProtocol(protocol)) {
         // RTP
         std::unique_ptr<RtpDataContentDescription> data_desc =
             ParseContentDescription<RtpDataContentDescription>(
@@ -2703,6 +2707,8 @@ bool ParseMediaDescription(
                 payload_types, pos, &content_name, &bundle_only,
                 &section_msid_signaling, &transport, candidates, error);
         content = std::move(data_desc);
+      } else {
+        return ParseFailed(line, "Unsupported protocol for media type", error);
       }
     } else {
       RTC_LOG(LS_WARNING) << "Unsupported media type: " << line;
@@ -3127,11 +3133,6 @@ bool ParseContent(const std::string& message,
       // SCTP specific attributes
       //
       if (HasAttribute(line, kAttributeSctpPort)) {
-        if (media_type != cricket::MEDIA_TYPE_DATA) {
-          return ParseFailed(
-              line, "sctp-port attribute found in non-data media description.",
-              error);
-        }
         if (media_desc->as_sctp()->use_sctpmap()) {
           return ParseFailed(
               line, "sctp-port attribute can't be used with sctpmap.", error);
@@ -3142,12 +3143,6 @@ bool ParseContent(const std::string& message,
         }
         media_desc->as_sctp()->set_port(sctp_port);
       } else if (HasAttribute(line, kAttributeMaxMessageSize)) {
-        if (media_type != cricket::MEDIA_TYPE_DATA) {
-          return ParseFailed(
-              line,
-              "max-message-size attribute found in non-data media description.",
-              error);
-        }
         int max_message_size;
         if (!ParseSctpMaxMessageSize(line, &max_message_size, error)) {
           return false;
