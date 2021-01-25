08b4ebd172591067c22979a013e80c47eeedacb5
Lahiru Ginnaliya Gamathige <glahiru@webrtc.org>
2021-01-25 04:32:45 -0800

    Add a new CallbacklList for DtlsHandshakeError in dtls_transport.
    - Signal is transferred from jsep_transport_controller to dtls_transport,
      jsep_transport_controller is already using Callbacklist and this
      modified the dtls_transport.
    
    Bug: webrtc:11943
    Change-Id: I4a7ed08e6dab21b8eb515d4d8971f9b084fb8c86

--- p2p/base/dtls_transport.cc
+++ p2p/base/dtls_transport.cc
@@ -820,7 +820,7 @@ void DtlsTransport::set_dtls_state(DtlsTransportState state) {
 }
 
 void DtlsTransport::OnDtlsHandshakeError(rtc::SSLHandshakeError error) {
-  SignalDtlsHandshakeError(error);
+  SendDtlsHandshakeError(error);
 }
 
 void DtlsTransport::ConfigureHandshakeTimeout() {
