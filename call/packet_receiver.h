2e824e27dd8b96c2ee8acf600cc3fd820ebb4b67
Tomas Gunnarsson <tommi@webrtc.org>
2021-02-12 18:04:48 +0100

    Change callback to only be for unknown SSRCs
    Change-Id: I40d5d1ffa9b87600c094338f679aa4918cfc078a

--- call/packet_receiver.h
+++ call/packet_receiver.h
@@ -30,11 +30,12 @@ class PacketReceiver {
     DELIVERY_PACKET_ERROR,
   };
 
-  // Definition of the callback to execute when packet delivery is complete.
-  // The callback will be issued on the same thread as called DeliverPacket.
-  typedef std::function<
-      void(DeliveryStatus, MediaType, rtc::CopyOnWriteBuffer, int64_t)>
-      PacketCallback;
+  // Definition of the callback to issue when packet delivery could not be
+  // completed on the grounds of the SSRC being uknown (status should be assumed
+  // to be DELIVERY_UNKNOWN_SSRC).
+  // An implementation of this callback can choose to retry delivery.
+  typedef std::function<void(MediaType, rtc::CopyOnWriteBuffer, int64_t)>
+      UnknownSsrcCallback;
 
   // Asynchronously handle packet delivery and report back to the caller when
   // delivery of the packet has completed.
@@ -50,10 +51,11 @@ class PacketReceiver {
   virtual void DeliverPacketAsync(MediaType media_type,
                                   rtc::CopyOnWriteBuffer packet,
                                   int64_t packet_time_us,
-                                  PacketCallback callback) {
+                                  UnknownSsrcCallback callback) {
+    // TODO(bugs.webrtc.org/11993): Remove this default implementation.
     DeliveryStatus status = DeliverPacket(media_type, packet, packet_time_us);
-    if (callback)
-      callback(status, media_type, std::move(packet), packet_time_us);
+    if (status == DELIVERY_UNKNOWN_SSRC && callback)
+      callback(media_type, std::move(packet), packet_time_us);
   }
 
   virtual DeliveryStatus DeliverPacket(MediaType media_type,
