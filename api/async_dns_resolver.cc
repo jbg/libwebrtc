a69673b09434970a1f02f32402479caed468f9cb
Harald Alvestrand <hta@webrtc.org>
2021-03-19 20:32:53 +0000

    Add a new API to DNS resolution
    This API should allow existing factories to be used unmodified, but
    offers a new API that documents ownership better and does not use
    sigslot.
    
    Bug: webrtc:12598
    Change-Id: I0f68371059cd4a18ab07b87fc0e7526dcc0ac669

---
+++ api/async_dns_resolver.cc
@@ -0,0 +1,20 @@
+/*
+ *  Copyright 2021 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "api/async_dns_resolver.h"
+
+namespace webrtc {
+
+// Symbol definition to satisfy the requirement for a key function
+// AsyncDnsResolverInterface::AsyncDnsResolverInterface() {}
+
+// AsyncDnsResolverInterface::~AsyncDnsResolverInterface() = default;
+
+}  // namespace webrtc
