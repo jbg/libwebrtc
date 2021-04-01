3450748dcfa00dc0ff5988e3e7e406444a188e0f
Victor Boivie <boivie@webrtc.org>
2021-04-01 08:23:54 +0200

    dcsctp: Add strong typed identifiers
    There are numerous identifiers and sequences in SCTP, all of them being
    unsigned 16 or 32-bit integers.
    
      * Stream identifiers
      * Payload Protocol Identifier (PPID)
      * Stream Sequence Numbers (SSN)
      * Message Identifiers (MID)
      * Fragment Sequence Numbers (FSN)
      * Transmission Sequence Numbers (TSN)
    
    The first two of these are publicly exposed in the API, and the
    remaining ones are never exposed to the client and are all part of SCTP
    protocol.
    
    The unwrapped sequence numbers have been renamed to not cause conflicts
    and the current UnwrappedSequenceNumber class doesn't support wrapping
    strongly typed integers as it can't reach into the type of the
    underlying integer. That's something to explore later.
    
    Bug: webrtc:12614
    Change-Id: I4e0016be26d5d4826783d6e0962044f56cbfa97d

---
+++ net/dcsctp/public/types.h
@@ -0,0 +1,29 @@
+/*
+ *  Copyright 2019 The Chromium Authors. All rights reserved.
+ *  Copyright (c) 2021 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#ifndef NET_DCSCTP_PUBLIC_TYPES_H_
+#define NET_DCSCTP_PUBLIC_TYPES_H_
+
+#include "net/dcsctp/public/strong_alias.h"
+
+namespace dcsctp {
+
+// Stream Identifier
+using StreamID = StrongAlias<class StreamIDTag, uint16_t>;
+
+// Payload Protocol Identifier (PPID)
+using PPID = StrongAlias<class PPIDTag, uint16_t>;
+
+// Timeout Identifier
+using TimeoutID = StrongAlias<class TimeoutTag, uint64_t>;
+
+}  // namespace dcsctp
+
+#endif  // NET_DCSCTP_PUBLIC_TYPES_H_
