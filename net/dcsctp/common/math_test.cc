369dcd019cb1e9a88a1e61e8bb114ec122557ed6
Victor Boivie <boivie@webrtc.org>
2021-04-13 23:42:39 +0200

    dcsctp: Ensure packet size doesn't exceed MTU
    Due to a previous refactoring, the SCTP packet header is only added when
    the first chunk is written. This wasn't reflected in the
    `bytes_remaining`, which made it add more than could fit within the MTU.
    
    Additionally, the maximum packet size must be even divisible by four as
    padding will be added to chunks that are not even divisble by four (up
    to three bytes of padding). So compensate for that.
    
    Bug: webrtc:12614
    Change-Id: I6b57dfbf88d1fcfcbf443038915dd180e796191a

--- net/dcsctp/common/math_test.cc
+++ net/dcsctp/common/math_test.cc
@@ -28,5 +28,33 @@ TEST(MathUtilTest, CanRoundUpTo4) {
   EXPECT_EQ(RoundUpTo4(10000000001), 10000000004);
 }
 
+TEST(MathUtilTest, CanRoundDownTo4) {
+  EXPECT_EQ(RoundDownTo4(0), 0);
+  EXPECT_EQ(RoundDownTo4(1), 0);
+  EXPECT_EQ(RoundDownTo4(2), 0);
+  EXPECT_EQ(RoundDownTo4(3), 0);
+  EXPECT_EQ(RoundDownTo4(4), 4);
+  EXPECT_EQ(RoundDownTo4(5), 4);
+  EXPECT_EQ(RoundDownTo4(6), 4);
+  EXPECT_EQ(RoundDownTo4(7), 4);
+  EXPECT_EQ(RoundDownTo4(8), 8);
+  EXPECT_EQ(RoundDownTo4(10000000000), 10000000000);
+  EXPECT_EQ(RoundDownTo4(10000000001), 10000000000);
+}
+
+TEST(MathUtilTest, IsDivisibleBy4) {
+  EXPECT_EQ(IsDivisibleBy4(0), true);
+  EXPECT_EQ(IsDivisibleBy4(1), false);
+  EXPECT_EQ(IsDivisibleBy4(2), false);
+  EXPECT_EQ(IsDivisibleBy4(3), false);
+  EXPECT_EQ(IsDivisibleBy4(4), true);
+  EXPECT_EQ(IsDivisibleBy4(5), false);
+  EXPECT_EQ(IsDivisibleBy4(6), false);
+  EXPECT_EQ(IsDivisibleBy4(7), false);
+  EXPECT_EQ(IsDivisibleBy4(8), true);
+  EXPECT_EQ(IsDivisibleBy4(10000000000), true);
+  EXPECT_EQ(IsDivisibleBy4(10000000001), false);
+}
+
 }  // namespace
 }  // namespace dcsctp
