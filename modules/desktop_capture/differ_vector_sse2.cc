47706c483fd822b0c6e5a60402648e6e1fe681f3
Zhaoliang Ma <zhaoliang.ma@intel.com>
2021-05-07 10:00:57 +0800

    desktop_capture: refine the sse2 code for differ vector
    Bug: None
    Test: modules_unittests
    Change-Id: I9c3698c5a9a2f48b6133e3bd8940a9ed52fa820c

--- modules/desktop_capture/differ_vector_sse2.cc
+++ modules/desktop_capture/differ_vector_sse2.cc
@@ -19,84 +19,36 @@
 
 namespace webrtc {
 
-extern bool VectorDifference_SSE2_W16(const uint8_t* image1,
-                                      const uint8_t* image2) {
-  __m128i acc = _mm_setzero_si128();
+namespace {
+extern bool VectorDifference_SSE2(const uint8_t* image1,
+                                  const uint8_t* image2,
+                                  const int block_size) {
   __m128i v0;
   __m128i v1;
-  __m128i sad;
   const __m128i* i1 = reinterpret_cast<const __m128i*>(image1);
   const __m128i* i2 = reinterpret_cast<const __m128i*>(image2);
-  v0 = _mm_loadu_si128(i1);
-  v1 = _mm_loadu_si128(i2);
-  sad = _mm_sad_epu8(v0, v1);
-  acc = _mm_adds_epu16(acc, sad);
-  v0 = _mm_loadu_si128(i1 + 1);
-  v1 = _mm_loadu_si128(i2 + 1);
-  sad = _mm_sad_epu8(v0, v1);
-  acc = _mm_adds_epu16(acc, sad);
-  v0 = _mm_loadu_si128(i1 + 2);
-  v1 = _mm_loadu_si128(i2 + 2);
-  sad = _mm_sad_epu8(v0, v1);
-  acc = _mm_adds_epu16(acc, sad);
-  v0 = _mm_loadu_si128(i1 + 3);
-  v1 = _mm_loadu_si128(i2 + 3);
-  sad = _mm_sad_epu8(v0, v1);
-  acc = _mm_adds_epu16(acc, sad);
 
-  // This essential means sad = acc >> 64. We only care about the lower 16
-  // bits.
-  sad = _mm_shuffle_epi32(acc, 0xEE);
-  sad = _mm_adds_epu16(sad, acc);
-  return _mm_cvtsi128_si32(sad) != 0;
+  const int vector_limit = block_size >> 2;
+  for (int i = 0; i < vector_limit; ++i) {
+    v0 = _mm_loadu_si128(i1 + i);
+    v1 = _mm_loadu_si128(i2 + i);
+    __m128i cmp = _mm_cmpeq_epi8(v0, v1);
+    unsigned bitmask = _mm_movemask_epi8(cmp);
+    if (bitmask != 0xFFFFU)
+      return true;
+  }
+  return false;
+}
+}  // namespace
+
+extern bool VectorDifference_SSE2_W16(const uint8_t* image1,
+                                      const uint8_t* image2) {
+  return VectorDifference_SSE2(image1, image2, 16);
 }
 
 extern bool VectorDifference_SSE2_W32(const uint8_t* image1,
                                       const uint8_t* image2) {
-  __m128i acc = _mm_setzero_si128();
-  __m128i v0;
-  __m128i v1;
-  __m128i sad;
-  const __m128i* i1 = reinterpret_cast<const __m128i*>(image1);
-  const __m128i* i2 = reinterpret_cast<const __m128i*>(image2);
-  v0 = _mm_loadu_si128(i1);
-  v1 = _mm_loadu_si128(i2);
-  sad = _mm_sad_epu8(v0, v1);
-  acc = _mm_adds_epu16(acc, sad);
-  v0 = _mm_loadu_si128(i1 + 1);
-  v1 = _mm_loadu_si128(i2 + 1);
-  sad = _mm_sad_epu8(v0, v1);
-  acc = _mm_adds_epu16(acc, sad);
-  v0 = _mm_loadu_si128(i1 + 2);
-  v1 = _mm_loadu_si128(i2 + 2);
-  sad = _mm_sad_epu8(v0, v1);
-  acc = _mm_adds_epu16(acc, sad);
-  v0 = _mm_loadu_si128(i1 + 3);
-  v1 = _mm_loadu_si128(i2 + 3);
-  sad = _mm_sad_epu8(v0, v1);
-  acc = _mm_adds_epu16(acc, sad);
-  v0 = _mm_loadu_si128(i1 + 4);
-  v1 = _mm_loadu_si128(i2 + 4);
-  sad = _mm_sad_epu8(v0, v1);
-  acc = _mm_adds_epu16(acc, sad);
-  v0 = _mm_loadu_si128(i1 + 5);
-  v1 = _mm_loadu_si128(i2 + 5);
-  sad = _mm_sad_epu8(v0, v1);
-  acc = _mm_adds_epu16(acc, sad);
-  v0 = _mm_loadu_si128(i1 + 6);
-  v1 = _mm_loadu_si128(i2 + 6);
-  sad = _mm_sad_epu8(v0, v1);
-  acc = _mm_adds_epu16(acc, sad);
-  v0 = _mm_loadu_si128(i1 + 7);
-  v1 = _mm_loadu_si128(i2 + 7);
-  sad = _mm_sad_epu8(v0, v1);
-  acc = _mm_adds_epu16(acc, sad);
-
-  // This essential means sad = acc >> 64. We only care about the lower 16
-  // bits.
-  sad = _mm_shuffle_epi32(acc, 0xEE);
-  sad = _mm_adds_epu16(sad, acc);
-  return _mm_cvtsi128_si32(sad) != 0;
+  return VectorDifference_SSE2(image1, image2, 32);
 }
 
 }  // namespace webrtc
