039021c8d08f55c7ad98391e96c759ee917edd74
Zhaoliang Ma <zhaoliang.ma@intel.com>
2021-05-07 10:53:50 +0800

    desktop_capture: support avx2 for differ vector
    Bug: None
    Change-Id: Icfe5201b41e3a6063f048d2b3152e237ebd3ae84

--- modules/desktop_capture/differ_vector_simd.h
+++ modules/desktop_capture/differ_vector_simd.h
@@ -8,24 +8,24 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-// This header file is used only differ_block.h. It defines the SSE2 rountines
+// This header file is used only differ_block.h. It defines the SIMD rountines
 // for finding vector difference.
 
-#ifndef MODULES_DESKTOP_CAPTURE_DIFFER_VECTOR_SSE2_H_
-#define MODULES_DESKTOP_CAPTURE_DIFFER_VECTOR_SSE2_H_
+#ifndef MODULES_DESKTOP_CAPTURE_DIFFER_VECTOR_SIMD_H_
+#define MODULES_DESKTOP_CAPTURE_DIFFER_VECTOR_SIMD_H_
 
 #include <stdint.h>
 
 namespace webrtc {
 
 // Find vector difference of dimension 16.
-extern bool VectorDifference_SSE2_W16(const uint8_t* image1,
+extern bool VectorDifference_SIMD_W16(const uint8_t* image1,
                                       const uint8_t* image2);
 
 // Find vector difference of dimension 32.
-extern bool VectorDifference_SSE2_W32(const uint8_t* image1,
+extern bool VectorDifference_SIMD_W32(const uint8_t* image1,
                                       const uint8_t* image2);
 
 }  // namespace webrtc
 
-#endif  // MODULES_DESKTOP_CAPTURE_DIFFER_VECTOR_SSE2_H_
+#endif  // MODULES_DESKTOP_CAPTURE_DIFFER_VECTOR_SIMD_H_
