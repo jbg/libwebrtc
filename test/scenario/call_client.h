9f71b0b0ba664267fc27d19d4c487538a43cc795
Danil Chapovalov <danilchap@webrtc.org>
2021-06-02 20:00:12 +0200

    Reduce usage of RtpHeaderParser::CreateForTest in favor of RtpPacket
    As a step to delete the legacy rtp packet parser.
    
    Bug: None
    Change-Id: I2aae86bc8847acd76cdd89007273a99f0298fdb9

--- test/scenario/call_client.h
+++ test/scenario/call_client.h
@@ -26,7 +26,6 @@
 #include "rtc_base/task_queue_for_test.h"
 #include "test/logging/log_writer.h"
 #include "test/network/network_emulation.h"
-#include "test/rtp_header_parser.h"
 #include "test/scenario/column_printer.h"
 #include "test/scenario/network_node.h"
 #include "test/scenario/scenario_config.h"
@@ -149,7 +148,6 @@ class CallClient : public EmulatedNetworkReceiverInterface {
   CallClientFakeAudio fake_audio_setup_;
   std::unique_ptr<Call> call_;
   std::unique_ptr<NetworkNodeTransport> transport_;
-  std::unique_ptr<RtpHeaderParser> const header_parser_;
   std::vector<std::pair<EmulatedEndpoint*, uint16_t>> endpoints_;
 
   int next_video_ssrc_index_ = 0;
