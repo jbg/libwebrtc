c52382fc5133d1fe0188cf63cc86bf30deaf414c
webrtc-version-updater <webrtc-version-updater@webrtc-ci.iam.gserviceaccount.com>
2021-06-03 21:05:40 -0700

    Update WebRTC code version (2021-06-04T04:05:37).
    TBR=webrtc-version-updater@webrtc-ci.iam.gserviceaccount.com,mbonadei@webrtc.org
    
    Bug: None
    Change-Id: I91a468f44ba3b191da3869654e695416e07b43d3

--- call/version.cc
+++ call/version.cc
@@ -13,7 +13,7 @@
 namespace webrtc {
 
 // The timestamp is always in UTC.
-const char* const kSourceTimestamp = "WebRTC source stamp 2021-06-02T04:02:07";
+const char* const kSourceTimestamp = "WebRTC source stamp 2021-06-04T04:05:37";
 
 void LoadWebRTCVersionInRegister() {
   // Using volatile to instruct the compiler to not optimize `p` away even
