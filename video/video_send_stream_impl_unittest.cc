cec5cf558ad6169d7dc1b0a165512079f926706b
Jakob Ivarsson <jakobi@webrtc.org>
2021-06-08 13:18:26 +0200

    Avoid video stream allocation on configuration change after timeout.
    Bug: b/189842675
    Change-Id: I0793bd4cbf2a4faed92cf811550437ae75742102

--- video/video_send_stream_impl_unittest.cc
+++ video/video_send_stream_impl_unittest.cc
@@ -1033,5 +1033,57 @@ TEST_F(VideoSendStreamImplTest, ConfiguresBitratesForSvc) {
         RTC_FROM_HERE);
   }
 }
+
+TEST_F(VideoSendStreamImplTest, AvoidAllocationAfterTimeout) {
+  std::unique_ptr<VideoSendStreamImpl> vss_impl;
+  test_queue_.SendTask(
+      [&] {
+        vss_impl = CreateVideoSendStreamImpl(
+            kDefaultInitialBitrateBps, kDefaultBitratePriority,
+            VideoEncoderConfig::ContentType::kRealtimeVideo);
+        EXPECT_CALL(bitrate_allocator_, AddObserver(vss_impl.get(), _))
+            .Times(1);
+        vss_impl->Start();
+        const uint32_t kBitrateBps = 100000;
+        EXPECT_CALL(rtp_video_sender_, GetPayloadBitrateBps())
+            .Times(1)
+            .WillOnce(Return(kBitrateBps));
+        static_cast<BitrateAllocatorObserver*>(vss_impl.get())
+            ->OnBitrateUpdated(CreateAllocation(kBitrateBps));
+
+        // The stream should be de-allocated after a timeout.
+        EXPECT_CALL(bitrate_allocator_, RemoveObserver(vss_impl.get()))
+            .Times(1);
+      },
+      RTC_FROM_HERE);
+
+  rtc::Event done;
+  test_queue_.PostDelayedTask(
+      [&] {
+        VideoStream qvga_stream;
+        qvga_stream.width = 320;
+        qvga_stream.height = 180;
+        qvga_stream.max_framerate = 30;
+        qvga_stream.min_bitrate_bps = 30000;
+        qvga_stream.target_bitrate_bps = 150000;
+        qvga_stream.max_bitrate_bps = 200000;
+        qvga_stream.max_qp = 56;
+        qvga_stream.bitrate_priority = 1;
+
+        int min_transmit_bitrate_bps = 30000;
+        // This should not allocate the stream since it has timed out.
+        static_cast<VideoStreamEncoderInterface::EncoderSink*>(vss_impl.get())
+            ->OnEncoderConfigurationChanged(
+                std::vector<VideoStream>{qvga_stream}, false,
+                VideoEncoderConfig::ContentType::kRealtimeVideo,
+                min_transmit_bitrate_bps);
+        testing::Mock::VerifyAndClearExpectations(&bitrate_allocator_);
+        vss_impl->Stop();
+        vss_impl.reset();
+        done.Set();
+      },
+      2000);
+  ASSERT_TRUE(done.Wait(5000));
+}
 }  // namespace internal
 }  // namespace webrtc
