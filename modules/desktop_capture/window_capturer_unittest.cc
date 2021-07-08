a6fafe93f0bdc2b2ebfb5d0dbbdfb038a0f40aa3
Mirko Bonadei <mbonadei@webrtc.org>
2021-07-08 10:42:53 +0200

    Re-enable skipped MSan tests.
    Bug: None
    Change-Id: I713f0905990ae7caacd0ad1758bffaa917fefd58

--- modules/desktop_capture/window_capturer_unittest.cc
+++ modules/desktop_capture/window_capturer_unittest.cc
@@ -44,13 +44,7 @@ class WindowCapturerTest : public ::testing::Test,
 };
 
 // Verify that we can enumerate windows.
-// TODO(bugs.webrtc.org/12950): Re-enable when libc++ issue is fixed
-#if defined(WEBRTC_LINUX) && defined(MEMORY_SANITIZER)
-#define MAYBE_Enumerate DISABLED_Enumerate
-#else
-#define MAYBE_Enumerate Enumerate
-#endif
-TEST_F(WindowCapturerTest, MAYBE_Enumerate) {
+TEST_F(WindowCapturerTest, Enumerate) {
   DesktopCapturer::SourceList sources;
   EXPECT_TRUE(capturer_->GetSourceList(&sources));
 
