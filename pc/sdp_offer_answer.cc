299773cbdf383a9d4c0b37f71038b811b16d06a8
Philipp Hancke <philipp.hancke@googlemail.com>
2021-11-10 11:56:00 +0100

    dont fire ontrack for rejected media sections
    BUG=chromium:952200
    
    Change-Id: I10769cf6446dae1ce48f5a881cb00e1846ad443b

--- pc/sdp_offer_answer.cc
+++ pc/sdp_offer_answer.cc
@@ -1679,8 +1679,9 @@ RTCError SdpOfferAnswerHandler::ApplyRemoteDescription(
       // Roughly the same as steps 2.2.8.6 of section 4.4.1.6 "Set the
       // RTCSessionDescription: Set the associated remote streams given
       // transceiver.[[Receiver]], msids, addList, and removeList".
-      // https://w3c.github.io/webrtc-pc/#set-the-rtcsessiondescription
-      if (RtpTransceiverDirectionHasRecv(local_direction)) {
+      // https://w3c.github.io/webrtc-pc/#set-the-session-description
+      if (RtpTransceiverDirectionHasRecv(local_direction) &&
+          !content->rejected) {
         std::vector<std::string> stream_ids;
         if (!media_desc->streams().empty()) {
           // The remote description has signaled the stream IDs.
