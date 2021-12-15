4330ad090acf65dba5d11995d96c0acbd93f0202
Evan Shrubsole <eshr@webrtc.org>
2021-12-15 14:33:40 +0100

    Add FrameBufferProxy in prep for FrameBuffer3
    This is a delegate that is used by video_receive_stream2 to handle frame
    buffer tasks like threading, and stats. This will be used in a follow up
    to use FrameBuffer3 as a strategy selected by field trial.
    
    Change-Id: I524279343d60a348d044d9085d618f12d7bf3a23
    Bug: webrtc:13343

--- video/video_receive_stream2.h
+++ video/video_receive_stream2.h
@@ -25,7 +25,7 @@
 #include "call/video_receive_stream.h"
 #include "modules/rtp_rtcp/include/flexfec_receiver.h"
 #include "modules/rtp_rtcp/source/source_tracker.h"
-#include "modules/video_coding/frame_buffer2.h"
+#include "modules/video_coding/frame_buffer_proxy.h"
 #include "modules/video_coding/nack_requester.h"
 #include "modules/video_coding/video_receiver2.h"
 #include "rtc_base/system/no_unique_address.h"
@@ -45,6 +45,7 @@ class RtpStreamReceiverInterface;
 class RtpStreamReceiverControllerInterface;
 class RtxReceiveStream;
 class VCMTiming;
+class FrameBufferProxy;
 
 namespace internal {
 
@@ -83,7 +84,8 @@ class VideoReceiveStream2
       public NackSender,
       public RtpVideoStreamReceiver2::OnCompleteFrameCallback,
       public Syncable,
-      public CallStatsObserver {
+      public CallStatsObserver,
+      public FrameSchedulingReceiver {
  public:
   // The default number of milliseconds to pass before re-requesting a key frame
   // to be sent.
@@ -184,6 +186,8 @@ class VideoReceiveStream2
   void GenerateKeyFrame() override;
 
  private:
+  void OnEncodedFrame(std::unique_ptr<EncodedFrame> frame) override;
+  void OnDecodableFrameTimeout(TimeDelta wait_time) override;
   void CreateAndRegisterExternalDecoder(const Decoder& decoder);
   int64_t GetMaxWaitMs() const RTC_RUN_ON(decode_queue_);
   void StartNextDecode() RTC_RUN_ON(decode_queue_);
@@ -248,7 +252,7 @@ class VideoReceiveStream2
   std::vector<std::unique_ptr<VideoDecoder>> video_decoders_;
 
   // Members for the new jitter buffer experiment.
-  std::unique_ptr<video_coding::FrameBuffer> frame_buffer_;
+  std::unique_ptr<FrameBufferProxy> frame_buffer_;
 
   std::unique_ptr<RtpStreamReceiverInterface> media_receiver_
       RTC_GUARDED_BY(packet_sequence_checker_);
