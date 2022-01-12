c4e89a1a97231e1cd1a0ad4fe21f4eb1578b89a3
Henrik Boström <hbos@webrtc.org>
2022-01-12 14:56:59 +0100

    [Java] Make default sdpSemantics NOT_SPECIFIED.
    The default value of sdpSemantics is about to change from PLAN_B to
    UNIFIED_PLAN. In order not to cause subtle bugs by applications that
    depend on the default value being PLAN_B, we are temporarily making the
    default NOT_SPECIFIED. Constructing with NOT_SPECIFIED causes the C++
    layer to crash (https://webrtc-review.googlesource.com/c/src/+/242968).
    This is in accordance to the publically announced plans:
    https://groups.google.com/u/1/g/discuss-webrtc/c/SdoVP02eUIk
    
    Bug: webrtc:11121
    Change-Id: Ib086186bee947d18d31b413e3aeba0cb247b377d

--- sdk/android/instrumentationtests/src/org/webrtc/PeerConnectionEndToEndTest.java
+++ sdk/android/instrumentationtests/src/org/webrtc/PeerConnectionEndToEndTest.java
@@ -680,6 +680,7 @@ public class PeerConnectionEndToEndTest {
                        .createIceServer());
 
     PeerConnection.RTCConfiguration rtcConfig = new PeerConnection.RTCConfiguration(iceServers);
+    rtcConfig.sdpSemantics = PeerConnection.SdpSemantics.UNIFIED_PLAN;
 
     ObserverExpectations offeringExpectations = new ObserverExpectations("PCTest:offerer");
     PeerConnection offeringPC = factory.createPeerConnection(rtcConfig, offeringExpectations);
@@ -946,6 +947,7 @@ public class PeerConnectionEndToEndTest {
                        .createIceServer());
 
     PeerConnection.RTCConfiguration rtcConfig = new PeerConnection.RTCConfiguration(iceServers);
+    rtcConfig.sdpSemantics = PeerConnection.SdpSemantics.UNIFIED_PLAN;
 
     ObserverExpectations offeringExpectations = new ObserverExpectations("PCTest:offerer");
     PeerConnection offeringPC = factory.createPeerConnection(rtcConfig, offeringExpectations);
@@ -1108,6 +1110,7 @@ public class PeerConnectionEndToEndTest {
 
     PeerConnection.RTCConfiguration rtcConfig =
         new PeerConnection.RTCConfiguration(Arrays.asList());
+    rtcConfig.sdpSemantics = PeerConnection.SdpSemantics.UNIFIED_PLAN;
     // NONE would prevent any candidate being signaled to the PC.
     rtcConfig.iceTransportsType = PeerConnection.IceTransportsType.NONE;
     // We must have the continual gathering enabled to allow the surfacing of candidates on the ICE
