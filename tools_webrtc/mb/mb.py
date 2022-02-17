adaa48444e52c67ad1075ebb65e1c115bd29e097
Jeremy Leconte <jleconte@webrtc.org>
2022-02-17 11:47:01 +0100

    DO NOT SUBMIT.
    Bug: webrtc:13556
    Change-Id: I2c75774b847d9f686c3abc00ba0400bbc3fcefae

--- tools_webrtc/mb/mb.py
+++ tools_webrtc/mb/mb.py
@@ -900,6 +900,7 @@ class MetaBuildWrapper:
 
     is_android = 'target_os="android"' in vals['gn_args']
     is_linux = self.platform.startswith('linux') and not is_android
+    is_ios = 'target_os="ios"' in vals['gn_args']
 
     if test_type == 'nontest':
       self.WriteFailureAndRaise('We should not be isolating %s.' % target,
@@ -931,6 +932,20 @@ class MetaBuildWrapper:
           '--target', target, '--logdog-bin-cmd', '../../bin/logdog_butler',
           '--logcat-output-file', '${ISOLATED_OUTDIR}/logcats',
           '--store-tombstones'
+      ]
+    elif is_ios:
+      cmdline += [
+          'bin/run_%s' % target, '--out-dir', '${ISOLATED_OUTDIR}',
+          '--xcode-build-version', '12d4e', '--xctest'
+          #'--platform',
+          #'iPhone X',
+          #'--version',
+          #'14.7',
+          #'--args-json',
+          #'{"test_args":
+          #  ["--undefok=\"enable-run-ios-unittests-with-xctest\""]}',
+          #'--iossim', 'src/out/Debug-iphonesimulator/iossim', '--platform',
+          #'iPhone 11', '--version', '14.7', '--xctest'
       ]
     else:
       if test_type == 'raw':
