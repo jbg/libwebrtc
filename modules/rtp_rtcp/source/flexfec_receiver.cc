307ce00adbf4c9baf506d3094a9e364ec213cf17
Philipp Hancke <philipp.hancke@googlemail.com>
2022-03-08 21:05:42 +0100

    flexfec: more verbose logging of recovered seqnum and length
    and add periodic logging at INFO level.
    This makes it possible to validate and debug flexfec implementations
    without custom builds.
    
    BUG=None
    
    Change-Id: I6f9e73e72ec3dcc0531f7adc62ac7019c7899270

--- modules/rtp_rtcp/source/flexfec_receiver.cc
+++ modules/rtp_rtcp/source/flexfec_receiver.cc
@@ -159,17 +159,25 @@ void FlexfecReceiver::ProcessReceivedPacket(
     // Set this flag first, since OnRecoveredPacket may end up here
     // again, with the same packet.
     recovered_packet->returned = true;
-    RTC_CHECK_GT(recovered_packet->pkt->data.size(), 0);
+    RTC_CHECK_GE(recovered_packet->pkt->data.size(), kRtpHeaderSize);
     recovered_packet_receiver_->OnRecoveredPacket(
         recovered_packet->pkt->data.cdata(),
         recovered_packet->pkt->data.size());
-    // Periodically log the incoming packets.
+    uint32_t media_ssrc =
+        ForwardErrorCorrection::ParseSsrc(recovered_packet->pkt->data.data());
+    uint16_t media_seq_num = ForwardErrorCorrection::ParseSequenceNumber(
+        recovered_packet->pkt->data.data());
+    RTC_LOG(LS_VERBOSE) << "Recovered media packet with SSRC: " << media_ssrc
+                        << " seq " << media_seq_num << " recovered length "
+                        << recovered_packet->pkt->data.size()
+                        << " from FlexFEC stream with SSRC: " << ssrc_;
+    // Periodically log the incoming packets at LS_INFO.
     int64_t now_ms = clock_->TimeInMilliseconds();
     if (now_ms - last_recovered_packet_ms_ > kPacketLogIntervalMs) {
-      uint32_t media_ssrc =
-          ForwardErrorCorrection::ParseSsrc(recovered_packet->pkt->data.data());
-      RTC_LOG(LS_VERBOSE) << "Recovered media packet with SSRC: " << media_ssrc
-                          << " from FlexFEC stream with SSRC: " << ssrc_ << ".";
+      RTC_LOG(LS_INFO) << "Recovered media packet with SSRC: " << media_ssrc
+                       << " seq " << media_seq_num << " recovered length "
+                       << recovered_packet->pkt->data.size()
+                       << " from FlexFEC stream with SSRC: " << ssrc_;
       last_recovered_packet_ms_ = now_ms;
     }
   }
