ae6ce33746ade5b261e8133484e8655d64518ec5
Tommi <tommi@webrtc.org>
2022-05-18 09:54:14 +0200

    Rename AudioReceiveStream to AudioReceiveStreamInterface
    Bug: webrtc:7484
    Change-Id: I22eaa7a9e082fc575cf7471d7a2f4f706564d54f

--- call/audio_receive_stream.h
+++ call/audio_receive_stream.h
@@ -27,7 +27,7 @@
 namespace webrtc {
 class AudioSinkInterface;
 
-class AudioReceiveStream : public MediaReceiveStreamInterface {
+class AudioReceiveStreamInterface : public MediaReceiveStreamInterface {
  public:
   struct Stats {
     Stats();
@@ -148,14 +148,14 @@ class AudioReceiveStream : public MediaReceiveStreamInterface {
     // decrypted in whatever way the caller choses. This is not required by
     // default.
     // TODO(tommi): Remove this member variable from the struct. It's not
-    // a part of the AudioReceiveStream state but rather a pass through
+    // a part of the AudioReceiveStreamInterface state but rather a pass through
     // variable.
     rtc::scoped_refptr<webrtc::FrameDecryptorInterface> frame_decryptor;
 
     // An optional frame transformer used by insertable streams to transform
     // encoded frames.
     // TODO(tommi): Remove this member variable from the struct. It's not
-    // a part of the AudioReceiveStream state but rather a pass through
+    // a part of the AudioReceiveStreamInterface state but rather a pass through
     // variable.
     rtc::scoped_refptr<webrtc::FrameTransformerInterface> frame_transformer;
   };
@@ -206,7 +206,7 @@ class AudioReceiveStream : public MediaReceiveStreamInterface {
   virtual const std::vector<RtpExtension>& GetRtpExtensions() const = 0;
 
  protected:
-  virtual ~AudioReceiveStream() {}
+  virtual ~AudioReceiveStreamInterface() {}
 };
 }  // namespace webrtc
 
