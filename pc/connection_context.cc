fde88fa05c5699b3d34f4443b08fa822ff66be1d
Byoungchan Lee <daniel.l@hpcnt.com>
2022-06-17 10:40:32 +0900

    Disallow invokes from network thread to itself
    https://webrtc-review.googlesource.com/c/src/+/265060 removed the last
    case the network thread invokes itself, now we can use
    DisallowAllInvokes on the network thread.
    
    Bug: webrtc:12802
    Change-Id: I262d65bb557e2976bab5b0d9e73756222b4e02ef

--- pc/connection_context.cc
+++ pc/connection_context.cc
@@ -12,6 +12,7 @@
 
 #include <type_traits>
 #include <utility>
+#include <vector>
 
 #include "api/transport/field_trial_based_config.h"
 #include "media/base/media_engine.h"
@@ -106,21 +107,26 @@ ConnectionContext::ConnectionContext(
           MaybeCreateSctpFactory(std::move(dependencies->sctp_factory),
                                  network_thread(),
                                  *trials_.get())) {
+  RTC_DCHECK_RUN_ON(signaling_thread_);
   signaling_thread_->AllowInvokesToThread(worker_thread());
   signaling_thread_->AllowInvokesToThread(network_thread_);
   worker_thread_->AllowInvokesToThread(network_thread_);
   if (network_thread_->IsCurrent()) {
-    // TODO(https://crbug.com/webrtc/12802) switch to DisallowAllInvokes
+    // In this case, signaling_thread_ == network_thread_,
+    // so the network_thread_ should be able to invoke itself.
     network_thread_->AllowInvokesToThread(network_thread_);
   } else {
-    network_thread_->PostTask(ToQueuedTask([thread = network_thread_] {
-      thread->DisallowBlockingCalls();
-      // TODO(https://crbug.com/webrtc/12802) switch to DisallowAllInvokes
-      thread->AllowInvokesToThread(thread);
-    }));
+    network_thread_->PostTask(ToQueuedTask(
+        [thread = network_thread_, worker_thread = worker_thread_.get()] {
+          thread->DisallowBlockingCalls();
+          thread->DisallowAllInvokes();
+          if (worker_thread == thread) {
+            // In this case, worker_thread_ == network_thread.
+            thread->AllowInvokesToThread(thread);
+          }
+        }));
   }
 
-  RTC_DCHECK_RUN_ON(signaling_thread_);
   rtc::InitRandom(rtc::Time32());
 
   rtc::SocketFactory* socket_factory = dependencies->socket_factory;
