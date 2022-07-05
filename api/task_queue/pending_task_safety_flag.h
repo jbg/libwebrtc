a6587ddac86cfd8ffc4b98ee44b0a12589f5d1b6
Danil Chapovalov <danilchap@webrtc.org>
2022-07-05 10:45:43 +0200

    Introduce helper to guard an invocable with a safety flag
    This helper suppose to be a drop-in replacement for ToQueuedTask
    when TaskQueueBase usage is converted to call PostTask that takes
    AnyInvocable.
    
    Bug: webrtc:14245
    Change-Id: I590a6ca068cf5e682ffb34770bd54cf5ce37d826

--- api/task_queue/pending_task_safety_flag.h
+++ api/task_queue/pending_task_safety_flag.h
@@ -13,6 +13,7 @@
 
 #include <utility>
 
+#include "absl/functional/any_invocable.h"
 #include "api/ref_counted_base.h"
 #include "api/scoped_refptr.h"
 #include "api/sequence_checker.h"
@@ -154,6 +155,16 @@ class ScopedTaskSafetyDetached final {
   rtc::scoped_refptr<PendingTaskSafetyFlag> flag_ =
       PendingTaskSafetyFlag::CreateDetached();
 };
+
+inline absl::AnyInvocable<void() &&> SafeTask(
+    rtc::scoped_refptr<PendingTaskSafetyFlag> flag,
+    absl::AnyInvocable<void() &&> task) {
+  return [flag = std::move(flag), task = std::move(task)]() mutable {
+    if (flag->alive()) {
+      std::move(task)();
+    }
+  };
+}
 
 }  // namespace webrtc
 
