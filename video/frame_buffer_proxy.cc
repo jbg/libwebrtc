12dbef018b80c50855d14b3825db3543fb73c781
Tommi <tommi@webrtc.org>
2022-07-24 11:47:36 +0200

    Make OnEncodedFrame and OnDecodableFrameTimeout expectations consistent.
    Fix call in FrameBuffer3Proxy to OnDecodableFrameTimeout to match with
    FrameBuffer2Proxy and how OnEncodedFrame is called.
    
    Bug: none
    Change-Id: I6f5f18bd9a425aa55f5826654b24c2c0ee5af5f7

--- video/frame_buffer_proxy.cc
+++ video/frame_buffer_proxy.cc
@@ -375,7 +375,11 @@ class FrameBuffer3Proxy : public FrameBufferProxy {
       timeout_tracker_.Stop();
       return;
     }
-    receiver_->OnDecodableFrameTimeout(delay);
+    // `OnDecodableFrameTimeout` and `OnEncodedFrame` should be called on the
+    // decoder thread.
+    decode_queue_->PostTask(SafeTask(decode_safety_, [this, delay]() {
+      receiver_->OnDecodableFrameTimeout(delay);
+    }));
     // Stop sending timeouts until receive starts waiting for a new frame.
     timeout_tracker_.Stop();
     decoder_ready_for_new_frame_ = false;
