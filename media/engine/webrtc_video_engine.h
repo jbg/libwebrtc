87186e9b114dfb0f342137a16a24793c9272cb1b
Tommi <tommi@webrtc.org>
2022-08-03 00:42:48 +0200

    [WebRtcVideoReceiveStream] Split initial config and reconfiguration.
    This is a step towards getting rid of reconfiguration via tearing down
    and reconstructing receive streams when parts of the configuration
    change at runtime.
    
    Bug: webrtc:11993
    Change-Id: I337e523f17805b75826ddbd75bd3d0eb6e910bd8

--- media/engine/webrtc_video_engine.h
+++ media/engine/webrtc_video_engine.h
@@ -328,6 +328,12 @@ class WebRtcVideoChannel : public VideoMediaChannel,
   static std::string CodecSettingsVectorToString(
       const std::vector<VideoCodecSettings>& codecs);
 
+  static void ExtractCodecInformation(
+      const std::vector<VideoCodecSettings>& recv_codecs,
+      std::map<int, int>& rtx_associated_payload_types,
+      std::set<int>& raw_payload_types,
+      std::vector<webrtc::VideoReceiveStreamInterface::Decoder>& decoders);
+
   // Called when the local ssrc changes. Sets `rtcp_receiver_report_ssrc_` and
   // updates the receive streams.
   void SetReceiverReportSsrc(uint32_t ssrc) RTC_RUN_ON(&thread_checker_);
@@ -511,13 +517,13 @@ class WebRtcVideoChannel : public VideoMediaChannel,
     void SetFlexFecPayload(int payload_type);
 
     void RecreateReceiveStream();
+    void CreateReceiveStream();
+    void StartReceiveStream();
 
     // Applies a new receive codecs configration to `config_`. Returns true
     // if the internal stream needs to be reconstructed, or false if no changes
     // were applied.
-    bool ConfigureCodecs(const std::vector<VideoCodecSettings>& recv_codecs);
-
-    std::string GetCodecNameFromPayloadType(int payload_type);
+    bool ReconfigureCodecs(const std::vector<VideoCodecSettings>& recv_codecs);
 
     WebRtcVideoChannel* const channel_;
     webrtc::Call* const call_;
