ca6f92d704cef4a59c664f78732e31a69b7bde9b
Philipp Hancke <phancke@microsoft.com>
2022-08-10 11:59:42 +0300

    cleanup obsolete sps-pps-idr field trial
    which has been superseeded by the equivalent nonstandard sdp fmtp
      sps-pps-idr-in-keyframe
    
    Bug: webrtc:11769
    Change-Id: I02667a165dd3f86b4685530c43f19531ec654737

--- video/pc_full_stack_tests.cc
+++ video/pc_full_stack_tests.cc
@@ -615,9 +615,6 @@ TEST(PCGenericDescriptorTest,
 }
 
 TEST(PCFullStackTest, Pc_Foreman_Cif_Delay_50_0_Plr_5_H264_Sps_Pps_Idr) {
-  test::ScopedFieldTrials override_field_trials(
-      AppendFieldTrials("WebRTC-SpsPpsIdrIsH264Keyframe/Enabled/"));
-
   std::unique_ptr<NetworkEmulationManager> network_emulation_manager =
       CreateNetworkEmulationManager();
   BuiltInNetworkBehaviorConfig config;
