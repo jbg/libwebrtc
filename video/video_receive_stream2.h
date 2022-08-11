cf83d88463d8c0ad6f42a647f5dad806dafbe706
Tommi <tommi@webrtc.org>
2022-08-11 12:19:23 +0200

    Allow ulpfec payload type reconfig without recreating receive streams.
    Bug: none
    Change-Id: I1c5dad7811dd93552c185145e5a1488a8e9bb863

--- video/video_receive_stream2.h
+++ video/video_receive_stream2.h
@@ -149,6 +149,7 @@ class VideoReceiveStream2
   void SetFlexFecProtection(RtpPacketSinkInterface* flexfec_sink) override;
   void SetLossNotificationEnabled(bool enabled) override;
   void SetNackHistory(TimeDelta history) override;
+  void SetUlpfecPayloadType(int payload_type) override;
 
   webrtc::VideoReceiveStreamInterface::Stats GetStats() const override;
 
