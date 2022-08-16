61808355460ef4eaf3c7f9f369cd180e3209a722
Joe Downing <joedow@google.com>
2022-08-16 16:22:40 -0700

    Allow concurrent desktop_capture instances on X11
    I would like to run a separate capturer for each desktop on Linux and
    I ran into the DCHECK in XErrorTrap when I was prototyping that
    solution.  I addressed it by using a Mutex and then experienced and
    occasional hang when capturing which I traced down to
    SharedXDisplay::ProcessPendingXEvents(), this is a shared display
    instance used by each unique capturer instance so I added a mutex
    there as well.
    
    I ran 2 capturer instances concurrently for well over an hour and did
    not experience any hangs or capture artifacts.
    
    Bug: webrtc:2022
    Change-Id: Ia6778cae4bbae48886fe45f2991f02e0ea08fef6

--- modules/desktop_capture/linux/x11/x_error_trap.cc
+++ modules/desktop_capture/linux/x11/x_error_trap.cc
@@ -12,37 +12,32 @@
 
 #include <stddef.h>
 
-#include "rtc_base/checks.h"
-
 namespace webrtc {
 
 namespace {
 
-// TODO(sergeyu): This code is not thread safe. Fix it. Bug 2202.
-static bool g_xserver_error_trap_enabled = false;
 static int g_last_xserver_error_code = 0;
 
+Mutex* AcquireMutex() {
+  static Mutex* mutex = new Mutex();
+  return mutex;
+}
+
 int XServerErrorHandler(Display* display, XErrorEvent* error_event) {
-  RTC_DCHECK(g_xserver_error_trap_enabled);
   g_last_xserver_error_code = error_event->error_code;
   return 0;
 }
 
 }  // namespace
 
-XErrorTrap::XErrorTrap(Display* display)
-    : original_error_handler_(NULL), enabled_(true) {
-  RTC_DCHECK(!g_xserver_error_trap_enabled);
+XErrorTrap::XErrorTrap() : mutex_lock_(AcquireMutex()) {
   original_error_handler_ = XSetErrorHandler(&XServerErrorHandler);
-  g_xserver_error_trap_enabled = true;
   g_last_xserver_error_code = 0;
 }
 
 int XErrorTrap::GetLastErrorAndDisable() {
   enabled_ = false;
-  RTC_DCHECK(g_xserver_error_trap_enabled);
   XSetErrorHandler(original_error_handler_);
-  g_xserver_error_trap_enabled = false;
   return g_last_xserver_error_code;
 }
 
