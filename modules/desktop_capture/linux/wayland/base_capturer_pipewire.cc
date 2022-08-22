543e8cb6157018247e031eaa502944259515ad2e
Alex Cooper <alcooper@chromium.org>
2022-08-22 14:42:59 -0700

    Add plumbing to control PipeWire picker visibility
    Introduces the notion of a "delegated source list" and corresponding
    controller. This is used by desktop capturers (currently just the
    PipeWire capturer), who control selecting the source through their own
    (often system-level) UI, rather than returning a source list with all
    available options that can then be selected by the embedder.
    
    Adds a method to get the controller which serves to also tell embedders
    if the capturer makes use of a delegated source list. The controller
    currently allows the embedder to request that the delegated source list
    be shown or hidden, and will in the future be used to expose events
    from the source list (e.g. selection, dismissal, error).
    
    Bug: chromium:1351572
    Change-Id: Ie1d36ed654013f59b8d9095deef01a4705fd5bde

--- modules/desktop_capture/linux/wayland/base_capturer_pipewire.cc
+++ modules/desktop_capture/linux/wayland/base_capturer_pipewire.cc
@@ -16,8 +16,6 @@
 #include "modules/desktop_capture/linux/wayland/xdg_desktop_portal_utils.h"
 #include "rtc_base/checks.h"
 #include "rtc_base/logging.h"
-#include "rtc_base/random.h"
-#include "rtc_base/time_utils.h"
 
 namespace webrtc {
 
@@ -44,8 +42,7 @@ BaseCapturerPipeWire::BaseCapturerPipeWire(
     : options_(options),
       is_screencast_portal_(false),
       portal_(std::move(portal)) {
-  Random random(rtc::TimeMicros());
-  source_id_ = static_cast<SourceId>(random.Rand(1, INT_MAX));
+  source_id_ = RestoreTokenManager::GetInstance().GetUnusedId();
 }
 
 BaseCapturerPipeWire::~BaseCapturerPipeWire() {}
@@ -53,6 +50,7 @@ BaseCapturerPipeWire::~BaseCapturerPipeWire() {}
 void BaseCapturerPipeWire::OnScreenCastRequestResult(RequestResponse result,
                                                      uint32_t stream_node_id,
                                                      int fd) {
+  is_portal_open_ = false;
   if (result != RequestResponse::kSuccess ||
       !options_.screencast_stream()->StartScreenCastStream(
           stream_node_id, fd, options_.get_width(), options_.get_height())) {
@@ -95,6 +93,7 @@ void BaseCapturerPipeWire::Start(Callback* callback) {
     }
   }
 
+  is_portal_open_ = true;
   portal_->Start();
 }
 
@@ -135,6 +134,35 @@ bool BaseCapturerPipeWire::SelectSource(SourceId id) {
   // Screen selection is handled by the xdg-desktop-portal.
   selected_source_id_ = id;
   return true;
+}
+
+DelegatedSourceListController*
+BaseCapturerPipeWire::GetDelegatedSourceListController() {
+  return this;
+}
+
+void BaseCapturerPipeWire::EnsureVisible() {
+  RTC_DCHECK(callback_);
+  if (is_portal_open_)
+    return;
+
+  // Clear any previously selected state/capture
+  portal_->Cleanup();
+  options_.screencast_stream()->StopScreenCastStream();
+
+  // Get a new source id to reflect that the source has changed.
+  source_id_ = RestoreTokenManager::GetInstance().GetUnusedId();
+
+  is_portal_open_ = true;
+  portal_->Start();
+}
+
+void BaseCapturerPipeWire::EnsureHidden() {
+  if (!is_portal_open_)
+    return;
+
+  is_portal_open_ = false;
+  portal_->Cleanup();
 }
 
 SessionDetails BaseCapturerPipeWire::GetSessionDetails() {
