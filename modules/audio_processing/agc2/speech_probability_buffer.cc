37f3f22017e7b09511b33bed30a26771dfd77f97
Hanna Silen <silen@webrtc.org>
2022-10-05 09:38:28 +0200

    Add SpeechProbabilityBuffer
    Add a buffer class to store speech probabilities and to estimate speech
    activity. Follows the implementation of speech activity computation in
    LoudnessHistogram but uses floats for computations.
    
    Bug: webrtc:7494
    Change-Id: I6ee72ec52919904ea4e1fbe51d61993aa7813c9f

---
+++ modules/audio_processing/agc2/speech_probability_buffer.cc
@@ -0,0 +1,108 @@
+/*
+ *  Copyright (c) 2022 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "modules/audio_processing/agc2/speech_probability_buffer.h"
+
+#include <algorithm>
+
+#include "rtc_base/checks.h"
+
+namespace webrtc {
+namespace {
+
+constexpr float kActivityThreshold = 0.3f;
+constexpr int kNumAnalysisFrames = 100;
+constexpr int kTransientWidthThreshold = 7;
+
+}  // namespace
+
+SpeechProbabilityBuffer::SpeechProbabilityBuffer(
+    float low_probability_threshold)
+    : low_probability_threshold_(low_probability_threshold),
+      probabilities_(kNumAnalysisFrames) {
+  RTC_DCHECK_GE(low_probability_threshold, 0.0f);
+  RTC_DCHECK_LE(low_probability_threshold, 1.0f);
+  RTC_DCHECK(!probabilities_.empty());
+}
+
+void SpeechProbabilityBuffer::Update(float probability) {
+  // Remove the oldest entry if the circular buffer is full.
+  if (buffer_is_full_) {
+    const float oldest_probability = probabilities_[buffer_index_];
+    sum_probabilities_ -= oldest_probability;
+  }
+
+  // Check for transients.
+  if (probability <= low_probability_threshold_) {
+    // Set a probability lower than the threshold to zero.
+    probability = 0.0f;
+
+    // Check if this has been a transient.
+    if (len_high_activity_ <= kTransientWidthThreshold) {
+      RemoveTransient();
+    }
+    len_high_activity_ = 0;
+  } else if (len_high_activity_ <= kTransientWidthThreshold) {
+    ++len_high_activity_;
+  }
+
+  // Update the circular buffer and the current sum.
+  probabilities_[buffer_index_] = probability;
+  sum_probabilities_ += probability;
+
+  // Increment the buffer index and check for wrap-around.
+  if (++buffer_index_ >= static_cast<int>(probabilities_.size())) {
+    buffer_index_ = 0;
+    buffer_is_full_ = true;
+  }
+}
+
+void SpeechProbabilityBuffer::RemoveTransient() {
+  // Don't expect to be here if high-activity region is longer than
+  // `kTransientWidthThreshold` or there has not been any transient.
+  RTC_DCHECK_LE(len_high_activity_, kTransientWidthThreshold);
+
+  // Remove previously added probabilities.
+  int index =
+      (buffer_index_ > 0) ? (buffer_index_ - 1) : (probabilities_.size() - 1);
+
+  while (len_high_activity_ > 0) {
+    sum_probabilities_ -= probabilities_[index];
+    probabilities_[index] = 0.0f;
+
+    // Update the circular buffer index.
+    index = (index > 0) ? (index - 1) : (probabilities_.size() - 1);
+    --len_high_activity_;
+  }
+}
+
+bool SpeechProbabilityBuffer::IsActiveSegment() const {
+  if (probabilities_.empty()) {
+    return sum_probabilities_ >= kActivityThreshold;
+  }
+  if (!buffer_is_full_) {
+    return false;
+  }
+  if (sum_probabilities_ < kActivityThreshold * probabilities_.size()) {
+    return false;
+  }
+  return true;
+}
+
+void SpeechProbabilityBuffer::Reset() {
+  sum_probabilities_ = 0.0f;
+
+  // Empty the circular buffer.
+  buffer_index_ = 0;
+  buffer_is_full_ = false;
+  len_high_activity_ = 0;
+}
+
+}  // namespace webrtc
