03420e87ade86ec50b5e358ac3f3584473dfd303
Artem Titov <titovartem@webrtc.org>
2022-10-12 00:06:15 +0200

    [PCLF] Add ability to provide custom VideoFrameWriter
    Bug: b/240540204
    Change-Id: Ica85954ea61b7caf4e2d726895b6a439b47d7bbb

--- test/pc/e2e/analyzer/video/video_dumping_test.cc
+++ test/pc/e2e/analyzer/video/video_dumping_test.cc
@@ -17,7 +17,6 @@
 
 #include "absl/types/optional.h"
 #include "api/scoped_refptr.h"
-#include "api/test/peerconnection_quality_test_fixture.h"
 #include "api/video/i420_buffer.h"
 #include "api/video/video_frame.h"
 #include "api/video/video_frame_buffer.h"
@@ -35,9 +34,6 @@ namespace {
 using ::testing::ElementsAreArray;
 using ::testing::Eq;
 using ::testing::Test;
-
-using VideoResolution = ::webrtc::webrtc_pc_e2e::
-    PeerConnectionE2EQualityTestFixture::VideoResolution;
 
 uint8_t RandByte(Random& random) {
   return random.Rand(255);
@@ -129,10 +125,11 @@ TEST_F(CreateVideoFrameWriterTest, VideoIsWritenCorrectly) {
   VideoFrame frame1 = CreateRandom2x2VideoFrame(1, random);
   VideoFrame frame2 = CreateRandom2x2VideoFrame(2, random);
 
-  std::unique_ptr<test::VideoFrameWriter> writer = CreateVideoFrameWriter(
-      video_filename_,
-      /*frame_ids_dump_file_name=*/absl::nullopt,
-      VideoResolution(/*width=*/2, /*height=*/2, /*fps=*/2));
+  std::unique_ptr<test::VideoFrameWriter> writer =
+      CreateVideoFrameWriter(std::make_unique<test::Y4mVideoFrameWriterImpl>(
+                                 std::string(video_filename_),
+                                 /*width=*/2, /*height=*/2, /*fps=*/2),
+                             /*frame_ids_dump_file_name=*/absl::nullopt);
 
   ASSERT_TRUE(writer->WriteFrame(frame1));
   ASSERT_TRUE(writer->WriteFrame(frame2));
@@ -151,9 +148,11 @@ TEST_F(CreateVideoFrameWriterTest, VideoIsWritenWithFrameIds) {
   VideoFrame frame1 = CreateRandom2x2VideoFrame(1, random);
   VideoFrame frame2 = CreateRandom2x2VideoFrame(2, random);
 
-  std::unique_ptr<test::VideoFrameWriter> writer = CreateVideoFrameWriter(
-      video_filename_, ids_filename_,
-      VideoResolution(/*width=*/2, /*height=*/2, /*fps=*/2));
+  std::unique_ptr<test::VideoFrameWriter> writer =
+      CreateVideoFrameWriter(std::make_unique<test::Y4mVideoFrameWriterImpl>(
+                                 std::string(video_filename_),
+                                 /*width=*/2, /*height=*/2, /*fps=*/2),
+                             ids_filename_);
 
   ASSERT_TRUE(writer->WriteFrame(frame1));
   ASSERT_TRUE(writer->WriteFrame(frame2));
@@ -177,9 +176,9 @@ TEST_F(VideoWriterTest, AllFramesAreWrittenWithSamplingModulo1) {
 
   {
     std::unique_ptr<test::VideoFrameWriter> frame_writer =
-        CreateVideoFrameWriter(
-            video_filename_, /*frame_ids_dump_file_name=*/absl::nullopt,
-            VideoResolution(/*width=*/2, /*height=*/2, /*fps=*/2));
+        std::make_unique<test::Y4mVideoFrameWriterImpl>(
+            std::string(video_filename_),
+            /*width=*/2, /*height=*/2, /*fps=*/2);
     VideoWriter writer(frame_writer.get(), /*sampling_modulo=*/1);
 
     writer.OnFrame(frame1);
@@ -203,9 +202,9 @@ TEST_F(VideoWriterTest, OnlyEvery2ndFramesIsWrittenWithSamplingModulo2) {
 
   {
     std::unique_ptr<test::VideoFrameWriter> frame_writer =
-        CreateVideoFrameWriter(
-            video_filename_, /*frame_ids_dump_file_name=*/absl::nullopt,
-            VideoResolution(/*width=*/2, /*height=*/2, /*fps=*/2));
+        std::make_unique<test::Y4mVideoFrameWriterImpl>(
+            std::string(video_filename_),
+            /*width=*/2, /*height=*/2, /*fps=*/2);
     VideoWriter writer(frame_writer.get(), /*sampling_modulo=*/2);
 
     writer.OnFrame(frame1);
