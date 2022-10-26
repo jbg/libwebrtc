dba439bf0daad55192f0ce489759efec627bedea
Sam Zackrisson <saza@webrtc.org>
2022-10-26 14:16:10 +0200

    APM: Signal error on unsupported sample rates
    Bug: chromium:1332484, chromium:1334991
    Change-Id: I9639d03dc837e1fdff64d1f9d1fff0edc0fb299f

--- modules/audio_processing/audio_processing_impl.cc
+++ modules/audio_processing/audio_processing_impl.cc
@@ -309,7 +309,7 @@ AudioProcessingImpl::AudioProcessingImpl(
   capture_nonlocked_.echo_controller_enabled =
       static_cast<bool>(echo_control_factory_);
 
-  Initialize();
+  RTC_DCHECK_EQ(Initialize(), kNoError);
 }
 
 AudioProcessingImpl::~AudioProcessingImpl() = default;
@@ -416,8 +416,14 @@ int AudioProcessingImpl::InitializeLocked(const ProcessingConfig& config) {
   UpdateActiveSubmoduleStates();
 
   for (const auto& stream : config.streams) {
-    if (stream.num_channels() > 0 && stream.sample_rate_hz() <= 0) {
-      return kBadSampleRateError;
+    if (stream.num_channels() > 0) {
+      // The resampler requires frames large enough to fit its convolution
+      // kernels, so rates are restricted to 8000 Hz.
+      if (stream.sample_rate_hz() < 8000 ||
+          stream.sample_rate_hz() >
+              static_cast<int>(AudioBuffer::kMaxSampleRate)) {
+        return kBadSampleRateError;
+      }
     }
   }
 
