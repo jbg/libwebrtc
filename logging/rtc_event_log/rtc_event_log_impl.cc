e825ee3d234b12170aaabaf00a5574e80a8cf918
Markus Handell <handellm@webrtc.org>
2022-11-25 08:48:03 +0000

    RtcEventLogImpl: avoid most PostTasks.
    Research in https://webrtc-review.googlesource.com/c/src/+/250165
    shows that RtcEventLogImpl::Log PostTask traffic can be removed
    almost entirely by pretending to be the destination queue and
    executing the lambda inline. This change goes further and
    additionally avoids memory allocation for the lambda if the inline
    version of PostTask is used.
    
    Bug: chromium:1288710
    Change-Id: I940f058fe5badecfd1685832da81a7079c28c0a9

--- logging/rtc_event_log/rtc_event_log_impl.cc
+++ logging/rtc_event_log/rtc_event_log_impl.cc
@@ -16,6 +16,7 @@
 #include <utility>
 #include <vector>
 
+#include "absl/memory/memory.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/optional.h"
 #include "api/task_queue/task_queue_base.h"
@@ -24,6 +25,7 @@
 #include "logging/rtc_event_log/encoder/rtc_event_log_encoder_new_format.h"
 #include "rtc_base/checks.h"
 #include "rtc_base/event.h"
+#include "rtc_base/inline_task_queue_adapter.h"
 #include "rtc_base/logging.h"
 #include "rtc_base/numerics/safe_conversions.h"
 #include "rtc_base/numerics/safe_minmax.h"
@@ -62,7 +64,7 @@ RtcEventLogImpl::RtcEventLogImpl(RtcEventLog::EncodingType encoding_type,
       output_scheduled_(false),
       logging_state_started_(false),
       task_queue_(
-          std::make_unique<rtc::TaskQueue>(task_queue_factory->CreateTaskQueue(
+          new InlineTaskQueueAdapter(task_queue_factory->CreateTaskQueue(
               "rtc_event_log",
               TaskQueueFactory::Priority::NORMAL))) {}
 
@@ -75,8 +77,8 @@ RtcEventLogImpl::~RtcEventLogImpl() {
 
   // We want to block on any executing task by invoking ~TaskQueue() before
   // we set unique_ptr's internal pointer to null.
-  rtc::TaskQueue* tq = task_queue_.get();
-  delete tq;
+  TaskQueueBase* tq = task_queue_.get();
+  tq->Delete();
   task_queue_.release();
 }
 
