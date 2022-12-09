5353b08b98af15d28c3f7423cc7dd785231761f0
Lionel Koenig <lionelk@webrtc.org>
2022-12-09 15:41:56 +0100

    Add RtcEvent to store when MinimumSetDelay is set on NetEq
    To be able to simulate offline some scenario in which the javascript
    layer set the minimum base buffer size of neteq, it is required to
    record those API calls. This change introduces this.
    
    Bug: To be created
    Change-Id: Ic817913eda60978d6fca3f8e12229aeec505ca25

--- logging/rtc_event_log/encoder/rtc_event_log_encoder_legacy.cc
+++ logging/rtc_event_log/encoder/rtc_event_log_encoder_legacy.cc
@@ -379,6 +379,7 @@ std::string RtcEventLogEncoderLegacy::Encode(const RtcEvent& event) {
     case RtcEvent::Type::GenericPacketSent:
     case RtcEvent::Type::GenericAckReceived:
     case RtcEvent::Type::FrameDecoded:
+    case RtcEvent::Type::NetEqSetMinimumDelay:
       // These are unsupported in the old format, but shouldn't crash.
       return "";
   }
