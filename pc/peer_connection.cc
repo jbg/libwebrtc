e970fcf1bdd368a3eb2b3160a00b91e5b6a23241
Per K <perkj@webrtc.org>
2023-01-04 16:29:46 +0100

    Introduce PacketReceiver::DeliverRtpPacket and PacketReceier::DeliverRtcpPacket
    DeliverRtpPacket use a parsed RTP packet as argument where the RTP extensions are supposed to be known.
    This method is implemented in webrt::Call and temporary used by the exising method  Call::DeliverRtp, but the idea is to instead avoid extra packet parsing by forwarding a RtpPacketReceived from RtpTransport::DemuxRtpPacket via  WebrtcVideoChannel::OnPacketReceived and WebrtcVoiceChannel.
    
    DeliverRtcpPacket is also implemented in Call and is directly used in PeerConnection::InitializeRtcpCallback.
    
    Bug: webrtc:14795, webrtc:7135
    Change-Id: Ib6ffe8e1229ac07fa459ee2fc9a0af8455a23bac

--- pc/peer_connection.cc
+++ pc/peer_connection.cc
@@ -2978,11 +2978,14 @@ std::function<void(const rtc::CopyOnWriteBuffer& packet,
                    int64_t packet_time_us)>
 PeerConnection::InitializeRtcpCallback() {
   RTC_DCHECK_RUN_ON(network_thread());
-  return [this](const rtc::CopyOnWriteBuffer& packet, int64_t packet_time_us) {
-    RTC_DCHECK_RUN_ON(network_thread());
-    call_ptr_->Receiver()->DeliverPacket(MediaType::ANY, packet,
-                                         packet_time_us);
-  };
+  return
+      [this](const rtc::CopyOnWriteBuffer& packet, int64_t /*packet_time_us*/) {
+        worker_thread()->PostTask(SafeTask(
+            worker_thread_safety_, [this, packet = std::move(packet)]() {
+              RTC_DCHECK_RUN_ON(worker_thread());
+              call_ptr_->Receiver()->DeliverRtcpPacket(packet);
+            }));
+      };
 }
 
 }  // namespace webrtc
