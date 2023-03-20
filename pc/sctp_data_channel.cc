f1f0bf4397b0ed1baec1039d630932923bf85547
Tommi <tommi@webrtc.org>
2023-03-20 10:28:43 +0100

    Remove SctpDataChannel::Init()
    This is a small tweak to explicitly remove this second construction
    step from SctpDataChannel and over to DataChannelController, which is
    where OnTransportReady() is invoked from.
    
    Bug: webrtc:11547
    Change-Id: Ie86fa85cbb79b405248f88b47d5920c7f163dba6

--- pc/sctp_data_channel.cc
+++ pc/sctp_data_channel.cc
@@ -147,17 +147,9 @@ rtc::scoped_refptr<SctpDataChannel> SctpDataChannel::Create(
     rtc::Thread* signaling_thread,
     rtc::Thread* network_thread) {
   RTC_DCHECK(controller);
-
-  if (!config.IsValid()) {
-    RTC_LOG(LS_ERROR) << "Failed to initialize the SCTP data channel due to "
-                         "invalid DataChannelInit.";
-    return nullptr;
-  }
-
-  auto channel = rtc::make_ref_counted<SctpDataChannel>(
+  RTC_DCHECK(config.IsValid());
+  return rtc::make_ref_counted<SctpDataChannel>(
       config, std::move(controller), label, signaling_thread, network_thread);
-  channel->Init();
-  return channel;
 }
 
 // static
@@ -209,26 +201,6 @@ SctpDataChannel::SctpDataChannel(
   // exists.
   if (id_.HasValue()) {
     controller_->AddSctpDataStream(id_);
-  }
-}
-
-void SctpDataChannel::Init() {
-  RTC_DCHECK_RUN_ON(signaling_thread_);
-
-  // Checks if the transport is ready to send because the initial channel
-  // ready signal may have been sent before the DataChannel creation.
-  // This has to be done async because the upper layer objects (e.g.
-  // Chrome glue and WebKit) are not wired up properly until after this
-  // function returns.
-  if (controller_->ReadyToSendData()) {
-    RTC_DCHECK(connected_to_transport_);
-    AddRef();
-    absl::Cleanup release = [this] { Release(); };
-    rtc::Thread::Current()->PostTask([this, release = std::move(release)] {
-      RTC_DCHECK_RUN_ON(signaling_thread_);
-      if (state_ != kClosed)
-        OnTransportReady();
-    });
   }
 }
 
