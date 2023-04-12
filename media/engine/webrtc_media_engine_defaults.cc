e5056de56532b045fb864065355bb57a9a7c00ef
Danil Chapovalov <danilchap@webrtc.org>
2023-04-12 11:01:18 +0200

    Restore simulcast adapter for default video encoder factory
    i.e. undo functional change of the
    https://webrtc-review.googlesource.com/c/src/+/299660
    
    Bug: webrtc:13573
    Change-Id: Icf552df5570fc0a0a63b563e7e6e8a9ab47fc0e3

--- media/engine/webrtc_media_engine_defaults.cc
+++ media/engine/webrtc_media_engine_defaults.cc
@@ -10,6 +10,8 @@
 #include "media/engine/webrtc_media_engine_defaults.h"
 
 #include <memory>
+#include <string>
+#include <vector>
 
 #include "api/audio_codecs/builtin_audio_decoder_factory.h"
 #include "api/audio_codecs/builtin_audio_encoder_factory.h"
@@ -17,10 +19,46 @@
 #include "api/video/builtin_video_bitrate_allocator_factory.h"
 #include "media/engine/internal_decoder_factory.h"
 #include "media/engine/internal_encoder_factory.h"
+#include "media/engine/simulcast_encoder_adapter.h"
 #include "modules/audio_processing/include/audio_processing.h"
 #include "rtc_base/checks.h"
 
 namespace webrtc {
+namespace {
+
+// This class wraps the internal factory and adds simulcast.
+class InternalEncoderFactoryWithSimulcast : public VideoEncoderFactory {
+ public:
+  InternalEncoderFactoryWithSimulcast() = default;
+  ~InternalEncoderFactoryWithSimulcast() override = default;
+
+  std::unique_ptr<VideoEncoder> CreateVideoEncoder(
+      const SdpVideoFormat& format) override {
+    // Try creating an InternalEncoderFactory-backed SimulcastEncoderAdapter.
+    // The adapter has a passthrough mode for the case that simulcast is not
+    // used, so all responsibility can be delegated to it.
+    if (format.IsCodecInList(factory_.GetSupportedFormats())) {
+      return std::make_unique<SimulcastEncoderAdapter>(&factory_, format);
+    }
+
+    return nullptr;
+  }
+
+  std::vector<SdpVideoFormat> GetSupportedFormats() const override {
+    return factory_.GetSupportedFormats();
+  }
+
+  CodecSupport QueryCodecSupport(
+      const SdpVideoFormat& format,
+      absl::optional<std::string> scalability_mode) const override {
+    return factory_.QueryCodecSupport(format, scalability_mode);
+  }
+
+ private:
+  InternalEncoderFactory factory_;
+};
+
+}  // namespace
 
 void SetMediaEngineDefaults(cricket::MediaEngineDependencies* deps) {
   RTC_DCHECK(deps);
@@ -37,7 +75,8 @@ void SetMediaEngineDefaults(cricket::MediaEngineDependencies* deps) {
     deps->audio_processing = AudioProcessingBuilder().Create();
 
   if (deps->video_encoder_factory == nullptr)
-    deps->video_encoder_factory = std::make_unique<InternalEncoderFactory>();
+    deps->video_encoder_factory =
+        std::make_unique<InternalEncoderFactoryWithSimulcast>();
   if (deps->video_decoder_factory == nullptr)
     deps->video_decoder_factory = std::make_unique<InternalDecoderFactory>();
 }
