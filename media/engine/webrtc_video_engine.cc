0bd3d7f2549fdff172dbdf61621e55791e128e97
Markus Handell <handellm@webrtc.org>
2023-05-08 13:31:05 +0200

    Introduce support for video packet batching.
    This CL introduces a new feature enabling video packet send batches.
    The feature is enabled via
    PeerConnectionInterface
    ::RTCConfiguration
    ::MediaConfig
    ::enable_send_packet_batching.
    
    PacketOptions have been augmented with attribute "batchable" (set for
    all video packets) and attribute "last_packet_in_batch" which gives
    injected AsyncPacketSockets a chance to understand when a batch begins
    and ends.
    
    When the feature is on, packets are collected in RtpSenderEgress. On
    reception of OnBatchComplete from PacingController, RtpSenderEgress
    sends the collected batch, setting "last_packet_in_batch" to true
    in the last packet.
    
    Bug: chromium:1439830
    Change-Id: I1846b9d4a8a0efd227d617691213a2e048bdc8a2

--- media/engine/webrtc_video_engine.cc
+++ media/engine/webrtc_video_engine.cc
@@ -1386,6 +1386,8 @@ bool WebRtcVideoChannel::AddSendStream(const StreamParams& sp) {
   config.crypto_options = crypto_options_;
   config.rtp.extmap_allow_mixed = ExtmapAllowMixed();
   config.rtcp_report_interval_ms = video_config_.rtcp_report_interval_ms;
+  config.rtp.enable_send_packet_batching =
+      video_config_.enable_send_packet_batching;
 
   WebRtcVideoSendStream* stream = new WebRtcVideoSendStream(
       call_, sp, std::move(config), default_send_options_,
@@ -2058,7 +2060,9 @@ std::vector<webrtc::RtpSource> WebRtcVideoChannel::GetSources(
 bool WebRtcVideoChannel::SendRtp(const uint8_t* data,
                                  size_t len,
                                  const webrtc::PacketOptions& options) {
-  MediaChannel::SendRtp(data, len, options);
+  webrtc::PacketOptions batchable_options = options;
+  batchable_options.batchable = true;
+  MediaChannel::SendRtp(data, len, batchable_options);
   return true;
 }
 
