e700593064d50d9f3e69222ee4e2c198b57d69fc
Alfred E. Heggestad <aheggestad@nvidia.com>
2023-06-05 14:24:19 +0200

    Flexfec: add logging of packet data
    Co-authored-by: Sun Shin <sushin@nvidia.com>
    Bug:  None
    Change-Id: I676c6e6ed52c7ddcd42bfe3d6cfb1a377b2e7dbc

--- modules/rtp_rtcp/source/flexfec_receiver.cc
+++ modules/rtp_rtcp/source/flexfec_receiver.cc
@@ -18,6 +18,7 @@
 #include "api/units/timestamp.h"
 #include "rtc_base/checks.h"
 #include "rtc_base/logging.h"
+#include "rtc_base/string_encode.h"
 
 namespace webrtc {
 
@@ -184,7 +185,20 @@ void FlexfecReceiver::ProcessReceivedPacket(
                        << parsed_packet.Ssrc() << " seq "
                        << parsed_packet.SequenceNumber() << " recovered length "
                        << recovered_packet->pkt->data.size()
+                       << " received length "
+                       << received_packet.pkt->data.size()
                        << " from FlexFEC stream with SSRC: " << ssrc_;
+
+      RTC_LOG_V(level) << "Recovered media packet data: "
+                       << rtc::hex_encode(absl::string_view(
+                              (const char*)recovered_packet->pkt->data.data(),
+                              recovered_packet->pkt->data.size()));
+
+      RTC_LOG_V(level) << "Received packet data: "
+                       << rtc::hex_encode(absl::string_view(
+                              (const char*)received_packet.pkt->data.data(),
+                              received_packet.pkt->data.size()));
+
       if (should_log_periodically) {
         last_recovered_packet_ = now;
       }
