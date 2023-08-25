3ba8eca069e22d06cabe55dd2032960ef8b42c13
Tony Herre <herre@google.com>
2023-08-25 16:01:49 +0200

    Remove EncodedFrame::MissingFrame and start removing missing_frames Decode() param
    Change-Id: Id299d82e441a351deff81c0f2812707a985d23d8

--- modules/video_coding/codecs/vp8/libvpx_vp8_decoder.h
+++ modules/video_coding/codecs/vp8/libvpx_vp8_decoder.h
@@ -31,7 +31,6 @@ class LibvpxVp8Decoder : public VideoDecoder {
 
   bool Configure(const Settings& settings) override;
   int Decode(const EncodedImage& input_image,
-             bool missing_frames,
              int64_t /*render_time_ms*/) override;
 
   int RegisterDecodeCompleteCallback(DecodedImageCallback* callback) override;
@@ -61,7 +60,6 @@ class LibvpxVp8Decoder : public VideoDecoder {
   DecodedImageCallback* decode_complete_callback_;
   bool inited_;
   vpx_codec_ctx_t* decoder_;
-  int propagation_cnt_;
   int last_frame_width_;
   int last_frame_height_;
   bool key_frame_required_;
