6d4ae0ed7f77cef830a270e505de0aa183c81025
Tommi <tommi@webrtc.org>
2023-10-21 16:50:13 +0200

    Simplify ClearChannel
    Bug: none
    Change-Id: I3ee302429b1412143fecf3036766c89a5226f8e7

--- pc/rtp_transceiver.cc
+++ pc/rtp_transceiver.cc
@@ -356,22 +356,16 @@ void RtpTransceiver::ClearChannel() {
 
   RTC_LOG_THREAD_BLOCK_COUNT();
 
-  if (channel_) {
-    signaling_thread_safety_->SetNotAlive();
-    signaling_thread_safety_ = nullptr;
-  }
-  std::unique_ptr<cricket::ChannelInterface> channel_to_delete;
+  signaling_thread_safety_->SetNotAlive();
+  signaling_thread_safety_ = nullptr;
 
   context()->network_thread()->BlockingCall([&]() {
-    if (channel_) {
-      channel_->SetFirstPacketReceivedCallback(nullptr);
-      channel_->SetRtpTransport(nullptr);
-      channel_to_delete = std::move(channel_);
-    }
+    channel_->SetFirstPacketReceivedCallback(nullptr);
+    channel_->SetRtpTransport(nullptr);
   });
 
   RTC_DCHECK_BLOCK_COUNT_NO_MORE_THAN(1);
-  PushNewMediaChannelAndDeleteChannel(std::move(channel_to_delete));
+  PushNewMediaChannelAndDeleteChannel(std::move(channel_));
 
   RTC_DCHECK_BLOCK_COUNT_NO_MORE_THAN(2);
 }
@@ -399,9 +393,7 @@ void RtpTransceiver::PushNewMediaChannelAndDeleteChannel(
 
     // Destroy the channel, if we had one, now _after_ updating the receivers
     // who might have had references to the previous channel.
-    if (channel_to_delete) {
-      channel_to_delete.reset(nullptr);
-    }
+    channel_to_delete = nullptr;
   });
 }
 
