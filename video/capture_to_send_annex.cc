1060a5a302ebfe898dca2b85449c9193fab3121f
Markus Handell <handellm@webrtc.org>
2023-10-31 16:44:25 +0100

    Measure wall clock time of capture and encode processing.
    This change introduces a new task queue concept, Voucher. They
    are associated with a currently running task tree. Whenever
    tasks are posted, the current voucher is inherited and set as
    current in the new task.
    
    The voucher exists for as long as there are direct and indirect
    tasks running that descend from the task where the voucher was
    created.
    
    Vouchers aggregate application-specific attachments, which perform
    logic unrelated to Voucher progression. This particular change adds
    an attachment that measures time from capture to all encode operations
    complete, and places it into the WebRTC.Video.CaptureToSendTimeMs UMA.
    
    An accompanying Chrome change crrev.com/c/4992282 ensures
    we track VEA operations in RTCVideoEncoder, and also includes the
    first resulting packet sends.
    
    Bug: 1498378
    Change-Id: I2a27800a4e5504f219d8b9d33c56a48904cf6dde

---
+++ video/capture_to_send_annex.cc
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (c) 2023 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "video/capture_to_send_annex.h"
+
+#include <memory>
+
+#include "api/task_queue/task_queue_base.h"
+#include "rtc_base/trace_event.h"
+#include "system_wrappers/include/clock.h"
+#include "system_wrappers/include/metrics.h"
+
+namespace webrtc {
+
+// Static.
+void CaptureToSendCompleteAnnex::AttachToCurrentVoucher(
+    Timestamp capture_reference_time) {
+  static const Voucher::Annex::Id kCaptureToEncodeAnnexId =
+      Voucher::Annex::GetNextId();
+  auto voucher = Voucher::CurrentOrCreateForCurrentTask();
+  voucher->SetAnnex(
+      kCaptureToEncodeAnnexId,
+      std::make_unique<CaptureToSendCompleteAnnex>(capture_reference_time));
+}
+
+CaptureToSendCompleteAnnex::CaptureToSendCompleteAnnex(
+    Timestamp capture_reference_time)
+    : capture_reference_time_(capture_reference_time) {}
+
+CaptureToSendCompleteAnnex::~CaptureToSendCompleteAnnex() {
+  auto duration =
+      Clock::GetRealTimeClock()->CurrentTime() - capture_reference_time_;
+  TRACE_EVENT1("webrtc", "CaptureToSendCompleteAnnex", "duration",
+               duration.us());
+  RTC_HISTOGRAM_COUNTS_1000("WebRTC.Video.CaptureToSendTimeMs", duration.ms());
+}
+
+}  // namespace webrtc
