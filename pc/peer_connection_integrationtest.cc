03dbb2443cd4361c37587279506454559bd43c5c
Per K <perkj@webrtc.org>
2023-11-21 13:13:56 +0100

    Use rtc::ReceivedPacket in Stun and TurnServer
    Bug: webrtc:15368, webrtc:11943
    Change-Id: I94cc3a47278d5489de7f170c8d43015d1551c437

--- pc/peer_connection_integrationtest.cc
+++ pc/peer_connection_integrationtest.cc
@@ -1755,8 +1755,8 @@ class PeerConnectionIntegrationIceStatesTest
   }
 
   void StartStunServer(const SocketAddress& server_address) {
-    stun_server_.reset(
-        cricket::TestStunServer::Create(firewall(), server_address));
+    stun_server_.reset(cricket::TestStunServer::Create(
+        firewall(), server_address, *network_thread()));
   }
 
   bool TestIPv6() {
