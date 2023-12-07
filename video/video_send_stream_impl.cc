b889c7163e8552542c7643814ddac48a5e1009dc
Danil Chapovalov <danilchap@webrtc.org>
2023-12-07 13:32:22 +0100

    Spread Environment
    Bug: webrtc:15656
    Change-Id: I01f88da7a06b3dcd4a729e90aaaff06d02365351

--- video/video_send_stream_impl.cc
+++ video/video_send_stream_impl.cc
@@ -221,7 +221,7 @@ PacingConfig::PacingConfig(const PacingConfig&) = default;
 PacingConfig::~PacingConfig() = default;
 
 VideoSendStreamImpl::VideoSendStreamImpl(
-    Clock* clock,
+    const Environment& env,
     SendStatisticsProxy* stats_proxy,
     RtpTransportControllerSendInterface* transport,
     BitrateAllocatorInterface* bitrate_allocator,
@@ -230,12 +230,11 @@ VideoSendStreamImpl::VideoSendStreamImpl(
     int initial_encoder_max_bitrate,
     double initial_encoder_bitrate_priority,
     VideoEncoderConfig::ContentType content_type,
-    RtpVideoSenderInterface* rtp_video_sender,
-    const FieldTrialsView& field_trials)
-    : clock_(clock),
+    RtpVideoSenderInterface* rtp_video_sender)
+    : env_(env),
       has_alr_probing_(config->periodic_alr_bandwidth_probing ||
-                       GetAlrSettings(field_trials, content_type)),
-      pacing_config_(PacingConfig(field_trials)),
+                       GetAlrSettings(env_.field_trials(), content_type)),
+      pacing_config_(PacingConfig(env_.field_trials())),
       stats_proxy_(stats_proxy),
       config_(config),
       worker_queue_(TaskQueueBase::Current()),
@@ -251,10 +250,11 @@ VideoSendStreamImpl::VideoSendStreamImpl(
       encoder_bitrate_priority_(initial_encoder_bitrate_priority),
       video_stream_encoder_(video_stream_encoder),
       rtp_video_sender_(rtp_video_sender),
-      configured_pacing_factor_(GetConfiguredPacingFactor(*config_,
-                                                          content_type,
-                                                          pacing_config_,
-                                                          field_trials)) {
+      configured_pacing_factor_(
+          GetConfiguredPacingFactor(*config_,
+                                    content_type,
+                                    pacing_config_,
+                                    env_.field_trials())) {
   RTC_DCHECK_GE(config_->rtp.payload_type, 0);
   RTC_DCHECK_LE(config_->rtp.payload_type, 127);
   RTC_DCHECK(!config_->rtp.ssrcs.empty());
@@ -262,7 +262,8 @@ VideoSendStreamImpl::VideoSendStreamImpl(
   RTC_DCHECK_NE(initial_encoder_max_bitrate, 0);
   RTC_LOG(LS_INFO) << "VideoSendStreamImpl: " << config_->ToString();
 
-  RTC_CHECK(AlrExperimentSettings::MaxOneFieldTrialEnabled(field_trials));
+  RTC_CHECK(
+      AlrExperimentSettings::MaxOneFieldTrialEnabled(env_.field_trials()));
 
   // Only request rotation at the source when we positively know that the remote
   // side doesn't support the rotation extension. This allows us to prepare the
@@ -281,14 +282,14 @@ VideoSendStreamImpl::VideoSendStreamImpl(
   // pacing settings.
   if (configured_pacing_factor_) {
     absl::optional<AlrExperimentSettings> alr_settings =
-        GetAlrSettings(field_trials, content_type);
+        GetAlrSettings(env_.field_trials(), content_type);
     int queue_time_limit_ms;
     if (alr_settings) {
       enable_alr_bw_probing = true;
       queue_time_limit_ms = alr_settings->max_paced_queue_time;
     } else {
       RateControlSettings rate_control_settings =
-          RateControlSettings::ParseFromKeyValueConfig(&field_trials);
+          RateControlSettings::ParseFromKeyValueConfig(&env_.field_trials());
       enable_alr_bw_probing = rate_control_settings.UseAlrProbing();
       queue_time_limit_ms = pacing_config_.max_pacing_delay.Get().ms();
     }
@@ -404,7 +405,7 @@ void VideoSendStreamImpl::OnBitrateAllocationUpdated(
     if (encoder_target_rate_bps_ == 0) {
       return;
     }
-    int64_t now_ms = clock_->TimeInMilliseconds();
+    int64_t now_ms = env_.clock().TimeInMilliseconds();
     if (video_bitrate_allocation_context_) {
       // If new allocation is within kMaxVbaSizeDifferencePercent larger
       // than the previously sent allocation and the same streams are still
