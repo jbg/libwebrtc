3d8c01b6c0fff6300b7759b0095a7476fe1187da
Sergey Silkin <ssilkin@webrtc.org>
2024-02-02 17:54:08 +0100

    Disable Android specific threading settings in libvpx VP8 encoder
    It used up to 3 threads for QVGA on Android before. This change disables Android-specific code path in NumberOfThreads() and uses the generic settings, which configure 1 thread for QVGA, instead. The change is guarded by a killswitch.
    
    Bug: webrtc:15828
    Change-Id: I0e9571ede64c6cb77d529d21ccb0310ccb8bfdaf

--- modules/video_coding/codecs/vp8/libvpx_vp8_encoder.cc
+++ modules/video_coding/codecs/vp8/libvpx_vp8_encoder.cc
@@ -317,7 +317,9 @@ LibvpxVp8Encoder::LibvpxVp8Encoder(std::unique_ptr<LibvpxInterface> interface,
       variable_framerate_experiment_(ParseVariableFramerateConfig(
           "WebRTC-VP8VariableFramerateScreenshare")),
       framerate_controller_(variable_framerate_experiment_.framerate_limit),
-      max_frame_drop_interval_(ParseFrameDropInterval()) {
+      max_frame_drop_interval_(ParseFrameDropInterval()),
+      android_specific_threading_settings_(webrtc::field_trial::IsEnabled(
+          "WebRTC-LibvpxVp8Encoder-AndroidSpecificThreadingSettings")) {
   // TODO(eladalon/ilnik): These reservations might be wasting memory.
   // InitEncode() is resizing to the actual size, which might be smaller.
   raw_images_.reserve(kMaxSimulcastStreams);
@@ -783,18 +785,20 @@ int LibvpxVp8Encoder::GetCpuSpeed(int width, int height) {
 
 int LibvpxVp8Encoder::NumberOfThreads(int width, int height, int cpus) {
 #if defined(WEBRTC_ANDROID)
-  if (width * height >= 320 * 180) {
-    if (cpus >= 4) {
-      // 3 threads for CPUs with 4 and more cores since most of times only 4
-      // cores will be active.
-      return 3;
-    } else if (cpus == 3 || cpus == 2) {
-      return 2;
-    } else {
-      return 1;
+  if (android_specific_threading_settings_) {
+    if (width * height >= 320 * 180) {
+      if (cpus >= 4) {
+        // 3 threads for CPUs with 4 and more cores since most of times only 4
+        // cores will be active.
+        return 3;
+      } else if (cpus == 3 || cpus == 2) {
+        return 2;
+      } else {
+        return 1;
+      }
     }
+    return 1;
   }
-  return 1;
 #else
 #if defined(WEBRTC_IOS)
   std::string trial_string =
