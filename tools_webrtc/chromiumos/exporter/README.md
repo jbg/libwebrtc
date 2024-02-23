474396ec51aef4955a51b6c674f9c4866f02f30c
Li-Yu Yu <aaronyu@google.com>
2024-02-23 05:12:08 +0000

    Open source webrtc importer tool
    This is a copy of the internal importer tool (http://shortn/_sv8NRvhD63)
    at commit a68e4f5c69a1c7a42dc09304d17f08c10dc82c55
    with minimal modifications:
    -   Added WebRTC license header
    -   Minimal README.md
    
    Bug: b:326526592
    Change-Id: I6a2b284a6b6c4f0cc461f0e0dc88c8c910d8e098

---
+++ tools_webrtc/chromiumos/exporter/README.md
@@ -0,0 +1,8 @@
+# WebRTC exporter for ChromiumOS
+
+This tool helps generate build rules of the audio processing module for ChromiumOS.
+
+The exported code is checked into the ChromiumOS repository:
+https://chromium.googlesource.com/chromiumos/third_party/webrtc-apm/
+
+TODO(aaronyu): Add more docs about how to use it.
---
+++ tools_webrtc/chromiumos/exporter/main.go
@@ -0,0 +1,184 @@
+/*
+ *  Copyright (c) 2024 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/afq984/webrtcimport/bazel"
+	"github.com/afq984/webrtcimport/gn"
+	"github.com/afq984/webrtcimport/misc"
+	. "github.com/afq984/webrtcimport/must"
+	"github.com/bazelbuild/buildtools/labels"
+)
+
+func resolve(l labels.Label) (labels.Label, bool) {
+	if after, found := strings.CutPrefix(l.Package, "third_party/abseil-cpp/"); found {
+		l.Repository = "com_google_absl"
+		l.Package = after
+		return l, false
+	}
+	if l.Package == "third_party/protobuf" {
+		return labels.Label{}, false
+	}
+	if l.Package == "third_party/libevent" || l.Package == "base/third_party/libevent" {
+		return labels.Label{
+			Repository: "pkg_config",
+			Package:    "libevent",
+			Target:     "libevent",
+		}, false
+	}
+	if strings.HasPrefix(l.Package, "third_party") {
+		switch l.Package {
+		case "third_party/rnnoise", "third_party/pffft":
+			return l, true
+		}
+		panic(l.Format())
+	}
+	switch l.Format() {
+	case "//experiments:registered_field_trials",
+		"//modules/audio_processing:audioproc_debug_proto":
+		return l, false
+	case "//:poison_default_task_queue":
+		return labels.Label{}, false
+	case "//build/config:executable_deps":
+		return labels.Label{}, false
+	}
+	return l, true
+}
+
+func loadDesc(path string, rootLibs []string) *bazel.Workspace {
+	f := Must1(os.Open(path))
+	defer f.Close()
+	gnTargets := Must1(gn.ParseDesc(f))
+
+	queue := misc.Map(rootLibs, labels.Parse)
+	queued := map[labels.Label]bool{}
+	numBazelTargets := 0
+	for _, l := range queue {
+		queued[l] = true
+	}
+
+	ws := bazel.NewWorkspace()
+
+	for len(queue) > 0 {
+		numBazelTargets++
+		label := queue[len(queue)-1]
+		queue = queue[:len(queue)-1]
+
+		gnTarget := gnTargets[label]
+		target := ws.Target(label)
+
+		for _, dep := range gnTarget.Deps {
+			dep, handleDep := resolve(dep)
+			if dep == (labels.Label{}) {
+				continue
+			}
+
+			target.AppendDep(dep)
+
+			if handleDep {
+				if queued[dep] {
+					continue
+				}
+				queued[dep] = true
+				if dep.Repository == "" {
+					// External dependency
+					queue = append(queue, dep)
+				}
+			}
+
+		}
+
+		switch gnTarget.Type {
+		case "source_set", "static_library":
+			target.Type = "cc_library"
+		case "executable":
+			target.Type = "cc_binary"
+		default:
+			log.Panicf("%q %+v", label.Format(), gnTarget)
+		}
+
+		for _, source := range gnTarget.Sources {
+			ws.AddSource(target, source)
+		}
+		for _, source := range gnTarget.Public {
+			ws.AddSource(target, source)
+		}
+	}
+
+	log.Printf("loaded %d Bazel targets out of %d GN targets", numBazelTargets, len(gnTargets))
+
+	return ws
+}
+
+func main() {
+	from := flag.String("from", "", "path of the source directory")
+	to := flag.String("to", "", "path of the output directory")
+	flag.Parse()
+	if *from == "" {
+		log.Panic("-from should not be empty")
+	}
+	if *to == "" {
+		log.Panic("-to should not be empty")
+	}
+
+	rootLibs := []string{
+		"//modules/audio_processing",
+		"//api/audio:aec3_factory",
+		"//api/task_queue:default_task_queue_factory",
+		"//rtc_tools:unpack_aecdump",
+	}
+	wsX86 := loadDesc(filepath.Join(*from, "/desc.json"), rootLibs)
+	wsArm := loadDesc(filepath.Join(*from, "/descarm.json"), rootLibs)
+	ws := bazel.Merge(map[string]*bazel.Workspace{
+		"@platforms//cpu:x86_64": wsX86,
+		"//:neon_build":          wsArm,
+	})
+
+	ws.AddBulidGN()
+	addWorkarounds(ws)
+	ws.Generate(*to)
+}
+
+func addWorkarounds(ws *bazel.Workspace) {
+	t := ws.Target(labels.Parse("//experiments:registered_field_trials"))
+	t.Type = "cc_library"
+	ws.AddSourceOverride(t, "experiments/registered_field_trials.h", `// This file was automatically generated. Do not edit.
+
+#ifndef GEN_REGISTERED_FIELD_TRIALS_H_
+#define GEN_REGISTERED_FIELD_TRIALS_H_
+
+#include "absl/strings/string_view.h"
+
+namespace webrtc {
+
+inline constexpr absl::string_view kRegisteredFieldTrials[] = {
+    "",
+};
+
+}  // namespace webrtc
+
+#endif  // GEN_REGISTERED_FIELD_TRIALS_H_
+`)
+
+	debugProto := ws.Target(labels.Parse("//modules/audio_processing:debug_proto"))
+	debugProto.Type = "proto_library"
+	ws.AddSource(debugProto, "modules/audio_processing/debug.proto")
+
+	debugCCProto := ws.Target(labels.Parse("//modules/audio_processing:audioproc_debug_proto"))
+	debugCCProto.Type = "cc_proto_library"
+	debugCCProto.AppendDep(debugProto.Label())
+}
