3a67cc351f1079b783b272b81b0ce981a5868aa3
Tommi <tommi@webrtc.org>
2024-04-16 08:37:28 +0200

    Remove deprecated ProxyInfo code
    Bug: none
    Change-Id: I82d3ee97927031d974e2ef657312101dd910eff4

--- api/packet_socket_factory.h
+++ api/packet_socket_factory.h
@@ -17,7 +17,6 @@
 
 #include "api/async_dns_resolver.h"
 #include "rtc_base/async_packet_socket.h"
-#include "rtc_base/proxy_info.h"
 #include "rtc_base/system/rtc_export.h"
 
 namespace rtc {
@@ -64,23 +63,10 @@ class RTC_EXPORT PacketSocketFactory {
       uint16_t max_port,
       int opts) = 0;
 
-  // TODO(tommi): [[deprecated]], then remove.
   virtual AsyncPacketSocket* CreateClientTcpSocket(
       const SocketAddress& local_address,
       const SocketAddress& remote_address,
-      const ProxyInfo& proxy_info,
-      const std::string& user_agent,
-      const PacketSocketTcpOptions& tcp_options) {
-    return CreateClientTcpSocket(local_address, remote_address, tcp_options);
-  }
-
-  // TODO(tommi): Make pure virtual.
-  virtual AsyncPacketSocket* CreateClientTcpSocket(
-      const SocketAddress& local_address,
-      const SocketAddress& remote_address,
-      const PacketSocketTcpOptions& tcp_options) {
-    return nullptr;
-  }
+      const PacketSocketTcpOptions& tcp_options) = 0;
 
   virtual std::unique_ptr<webrtc::AsyncDnsResolverInterface>
   CreateAsyncDnsResolver() = 0;
