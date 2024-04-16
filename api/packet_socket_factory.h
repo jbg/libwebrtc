6b006d3d083feb269ed92e84e71d9e6203dc3125
Tommi <tommi@webrtc.org>
2024-04-16 08:20:31 +0200

    Remove ProxyInfo references
    Bug: none
    Change-Id: Iab4410b362a8296b2e00cf71080010e515f9f4ba

--- api/packet_socket_factory.h
+++ api/packet_socket_factory.h
@@ -64,12 +64,23 @@ class RTC_EXPORT PacketSocketFactory {
       uint16_t max_port,
       int opts) = 0;
 
+  // TODO(tommi): [[deprecated]], then remove.
   virtual AsyncPacketSocket* CreateClientTcpSocket(
       const SocketAddress& local_address,
       const SocketAddress& remote_address,
       const ProxyInfo& proxy_info,
       const std::string& user_agent,
-      const PacketSocketTcpOptions& tcp_options) = 0;
+      const PacketSocketTcpOptions& tcp_options) {
+    return CreateClientTcpSocket(local_address, remote_address, tcp_options);
+  }
+
+  // TODO(tommi): Make pure virtual.
+  virtual AsyncPacketSocket* CreateClientTcpSocket(
+      const SocketAddress& local_address,
+      const SocketAddress& remote_address,
+      const PacketSocketTcpOptions& tcp_options) {
+    return nullptr;
+  }
 
   virtual std::unique_ptr<webrtc::AsyncDnsResolverInterface>
   CreateAsyncDnsResolver() = 0;
