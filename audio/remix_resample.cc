795b9f46ff04ea258789a1213cc3e60b74e61e86
Tommi <tommi@webrtc.org>
2024-04-23 12:31:28 +0200

    Update AudioFrameOperations to require ArrayView
    Bug: chromium:335805780
    Change-Id: I14d97315f4cffa21bcc11b063e86c5adcebe78ae

--- audio/remix_resample.cc
+++ audio/remix_resample.cc
@@ -30,6 +30,7 @@ void RemixAndResample(const AudioFrame& src_frame,
   dst_frame->packet_infos_ = src_frame.packet_infos_;
 }
 
+// TODO(tommi): Update to require ArrayView.
 void RemixAndResample(const int16_t* src_data,
                       size_t samples_per_channel,
                       size_t num_channels,
@@ -48,8 +49,11 @@ void RemixAndResample(const int16_t* src_data,
         << "dst_frame->num_channels_: " << dst_frame->num_channels_;
 
     AudioFrameOperations::DownmixChannels(
-        src_data, num_channels, samples_per_channel, dst_frame->num_channels_,
-        downmixed_audio);
+        rtc::ArrayView<const int16_t>(src_data,
+                                      num_channels * samples_per_channel),
+        num_channels, samples_per_channel, dst_frame->num_channels_,
+        rtc::ArrayView<int16_t>(&downmixed_audio[0], dst_frame->num_channels_ *
+                                                         samples_per_channel));
     audio_ptr = downmixed_audio;
     audio_ptr_num_channels = dst_frame->num_channels_;
   }
