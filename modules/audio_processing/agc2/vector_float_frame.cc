f8b8af9cc58310dcced4cf978d0ef86d87c24b54
Tommi <tommi@webrtc.org>
2024-06-01 18:29:32 +0200

    Updates to AudioFrameView and VectorFloatFrame
    Using DeinterleavedView<> simplifies these two classes, so now the
    classes are arguably thing wrappers on top of DeinterleavedView<>.
    
    The changes are:
    * Make VectorFloatFrame not use a vector of vectors but rather
      just hold a one dimensional vector of samples and leaves the mapping
      into the buffer up to DeinterleavedView<>.
    * Remove the `channel_ptrs_` vector which was required due to an
      issue with AudioFrameView.
    * AudioFrameView is now a wrapper over DeinterleavedView<>. The most
      important change is to remove the `audio_samples_` pointer, which
      pointed into an externally owned pointer array (in addition to
      the array that holds the samples themselves). Now AudioFrameView
      can be initialized without requiring such a long-lived array.
    
    Bug: chromium:335805780
    Change-Id: I8f3c23c0ac4b5a337f68e9161fc3a97271f4e87d

--- modules/audio_processing/agc2/vector_float_frame.cc
+++ modules/audio_processing/agc2/vector_float_frame.cc
@@ -12,28 +12,28 @@
 
 namespace webrtc {
 
-namespace {
-
-std::vector<float*> ConstructChannelPointers(
-    std::vector<std::vector<float>>* x) {
-  std::vector<float*> channel_ptrs;
-  for (auto& v : *x) {
-    channel_ptrs.push_back(v.data());
-  }
-  return channel_ptrs;
-}
-}  // namespace
-
 VectorFloatFrame::VectorFloatFrame(int num_channels,
                                    int samples_per_channel,
                                    float start_value)
-    : channels_(num_channels,
-                std::vector<float>(samples_per_channel, start_value)),
-      channel_ptrs_(ConstructChannelPointers(&channels_)),
-      float_frame_view_(channel_ptrs_.data(),
-                        channels_.size(),
-                        samples_per_channel) {}
+    : channels_(num_channels * samples_per_channel, start_value),
+      view_(channels_.data(), samples_per_channel, num_channels) {}
 
 VectorFloatFrame::~VectorFloatFrame() = default;
+
+AudioFrameView<float> VectorFloatFrame::float_frame_view() {
+  // This initialization assumes the implementation detail in `AudioFrameView`
+  // that only the first channel pointer is required.
+  float* init[] = {&view_.data()[0]};
+  return AudioFrameView<float>(init, view_.num_channels(),
+                               view_.samples_per_channel());
+}
+
+AudioFrameView<const float> VectorFloatFrame::float_frame_view() const {
+  // This initialization assumes the implementation detail in `AudioFrameView`
+  // that only the first channel pointer is required.
+  const float* init[] = {&view_.data()[0]};
+  return AudioFrameView<const float>(init, view_.num_channels(),
+                                     view_.samples_per_channel());
+}
 
 }  // namespace webrtc
