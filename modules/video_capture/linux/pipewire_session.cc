c2d812ea3451e58e646037d68e4efcfaa1105340
Jan Grulich <grulja@gmail.com>
2024-06-11 15:48:14 +0200

    PipeWire camera: add support for ARGB/ABGR/xRGB/xBGR formats
    Adds support for 32 bits formats needed for libcamera software ISP. This
    is needed, because libcamera enforces 8 byte alignment and we only
    support 3 byte alignment for RGB. This will make it work with 32 bits
    aligned output formats recently added to libcamera.
    
    Relevant libcamera patch: https://patchwork.libcamera.org/patch/20253/
    
    Bug: TBD
    Change-Id: I8d89120660b2304b880d952c5acd7f5cd09b611e

--- modules/video_capture/linux/pipewire_session.cc
+++ modules/video_capture/linux/pipewire_session.cc
@@ -37,6 +37,12 @@ VideoType PipeWireRawFormatToVideoType(uint32_t id) {
       return VideoType::kUYVY;
     case SPA_VIDEO_FORMAT_RGB:
       return VideoType::kRGB24;
+    case SPA_VIDEO_FORMAT_ARGB:
+    case SPA_VIDEO_FORMAT_xRGB:
+      return VideoType::kARGB;
+    case SPA_VIDEO_FORMAT_ABGR:
+    case SPA_VIDEO_FORMAT_xBGR:
+      return VideoType::kABGR;
     default:
       return VideoType::kUnknown;
   }
